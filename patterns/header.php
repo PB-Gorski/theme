54dc352bd39ac195956f3f845a4336a43e9f8668
Tomek <TK-54-4B>
2023-09-04 21:40:42 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -31,9 +31,9 @@
       <!-- wp:navigation /-->
 
       <!-- wp:group -->
-      <div class="wp-block-group modal w-full h-[209px] bg-bgLightGray opacity-0 absolute top-[100px] left-0 z-[1] transition ease duration-300">
+      <div class="wp-block-group modal w-full h-[209px] py-[20px] bg-bgLightGray opacity-0 absolute top-[100px] left-0 z-[1] transition ease duration-300">
         <!-- wp:list -->
-        <ul class="wp-block-list">
+        <ul class="wp-block-list containe mx-auto flex gap-[20px]">
           <!-- wp:list-item -->
           <li class="uppercase text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
             WSZYSTKIE
