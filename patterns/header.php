93d69791891e30dd1aaa0e520491a5035fc613f1
Tomek <TK-54-4B>
2023-09-04 18:20:36 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -8,10 +8,11 @@
  */
 ?>
 
-<!-- wp:list {"templateLock":"contentOnly","anchor":true} -->
-<ul id="Menu-desktop" class="wp-block-list nav-desktop container mx-auto h-[100px] flex items-center">
-  <!-- wp:list-item -->
-  <li class="logo mr-[80px]">
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="Menu-desktop" class="wp-block-list nav-desktop container mx-auto h-[100px] flex items-center">
+  <!-- wp:group -->
+  <div class="wp-block-group logo mr-[80px]">
+    <!-- wp:paragraph -->
     <a href="<?php home_url(); ?>">
       <!-- wp:image -->
       <figure class="wp-block-image w-[115px]">
@@ -19,47 +20,52 @@
       </figure>
       <!-- /wp:image -->
     </a>
-  </li>
-  <!-- /wp:list-item -->
-
-  <!-- wp:navigation /-->
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group btn-wrapper flex items-center gap-[10px]">
-    <!-- wp:group -->
-    <div class="wp-block-group btn-lang flex gap-[5px]">
-      <!-- wp:paragraph -->
-      <a href="#" class="">PL</a>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p class="">/</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <a href="#" class="">ENG</a>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+  <div class="wp-block-group nav-wrapper flex items center justify-between">
+    <!-- wp:navigation /-->
 
     <!-- wp:group -->
-    <div class="wp-block-group btn-fb bg-blue-700 text-white w-[20px] h-[20px] text-center rounded-[5px]">
-      <!-- wp:paragraph -->
-      <p>f</p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+    <div class="wp-block-group btn-wrapper flex items-center gap-[10px]">
+      <!-- wp:group -->
+      <div class="wp-block-group btn-lang flex gap-[5px]">
+        <!-- wp:paragraph -->
+        <a href="#" class="">PL</a>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="">/</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <a href="#" class="">ENG</a>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
 
-    <!-- wp:group -->
-    <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[230px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[230px] before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
-      <!-- wp:paragraph -->
-      <p class="font-bold z-[1]">Znajdź mieszkanie</p>
-      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group btn-fb bg-blue-700 text-white w-[20px] h-[20px] text-center rounded-[5px]">
+        <!-- wp:paragraph -->
+        <p>f</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[230px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 desktop:before:w-[90px] before:w-[230px] before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
+        <!-- wp:paragraph -->
+        <p class="font-bold z-[1]">Znajdź mieszkanie</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 
-</ul>
-<!-- /wp:list -->
+</div>
+<!-- /wp:group -->
 
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"anchor":true} -->
 <div id="Mobile" class="wp-block-group nav-mobile  h-[60px] bg-white desktop:hidden container mx-auto px-[20px] transition ease-out duration-300">
