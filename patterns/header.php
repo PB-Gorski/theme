e3a24332abf13ad8f3c4ce1d51a499f7cfd39eab
Tomek <TK-54-4B>
2023-09-04 18:29:29 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -25,7 +25,7 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group nav-wrapper flex items center justify-between gap-[20px]">
+  <div class="wp-block-group nav-wrapper w-full flex items center justify-between gap-[20px]">
     <!-- wp:navigation /-->
 
     <!-- wp:group -->
