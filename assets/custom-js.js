5a14528ca4214b88ef93a44e03463ad39796bee5
Tomek <TK-54-4B>
2023-09-04 21:18:54 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
-  AOS.init({
-    duration: 1200,
-  });
+  // AOS.init({
+  //   duration: 1200,
+  // });
   console.log('test2');
 
   const hoverMenuItem = document.querySelector('.hover-item');
