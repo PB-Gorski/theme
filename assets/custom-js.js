da7cdefa7e3fde06252f17eff7f93fa11026c096
Tomek <TK-54-4B>
2023-09-04 21:20:14 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -2,13 +2,13 @@ window.addEventListener("load", function() {
   // AOS.init({
   //   duration: 1200,
   // });
-  console.log('test2');
 
   const hoverMenuItem = document.querySelector('.hover-item');
   const modal = document.querySelector('.modal');
 
   hoverMenuItem.addEventListener('mouseover',()=>{
     modal.classList.add('opacity-100');
+    console.log('test');
   });
 
 
