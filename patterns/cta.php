5b6ccfdb662f9758bed1a10238514594e2872aa7
Tomek <TK-54-4B>
2023-09-05 17:17:05 +0200

    test

---
+++ patterns/cta.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: cta
+ * Slug: cta
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="cta" class="wp-block-group cta h-[600pxpx]">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    test cta
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
