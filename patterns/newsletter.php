9402b1b8c5bf072e9e0f3a9ae06e99c93fccc34c
Tomek <TK-54-4B>
2023-09-05 18:43:13 +0200

    test

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -12,7 +12,39 @@
 <div id="newsletter" class="wp-block-group newsletter h-[330px] relative bg-[#3d4b67]">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex">
-    newsletter
+    <!-- wp:group -->
+    <div class="wp-block-group col1 w-[50px]">
+      <!-- wp:paragraph -->
+      <p class="text-white text-[40px] leading-[150%]">Bądź zawsze na bieżąco z naszą ofertą - zapisz się do newslettera</p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:group -->
+    <div class="wp-block-group col2 w-[50px]">
+      <!-- wp:group -->
+      <div class="wp-block-group input">
+        <!-- wp:paragraph -->
+        <p class="w-[370px] h-[60px] bg-white text-white text-[40px] leading-[150%]">input</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group group btn-find-more relative bg-[#e90d38] text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+          <!-- wp:paragraph -->
+          <p class="font-bold z-[1]">Zapsiz się</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group input checkbox">
+        <!-- wp:paragraph -->
+        <p class="text-white text-[40px] leading-[150%]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych.Więcej.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
