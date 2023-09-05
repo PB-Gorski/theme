475419abd19335bdf69f2aae2a57b02f069953b8
Tomek <TK-54-4B>
2023-09-05 12:18:57 +0200

    test

--- patterns/search-bar.php
+++ patterns/search-bar.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="search-bar" class="wp-block-group search-bar">
+<div id="search-bar" class="wp-block-group search-bar h-[800px]">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
     <!-- wp:paragraph -->
