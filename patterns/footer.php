307a1032a7c156fb2b25309722f4ee282b8e9929
Tomek <TK-54-4B>
2023-09-05 18:32:03 +0200

    test

---
+++ patterns/footer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: footer
+ * Slug: footer
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="footer" class="wp-block-group footer h-[450px] relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex">
+    footer
+
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
