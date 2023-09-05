0402f9952d821855de42d87753e605aca85a0868
Tomek <TK-54-4B>
2023-09-05 16:58:33 +0200

    test

--- patterns/investments.php
+++ patterns/investments.php
@@ -81,10 +81,14 @@
     <!-- /wp:list -->
 
     <!-- wp:group -->
-    <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white max-w-[315px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
-      <!-- wp:paragraph -->
-      <p class="font-bold z-[1]">Zobacz wszystkie inwestyjcje</p>
-      <!-- /wp:paragraph -->
+    <div class="wp-block-group absolute bottom-0 left-[50%] translate-x-[-50%]">
+      <!-- wp:group -->
+      <div class="wp-block-group btn-find-more relative bg-[#3d4b66] text-white max-w-[315px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
+        <!-- wp:paragraph -->
+        <p class="font-bold z-[1]">Zobacz wszystkie inwestyjcje</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
