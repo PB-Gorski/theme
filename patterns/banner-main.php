05bac46a0e29359639dedcb95797a7be0d953a2b
Tomek <TK-54-4B>
2023-09-05 11:43:01 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -13,7 +13,7 @@
   <!-- wp:group -->
   <div class="wp-block-group call-back-pop-up w-[200px] h-[280px] text-center bg-transparent flex flex-col jusify-center items-center absolute bottom-[180px] right-[20px] z-[1]">
     <!-- wp:group -->
-    <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[15px]">
+    <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[10px]">
       <!-- wp:paragraph -->
       <p>Zostaw swój numer -</p>
       <!-- /wp:paragraph -->
