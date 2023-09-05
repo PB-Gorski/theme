73f8e320b2d8b5146465d073c88e340304893b85
Tomek <TK-54-4B>
2023-09-05 11:35:31 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -11,28 +11,33 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="Banner" class="wp-block-group banner h-screen desktop:h-screen relative">
   <!-- wp:group -->
-  <div class="wp-block-group call-back-pop-up w-[200px] h-[160px] text-center bg-white absolute bottom-[350px] right-[20px]">
-    <!-- wp:paragraph -->
-    <p>Zostaw swój numer -</p>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph -->
-    <p class="font-bold">Odzwonimy do Ciebie</p>
-    <!-- /wp:paragraph -->
+  <div class="wp-block-group call-back-pop-up w-[200px] h-[250px] text-center bg-transparent absolute bottom-[350px] right-[20px]">
     <!-- wp:group -->
-    <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-[230px] before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+    <div class="wp-block-group text w-full h-[160px] bg-white flex flex-col justify-center items-center">
       <!-- wp:paragraph -->
-      <p class="font-bold z-[1]">Zostaw numer</p>
+      <p>Zostaw swój numer -</p>
       <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="font-bold">Odzwonimy do Ciebie</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+        <!-- wp:paragraph -->
+        <p class="font-bold z-[1]">Zostaw numer</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
+
     <!-- wp:image -->
-    <figure class="wp-block-image">
+    <figure class="wp-block-image phone-icon">
       <img src="<?php echo home_url() ?>//wp-content/themes/pbgorski/assets/img/frontpage/phone-icon.png" alt="phone-icon">
     </figure>
     <!-- /wp:image -->
-
   </div>
   <!-- /wp:group -->
+
   <!-- wp:group -->
   <div class="wp-block-group h-[80%] mx-auto">
     <!-- wp:group -->
