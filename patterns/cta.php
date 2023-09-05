c203ef46255a25ec6bf15d998e398b4d6eae491a
Tomek <TK-54-4B>
2023-09-05 18:18:21 +0200

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -9,14 +9,14 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="cta" class="wp-block-group cta h-[580px] bg-[#f6f7fb] relative">
+<div id="cta" class="wp-block-group cta h-[624px] bg-[#f6f7fb] relative">
   <!-- wp:image -->
-  <figure class="wp-block-image absolute left-0 top-0">
+  <figure class="wp-block-image absolute left-0 top-0 z-[-1]">
     <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/frontpage/cta-bg.jpeg" alt="cta-bg">
   </figure>
   <!-- /wp:image -->
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] flex">
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex">
     <!-- wp:group -->
     <div class="wp-block-group col1 w-[45%]">
       <!-- wp:paragraph -->
@@ -36,7 +36,7 @@
         <!-- wp:group -->
         <div class="wp-block-group inner-col1 w-[50%]">
           <!-- wp:paragraph -->
-          <p class="text-[40px] text-bgDarkGray font-bold">Zadzwoń do nas</p>
+          <p class="text-bgDarkGray">Zadzwoń do nas</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <p class="relative text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-lightBlue before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">(58) 340 79 00</p>
@@ -45,26 +45,28 @@
         <!-- /wp:group -->
 
         <!-- wp:group -->
-        <div class="wp-block-group inner-col2 w-[50%]">
+        <div class="wp-block-group inner-col2 w-[50%] flex flex-col gap-[30px] items-center jusify-center">
           <!-- wp:paragraph -->
-          <p class="text-[40px] text-bgDarkGray font-bold">Zadzwoń do nas</p>
+          <p class="text-bgDarkGray">Napisz</p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
-          <p class="relative text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-lightBlue before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">(58) 340 79 00</p>
+          <p class="relative text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-lightBlue before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">sprzedaz@pbgorski.pl</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
+        <!-- wp:paragraph -->
+        <p class="font-bold z-[1]">Skontaktuj się</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-    <!-- wp:group -->
-    <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
-      <!-- wp:paragraph -->
-      <p class="font-bold z-[1]">Skontaktuj się</p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
+
   </div>
   <!-- /wp:group -->
 </div>
