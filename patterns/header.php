02f99828b21aca5c65e0b1e214b1f8fec2c5d87d
Tomek <TK-54-4B>
2023-09-05 10:31:09 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -38,17 +38,17 @@
           <!-- wp:list -->
           <ul class="wp-block-list mb-[20px] flex items-center gap-[20px] text-[14px] font-medium">
             <!-- wp:list-item -->
-            <li class="uppercase text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+            <li class="uppercase allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
               WSZYSTKIE
             </li>
             <!-- /wp:list-item -->
             <!-- wp:list-item -->
-            <li class="uppercase text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+            <li class="uppercase gdanskTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
               GDAŃSK
             </li>
             <!-- /wp:list-item -->
             <!-- wp:list-item -->
-            <li class="uppercase text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+            <li class="uppercase gdyniaTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
               GDYNIA
             </li>
             <!-- /wp:list-item -->
