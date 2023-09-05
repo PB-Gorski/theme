781e0b9fe394f793cd0309eac5a5a721a61c7de6
Tomek <TK-54-4B>
2023-09-05 12:14:06 +0200

    test

--- patterns/search-bar.php
+++ patterns/search-bar.php
@@ -15,9 +15,11 @@
     <!-- wp:paragraph -->
     <p>input serach</p>
     <!-- /wp:paragraph -->
-    <!-- wp:input -->
-    <input type="text">
-    <!-- /wp:input -->
+    <!-- wp:paragraph -->
+    <label for="name">Name (4 to 8 characters):</label>
+
+    <input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10" />
+    <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 </div>
