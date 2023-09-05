176fbd80f3697c830e1596d8b79dc6977b181bb9
Tomek <TK-54-4B>
2023-09-05 18:34:11 +0200

    test

---
+++ patterns/newsletter.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: newsletter
+ * Slug: newsletter
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="newsletter" class="wp-block-group newsletter h-[330px] relative bg-[#3d4b67]">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex">
+    newsletter
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
