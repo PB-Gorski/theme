1b829eac23b99e8fb44399eeedf9a9be9f0f1604
Tomek <TK-54-4B>
2023-09-05 18:01:33 +0200

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -9,16 +9,17 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="cta" class="wp-block-group cta h-[580px] bg-[#f6f7fb]">
+<div id="cta" class="wp-block-group cta h-[580px] bg-[#f6f7fb] relative">
+  <!-- wp:image -->
+  <figure class="wp-block-image absolute left-0 top-0">
+    <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/frontpage/cta-bg.jpeg" alt="cta-bg">
+  </figure>
+  <!-- /wp:image -->
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] flex">
     <!-- wp:group -->
     <div class="wp-block-group col1 w-[50%]">
-      <!-- wp:image -->
-      <figure class="wp-block-image">
-        <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/frontpage/cta-bg-2.jpg" alt="cta-bg">
-      </figure>
-      <!-- /wp:image -->
+
     </div>
     <!-- /wp:group -->
     <!-- wp:group -->
