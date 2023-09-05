80af6827b5941044359fe25d09fd2fd9e289cf22
Tomek <TK-54-4B>
2023-09-05 20:16:27 +0200

    test

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -11,7 +11,7 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="newsletter" class="wp-block-group newsletter h-[330px] relative bg-[#3d4b67]">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex items-center">
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full flex items-center gap-[80px]">
     <!-- wp:group -->
     <div class="wp-block-group col1 w-[50%]">
       <!-- wp:paragraph -->
