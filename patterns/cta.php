237e723e13e7c4d33072c4c708d6a5bb94084794
Tomek <TK-54-4B>
2023-09-05 18:10:04 +0200

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -18,13 +18,38 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] flex">
     <!-- wp:group -->
-    <div class="wp-block-group col1 w-[50%]">
-
+    <div class="wp-block-group col1 w-[45%]">
+      <!-- wp:paragraph -->
+      <p class="hidden">empty space</p>
+      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
     <!-- wp:group -->
-    <div class="wp-block-group col2 w-[50%]">
+    <div class="wp-block-group col2 w-[55%]">
+      <!-- wp:paragraph -->
+      <p class="text-[40px] font-bold">Potrzebujesz więcej informacji?</p>
+      <!-- /wp:paragraph -->
 
+      <!-- wp:group -->
+      <div class="wp-block-group inner-col1 w-[50%]">
+        <!-- wp:paragraph -->
+        <p class="text-[40px] text-bgDarkGray font-bold">Zadzwoń do nas</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="relative text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-lightBlue before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">(58) 340 79 00</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group inner-col2 w-[50%]">
+        <!-- wp:paragraph -->
+        <p class="text-[40px] text-bgDarkGray font-bold">Zadzwoń do nas</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="relative text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-lightBlue before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">(58) 340 79 00</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
