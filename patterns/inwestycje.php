c90ec6ad94d77648b162962f0012141402b1f2de
Tomek <TK-54-4B>
2023-09-05 13:42:19 +0200

    test

---
+++ patterns/inwestycje.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: inwestycje
+ * Slug: inwestycje
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="inwestycje" class="wp-block-group inwestycje h-[600pxpx] bg-red-300">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    inwestycje
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
