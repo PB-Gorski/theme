74f63059d66462d587816476bfeb1eefa8a353b4
Tomek <TK-54-4B>
2023-09-05 12:42:17 +0200

    test

--- patterns/search-bar.php
+++ patterns/search-bar.php
@@ -33,7 +33,10 @@
         <!-- wp:group -->
         <div class="wp-block-group custom-options">
           <!-- wp:paragraph -->
-          <span class="custom-option selected" data-value="gdansk">Gdańsk</span>
+          <span class="custom-option selected" data-value="">Miasto</span>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <span class="custom-option" data-value="gdansk">Gdańsk</span>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <span class="custom-option" data-value="gdynia">Gdynia</span>
