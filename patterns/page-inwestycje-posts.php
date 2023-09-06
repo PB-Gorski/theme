1a252144c6cf99feeb778de64840c2c3da7a7db4
Tomek <TK-54-4B>
2023-09-06 20:57:46 +0200

    test

--- patterns/page-inwestycje-posts.php
+++ patterns/page-inwestycje-posts.php
@@ -78,6 +78,8 @@
           $post_query->the_post();
           $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
           $counter++;
+          echo $the_post->ID;
+          echo $post_query->ID;
       ?>
           <!-- wp:list-item -->
           <li class="invest-tile max-w-full h-[450px] mb-[80px]">
