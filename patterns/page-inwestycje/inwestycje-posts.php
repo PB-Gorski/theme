bbc6c1a2496f8a87b8bc95db4893b29ae0504fb1
Tomek <TK-54-4B>
2023-09-06 16:54:57 +0200

    test

---
+++ patterns/page-inwestycje/inwestycje-posts.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: inwestycje-post
+ * Slug: inwestycje-post
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:inwestycje-post {"templateLock":"contentOnly","anchor":true} -->
+<div id="inwestycje-post" class="wp-block-group inwestycje-post h-[35vh] desktop:h-[80vh] relative">
+  <!-- wp:paragraph -->
+  <p>posts-test</p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
