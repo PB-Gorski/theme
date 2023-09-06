ebb0d5cc4bd53398f46c9cf4399925f4e4fdc17d
Tomek <TK-54-4B>
2023-09-06 10:08:37 +0200

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -26,7 +26,7 @@
     <!-- /wp:group -->
 
     <!-- wp:group -->
-    <div class="wp-block-group col2 w-[55%] pt-[180px] fflex flex-col gap-[30px] items-center jusify-center">
+    <div class="wp-block-group col2 w-[55%] pt-[160px] fflex flex-col gap-[30px] items-center jusify-center">
       <!-- wp:paragraph -->
       <p class="text-[40px] font-bold mb-[40px]">Potrzebujesz więcej informacji?</p>
       <!-- /wp:paragraph -->
