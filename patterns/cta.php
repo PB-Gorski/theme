f7d14f79db84b56933204f6704d63a2b747c2710
Tomek <TK-54-4B>
2023-09-06 10:09:58 +0200

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -58,7 +58,7 @@
       <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
+      <div class="wp-block-group group btn-find-more mb-[80px] relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
         <!-- wp:paragraph -->
         <p class="font-bold z-[1]">Skontaktuj się</p>
         <!-- /wp:paragraph -->
