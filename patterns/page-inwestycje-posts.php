8ed0aa0b119500a31b324527b55c5f6aa1e7a2b9
Tomek <TK-54-4B>
2023-09-06 19:12:29 +0200

    test

--- patterns/page-inwestycje-posts.php
+++ patterns/page-inwestycje-posts.php
@@ -9,30 +9,35 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="inwestycje-post" class="wp-block-group inwestycje-post relative bg-[#f6f7fa]">
-  <!-- wp:paragraph -->
-  <p class="container mx-auto desktop:px-0 px-[20px] mb-[40px] text-[60px] text-[#2e384c] font-bold">Wszystkie inwestycje</p>
-  <!-- /wp:paragraph -->
+<div id="inwestycje-post" class="wp-block-group inwestycje-post relative">
 
-  <!-- wp:list -->
-  <ul class="wp-block-list container mx-auto desktop:px-0 px-[20px] mb-[20px] flex items-center gap-[20px] text-[14px] font-medium">
-    <!-- wp:list-item -->
-    <li class="uppercase allTabBtn tab-active text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
-      WSZYSTKIE
-    </li>
-    <!-- /wp:list-item -->
-    <!-- wp:list-item -->
-    <li class="uppercase gdanskTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
-      GDAŃSK
-    </li>
-    <!-- /wp:list-item -->
-    <!-- wp:list-item -->
-    <li class="uppercase gdyniaTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
-      GDYNIA
-    </li>
-    <!-- /wp:list-item -->
-  </ul>
-  <!-- /wp:list -->
+  <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+  <div class="wp-block-group bg-[#f6f7fa]">
+    <!-- wp:paragraph -->
+    <p class="container mx-auto desktop:px-0 px-[20px] mb-[40px] text-[60px] text-[#2e384c] font-bold">Wszystkie inwestycje</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:list -->
+    <ul class="wp-block-list container mx-auto desktop:px-0 px-[20px] mb-[20px] flex items-center gap-[20px] text-[14px] font-medium">
+      <!-- wp:list-item -->
+      <li class="uppercase allTabBtn tab-active text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+        WSZYSTKIE
+      </li>
+      <!-- /wp:list-item -->
+      <!-- wp:list-item -->
+      <li class="uppercase gdanskTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+        GDAŃSK
+      </li>
+      <!-- /wp:list-item -->
+      <!-- wp:list-item -->
+      <li class="uppercase gdyniaTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
+        GDYNIA
+      </li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+  </div>
+  <!-- /wp:group -->
 
   <!-- wp:group -->
   <div class="wp-block-group investments-posts container mx-auto desktop:px-0 px-[20px]">
@@ -63,9 +68,9 @@
                 <!-- /wp:image -->
 
                 <!-- wp:group -->
-                <div class="wp-block-group content w-[50%]">
+                <div class="wp-block-group content w-[50%] flex flex-col items-center">
                   <!-- wp:paragraph -->
-                  <p class="mr-[20px] text-[30px] desktop:text-[36px] font-bold"><?php the_title(); ?></p>
+                  <p class="mr-[20px] text-[30px] desktop:text-[40px] font-bold"><?php the_title(); ?></p>
                   <!-- /wp:paragraph -->
                   <!-- wp:paragraph -->
                   <p class="mb-[15px] text-[18px] text-bgDarkGray">Gdańsk</p>
