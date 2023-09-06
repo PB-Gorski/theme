37ca440e8d43d5cc444f1e9657a5d55dbe80fd80
Tomek <TK-54-4B>
2023-09-06 08:45:39 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -43,45 +43,48 @@
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group call-back-pop-up w-[200px] h-[280px] text-[16px] text-center bg-transparent flex flex-col jusify-center items-center fixed bottom-[30px] right-[20px] z-[2]">
     <!-- wp:group -->
-    <div class="wp-block-group call-back-pop-up w-[200px] h-[280px] text-[16px] text-center bg-transparent flex flex-col jusify-center items-center fixed bottom-[30px] right-[20px] z-[2]">
+    <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[10px] shadow-lg">
+      <!-- wp:paragraph -->
+      <p>Zostaw swój numer -</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <p class="font-bold">Odzwonimy do Ciebie</p>
+      <!-- /wp:paragraph -->
       <!-- wp:group -->
-      <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[10px] shadow-lg">
+      <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
         <!-- wp:paragraph -->
-        <p>Zostaw swój numer -</p>
+        <p class="font-bold z-[1]">Zostaw numer</p>
         <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="font-bold">Odzwonimy do Ciebie</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:group -->
-        <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
-          <!-- wp:paragraph -->
-          <p class="font-bold z-[1]">Zostaw numer</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
-
-      <!-- wp:image -->
-      <figure class="wp-block-image phone-icon cursor-pointer">
-        <img src="<?php echo home_url() ?>//wp-content/themes/pbgorski/assets/img/frontpage/phone-icon.png" alt="phone-icon">
-      </figure>
-      <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
 
+    <!-- wp:image -->
+    <figure class="wp-block-image phone-icon cursor-pointer">
+      <img src="<?php echo home_url() ?>//wp-content/themes/pbgorski/assets/img/frontpage/phone-icon.png" alt="phone-icon">
+    </figure>
+    <!-- /wp:image -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group h-[80%] mx-auto">
     <!-- wp:group -->
-    <div class="wp-block-group h-[80%] mx-auto">
-      <!-- wp:group -->
-      <div class="wp-block-group h-[80%] w-full">
-        <!-- wp:core/video -->
-        <figure class="wp-block-video banner-video"><video autoplay="true" loop muted preload src="https://www.pbgorski.pl/assets/front/videos/header/hp.mp4?1688121499"></video>
-        </figure>
-        <!-- /wp:core/video -->
-      </div>
-      <!-- /wp:group -->
+    <div class="wp-block-group h-[80%] w-full">
+      <!-- wp:core/video -->
+      <figure class="wp-block-video banner-video"><video autoplay="true" loop muted preload src="https://www.pbgorski.pl/assets/front/videos/header/hp.mp4?1688121499"></video>
+      </figure>
+      <!-- /wp:core/video -->
     </div>
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
