c6a0f4280cbd6fa63456e9b5beff3b89d2e4614a
Tomek <TK-54-4B>
2023-09-06 12:49:24 +0200

    test

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -13,7 +13,7 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full desktop:flex items-center gap-[80px]">
     <!-- wp:group -->
-    <div class="wp-block-group col1 desktop:w-[50%]">
+    <div class="wp-block-group col1 desktop:w-[50%] mb-[20px] desktop:mb-0">
       <!-- wp:paragraph -->
       <p class="text-white text-[25px] desktop:text-[40px] leading-[150%]">Bądź zawsze na bieżąco z naszą ofertą - zapisz się do newslettera</p>
       <!-- /wp:paragraph -->
@@ -23,12 +23,12 @@
     <!-- wp:group -->
     <div class="wp-block-group col2 desktop:w-[50%]">
       <!-- wp:group -->
-      <div class="wp-block-group input mb-[15px] flex">
+      <div class="wp-block-group input mb-[15px] desktop:flex">
         <!-- wp:paragraph -->
-        <p class="w-[370px] h-[60px] bg-white text-white text-[40px] leading-[150%]">input</p>
+        <p class="w-full mb-[20px] desktop:w-[370px] h-[60px] bg-white text-white text-[40px] leading-[150%]">input</p>
         <!-- /wp:paragraph -->
         <!-- wp:group -->
-        <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+        <div class="wp-block-group group btn-find-more relative bg-primaryRed text-white w-full desktop:w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
           <!-- wp:paragraph -->
           <p class="font-bold z-[1]">Zapsiz się</p>
           <!-- /wp:paragraph -->
