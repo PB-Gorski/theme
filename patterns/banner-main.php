294881c0c08fa86780cdf9228744d41bb7bc9ed7
Tomek <TK-54-4B>
2023-09-06 09:31:19 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -17,9 +17,9 @@
   <!-- wp:group -->
   <div class="wp-block-group call-back-modal w-[850px] h-[370px] p-[45px_60px] bg-white fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-[2]">
     <!-- wp:group -->
-    <div class="wp-block-group btn-close absolute top-[10px] letf-[10px]">
+    <div class="wp-block-group btn-close absolute top-[10px] letf-[10px] cursor-pointer">
       <!-- wp:paragraph -->
-      <p class="text-[40px] text-bgDarkGray opacity-[50%]">x</p>
+      <p class="text-[40px] text-bgDarkGray opacity-[50%]">×</p>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
