03f03d28a2a211880e887cda55a01980d798c088
Tomek <TK-54-4B>
2023-09-06 11:03:51 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -3,6 +3,26 @@ window.addEventListener("load", function() {
   //   duration: 1200,
   // });
 
+  // navbar mobile handling
+  const btnOpenMenu = document.querySelector(".btn-open-menu");
+  const mainNavMenu = document.querySelector(".mobile-nav-menu");
+
+  btnOpenMenu.addEventListener("click", () => {
+    if (mainNavMenu.classList.contains("opacity-[0]")) {
+      mainNavMenu.classList.remove("opacity-[0]");
+      mainNavMenu.classList.add("opacity-[1]");
+      mainNavMenu.classList.replace('z-[-2]','z-[3]');
+      mainNavMenu.classList.remove('translate-x-[100%]');
+      btnOpenMenu.classList.add("active");
+    } else if (mainNavMenu.classList.contains("opacity-[1]")) {
+      mainNavMenu.classList.remove("opacity-[1]");
+      mainNavMenu.classList.add("opacity-[0]");
+      mainNavMenu.classList.replace('z-[3]', 'z-[-2]');
+      mainNavMenu.classList.add('translate-x-[100%]');
+      btnOpenMenu.classList.remove("active");
+    }
+  });
+
   const hoverMenuItem = document.querySelector('.hover-item');
   const modal = document.querySelector('.modal');
   const allTabBtn = document.querySelector('.allTabBtn');
