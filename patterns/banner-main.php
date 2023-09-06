b47e5007704d667a0d61e4d779a193bfdff74b81
Tomek <TK-54-4B>
2023-09-06 08:49:53 +0200

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -34,13 +34,13 @@
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group input checkbox">
-        <!-- wp:paragraph -->
-        <p class="text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych.Więcej.</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+    <!-- wp:group -->
+    <div class="wp-block-group input checkbox">
+      <!-- wp:paragraph -->
+      <p class="text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych.Więcej.</p>
+      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
   </div>
