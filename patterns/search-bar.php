775cfe35ab1eb0e947e4c37fda1e72e63350af35
Tomek <TK-54-4B>
2023-09-06 11:26:18 +0200

    test

--- patterns/search-bar.php
+++ patterns/search-bar.php
@@ -11,7 +11,7 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="search-bar" class="wp-block-group search-bar h-[250px] desktop:h-[115px] bg-bgLightGray py-[25px] transition-height duration-[300ms] ease-linear">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] text-[18px] flex flex-col desktop:flex-row gap-[1px]">
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] text-[18px] flex flex-col desktop:flex-row gap-[10px] desktop:gap-[1px]">
     <!-- wp:group -->
     <div class="wp-block-group miasto select-wrapper">
       <!-- wp:group -->
