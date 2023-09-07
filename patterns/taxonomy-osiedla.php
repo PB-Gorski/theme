005d2d42c3ec504a6e4ec5ae866d91f0ff2cd857
Tomek <TK-54-4B>
2023-09-07 14:05:32 +0200

    test

--- patterns/taxonomy-osiedla.php
+++ patterns/taxonomy-osiedla.php
@@ -11,36 +11,36 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="Wpis" class="wp-block-group taxonomy-osiedla mb-[150px] ooverflow-hidden">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px] desktop:pt-[60px]">
-    <!-- wp:paragraph -->
-    <a href="/aktualnosci/" class="group desktop:mt-[60px] mb-[20px] desktop:mb-[40px] w-fit font-semibold flex items-center gap-[8px]">
-      <!-- wp:group -->
-      <div class="wp-block-group group w-[56px] h-[56px]">
-        <!-- wp:paragraph -->
-        <p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
-            <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
-            <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
-            <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
-          </svg>
-        </p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[14px]">Wróć do aktualności</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-
-    <!-- wp:paragraph -->
-    <p>taxonomy-osiedla</p>
-    <!-- /wp:paragraph -->
-
+  <div class="wp-block-group desktop:pt-[60px]">
     <!-- wp:group -->
     <div class="wp-block-group title-wrapper bg-[#f6f7fa]">
       <!-- wp:group -->
       <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+        <!-- wp:paragraph -->
+        <a href="/aktualnosci/" class="group desktop:mt-[60px] mb-[20px] desktop:mb-[40px] w-fit font-semibold flex items-center gap-[8px]">
+          <!-- wp:group -->
+          <div class="wp-block-group group w-[56px] h-[56px]">
+            <!-- wp:paragraph -->
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
+                <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
+                <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
+                <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
+              </svg>
+            </p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+          <!-- wp:paragraph -->
+          <p class="text-[14px]">Wróć do aktualności</p>
+          <!-- /wp:paragraph -->
+        </a>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph -->
+        <p>taxonomy-osiedla</p>
+        <!-- /wp:paragraph -->
+
         <!-- wp:group -->
         <div class="wp-block-group">
           <!-- wp:paragraph -->
