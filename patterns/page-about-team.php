82f294c5070d76389b62c498f4f3b97ec8940e44
Tomek <TK-54-4B>
2023-09-07 11:15:22 +0200

    test

---
+++ patterns/page-about-team.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Title: about-team
+ * Slug: about-team
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group team relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
+    <!-- wp:paragraph -->
+    <p class="text-[30px] text-textGray leading-[44px]">Rozmach i różnorodność inwestycji prowadzonych przez firmę Górski pokazuje odwagę i konsekwencję w wieloletnim działaniu przedsiębiorstwa.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="text-[18px] text-bgDarkGray leading-[30px]">Dziś aktywność dewelopera nie ogranicza się tylko do działań budowlanych. Firma GÓRSKI wspiera także sportowe talenty, sponsoruje wydarzenia sportowe i kulturalne.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="text-[18px] text-bgDarkGray leading-[30px]">Założycielem spółki PB Górski, ze stuprocentowym polskim kapitałem, jest Bogdan Górski, który dzięki swojej determinacji i ciężkiej pracy w przeciągu przeszło 40 lat stworzył prężną firmę budowlaną i deweloperską. Dzięki temu, przez wiele lat, firma zdążyła rozwinąć m.in. własną, niezależną bazę dystrybucyjną, która ułatwia cały proces realizacji inwestycji. Obecnie PB Górski jest w posiadaniu własnej żwirowni, maszyn do rozdrabniania kruszywa, jest także producentem betonu.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:image -->
+    <figure class="wp-block-image">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/img-team-1.jpeg" alt="team-image">
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:paragraph -->
+    <p class="text-[30px] text-textGray leading-[44px]">
+      Potwierdzeniem wysokiej jakości świadczonych usług, a także zadowolenia klientów są liczne nagrody i wyróżnienia zdobywane na przestrzeni lat.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="text-[18px] text-bgDarkGray leading-[30px]">To właśnie one dają powód do dumy dla Zarządu i przekonują, że obrana droga rozwoju jest słuszna i tylko wysoka jakość oraz solidność wykonywanych usług mogą być gwarancją sukcesu. Spółkę, ze stu procentowym kapitałem polskim reprezentują jej założyciele i obecni członkowie Zarządu: Bogdan Górski – Prezes, Iwona Górska – Wiceprezes oraz Marcin Górski - Wiceprezes.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
