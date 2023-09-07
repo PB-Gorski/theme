9dd935cc4c5694a4f07f1b138338ba7f4c357bc0
Tomek <TK-54-4B>
2023-09-07 19:06:51 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -14,44 +14,48 @@
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
     <!-- wp:group -->
     <div class="wp-block-group left-col">
-      <!-- wp:paragraph -->
-      <p class="text-[30px] text-textGray mb-[40px]">Biuro Sprzedaży Mieszkań / Siedziba Firmy</p>
-      <!-- /wp:paragraph -->
       <!-- wp:group -->
-      <div class="wp-block-group address mb-[40px] text-[#8a8f99]">
+      <div class="wp-group-block section1 mb-[50px]">
         <!-- wp:paragraph -->
-        <p>ul. Joachima Lelewela 37 lok. E,</p>
+        <p class="text-[30px] text-textGray mb-[30px] font-bold">Biuro Sprzedaży Mieszkań / Siedziba Firmy</p>
         <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group address mb-[30px] text-[#8a8f99]">
+          <!-- wp:paragraph -->
+          <p>ul. Joachima Lelewela 37 lok. E,</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>80-442 Gdańsk</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- wp:group -->
+        <div class="wp-block-group phone mb-[30px]">
+          <!-- wp:paragraph -->
+          <p>kom. (+48) 607-700-555</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>kom. (+48) 691-990-112</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group when-open mb-[30px] text-primaryRed font-bold">
+          <!-- wp:paragraph -->
+          <p>Poniedziałek - czwartek: 8:00 - 17:00</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>Piątek: 8:00 - 16:00</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>Sobota: 9:00 - 14:00</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
         <!-- wp:paragraph -->
-        <p>80-442 Gdańsk</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- wp:group -->
-      <div class="wp-block-group phone mb-[40px]">
-        <!-- wp:paragraph -->
-        <p>kom. (+48) 607-700-555</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p>kom. (+48) 691-990-112</p>
+        <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group when-open mb-[40px] text-primaryRed font-bold">
-        <!-- wp:paragraph -->
-        <p>Poniedziałek - czwartek: 8:00 - 17:00</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p>Piątek: 8:00 - 16:00</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p>Sobota: 9:00 - 14:00</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
-      <!-- /wp:paragraph -->
 
     </div>
     <!-- /wp:group -->
