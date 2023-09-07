3d8a3ddd28cf991854b2455eb45e2dc447a5dd3a
Tomek <TK-54-4B>
2023-09-07 19:02:02 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -12,7 +12,49 @@
 <div id="content" class="wp-block-group content relative">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
-    content
+    <!-- wp:group -->
+    <div class="wp-block-group left-col">
+      <!-- wp:paragraph -->
+      <p class="text-[30px] text-textGray">Biuro Sprzedaży Mieszkań / Siedziba Firmy</p>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group address text-[#8a8f99]">
+        <!-- wp:paragraph -->
+        <p>ul. Joachima Lelewela 37 lok. E,</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>80-442 Gdańsk</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- wp:group -->
+      <div class="wp-block-group phone">
+        <!-- wp:paragraph -->
+        <p>kom. (+48) 607-700-555</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>kom. (+48) 691-990-112</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group when-open text-primaryRed font-bold">
+        <!-- wp:paragraph -->
+        <p>Poniedziałek - czwartek: 8:00 - 17:00</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>Piątek: 8:00 - 16:00</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>Sobota: 9:00 - 14:00</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
+      <!-- /wp:paragraph -->
+
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
