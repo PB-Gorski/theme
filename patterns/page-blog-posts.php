6fc1170021bb145c45b0f35ca7f310c04ba7ed60
Tomek <TK-54-4B>
2023-09-07 11:51:01 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -83,7 +83,7 @@
           <li class="invest-tile max-w-full h-[450px] mb-[80px]">
             <a href="<?php the_permalink(); ?>" class="relative group p-[25px] desktop:max-w-full max-w-[310px] h-full" data-aos="fade-up" data-aos-duration="300" data-aos-offset="30">
               <!-- wp:group -->
-              <div class="wp-block-group wrapper w-full h-full flex <?php echo (!($counter % 2)) ? 'flex-row-reverse' : ''; ?> gap-[50px]">
+              <div class="wp-block-group wrapper w-full h-full flex gap-[50px]">
                 <!-- wp:image -->
                 <figure class="wp-block-image w-[50%] h-[330px] desktop:h-full mb-[30px]">
                   <img src="<?php echo $postImageUrl[0]; ?>" alt="services-background-image">
