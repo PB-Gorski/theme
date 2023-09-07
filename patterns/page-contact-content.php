7d1a4051e5a4addc42ada9218abc4e540cbc6b7d
Tomek <TK-54-4B>
2023-09-07 16:51:57 +0200

    test

---
+++ patterns/page-contact-content.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Title: contact-content
+ * Slug: contact-content
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group content relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
+    content
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
