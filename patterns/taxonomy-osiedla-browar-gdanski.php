655d2d814dd5505d1ac1db4a951fb7f03df26432
Tomek <TK-54-4B>
2023-09-07 15:44:22 +0200

    test

--- patterns/taxonomy-osiedla-browar-gdanski.php
+++ patterns/taxonomy-osiedla-browar-gdanski.php
@@ -62,6 +62,8 @@
             <?php
             foreach ($taxonomies as $taxonomy) {
               $taxName = $taxonomy->name;
+              echo $taxName . '<br>';
+              echo $taxonomy->name . '<br>';
             ?>
               <!-- wp:list-item -->
               <li class="CityTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer <?php echo ($taxName == 'Browar Gdańsk') ? 'tab-active' : ''; ?>"><a href="<?php echo get_category_link($taxonomy->term_id) ?>"><?php echo $taxName; ?></a></li>
