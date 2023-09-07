c0ef624935b701651ac0bbb0c01f981a0414e09e
Tomek <TK-54-4B>
2023-09-07 19:28:34 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -71,15 +71,15 @@
           <!-- /wp:paragraph -->
         </div>
         <!-- wp:group -->
-        <div class="wp-block-group phone mb-[30px] font-semibold">
+        <div class="wp-block-group phone mb-[30px] font-semibold flex gap-y-[]">
           <!-- wp:paragraph -->
-          <a href="mailto:sprzedaz@pbgorski.pl">sprzedaz@pbgorski.pl</a>
+          <a href="mailto:sprzedaz@pbgorski.pl" class="mb-[40px] block"> email: sprzedaz@pbgorski.pl</a>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
-          <a href="tel:+48583407900">tel. (+48) 58 340-79-00</a>
+          <a href="tel:+48583407900" class="block">tel. (+48) 58 340-79-00</a>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
-          <a href="tel:+48583047101">tel. (+48) 58 304-71-01</a>
+          <a href="tel:+48583047101" class="block">tel. (+48) 58 304-71-01</a>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -108,7 +108,7 @@
           <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
             <figure class="wp-block-image">
-              <img src="../assets/img/page-contact/maps-pin.png" alt="map-pin">
+              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
             </figure>
             <!-- /wp:image -->
             <!-- wp:paragraph -->
@@ -122,7 +122,7 @@
           <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
             <figure class="wp-block-image">
-              <img src="../assets/img/page-contact/nav-pin.png" alt="map-pin">
+              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="map-pin">
             </figure>
             <!-- /wp:image -->
             <!-- wp:paragraph -->
