3e57b8ae86fd7ee1d50f9ffe15b1aaae76da1abf
Tomek <TK-54-4B>
2023-09-07 10:28:39 +0200

    test

--- patterns/page-about-content.php
+++ patterns/page-about-content.php
@@ -13,16 +13,16 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
     <!-- wp:group -->
-    <div class="wp-block-group row1 flex">
+    <div class="wp-block-group row1 flex gap-[100px]">
       <!-- wp:group -->
-      <div class="wp-block-group col1 w-[50%]">
+      <div class="wp-block-group col1 w-[50%] pl-[60px]">
         <!-- wp:paragraph -->
         <p class="text-[30px] text-textGray">40 lat obecności na pomorskim rynku, dziesiątki zrealizowanych inwestycji i wiele spektakularnych przedsięwzięć w trakcie realizacji.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:group -->
-      <div class="wp-block-group col2 w-[50%] text-[18px] text-bgDarkGray">
+      <div class="wp-block-group col2 w-[50%] text-[18px] text-bgDarkGray leading-[30px]">
         <!-- wp:paragraph -->
         <p>Przedsiębiorstwo Budowlane Górski to jedna z wiodących firm deweloperskich w Trójmieście, która nie boi się wyzwań i bezustannie stara się sprostać oczekiwaniom klientów.</p>
         <!-- /wp:paragraph -->
