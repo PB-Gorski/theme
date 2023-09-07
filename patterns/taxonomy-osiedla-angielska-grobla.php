d92ccc3d5d1df43166c892277d69579380a9736b
Tomek <TK-54-4B>
2023-09-07 15:12:56 +0200

    test

--- patterns/taxonomy-osiedla-angielska-grobla.php
+++ patterns/taxonomy-osiedla-angielska-grobla.php
@@ -65,12 +65,13 @@
 
             <?php
             foreach ($taxonomies as $taxonomy) {
-              $taxNoSpaces = str_replace(' ', '-', strtolower($taxonomy->name));
+              // $taxNoSpaces = str_replace(' ', '-', strtolower($taxonomy->name));
+              $taxName = $taxonomy->name;
               // echo $taxNoSpaces . '<br>';
               // echo $taxonomy->name . '<br>';
             ?>
               <!-- wp:list-item -->
-              <li class="CityTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer <?php ($taxNoSpaces == 'Angielska Grobla') ? 'tab-active' : 'false'; ?>"><a href="<?php echo get_category_link($taxonomy->term_id) ?>" class=""><?php echo $taxonomy->name; ?></a></li>
+              <li class="CityTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer <?php ($taxName == 'Angielska Grobla') ? 'tab-active' : 'false'; ?>"><a href="<?php echo get_category_link($taxonomy->term_id) ?>" class=""><?php echo $taxName; ?></a></li>
               <!-- /wp:list-item -->
             <?php }; ?>
           </ul>
