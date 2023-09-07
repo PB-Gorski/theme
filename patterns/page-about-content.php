02be7abe9588b14e9c068ba50f892213765a5ff8
Tomek <TK-54-4B>
2023-09-07 10:23:13 +0200

    test

--- patterns/page-about-content.php
+++ patterns/page-about-content.php
@@ -9,7 +9,31 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="content" class="wp-block-group content h-[35vh] desktop:h-[90vh] relative">
-  content
+<div id="content" class="wp-block-group content relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:group -->
+    <div class="wp-block-group row1 flex">
+      <!-- wp:group -->
+      <div class="wp-block-group col1 w-[50%]">
+        <!-- wp:paragraph -->
+        <p>40 lat obecności na pomorskim rynku, dziesiątki zrealizowanych inwestycji i wiele spektakularnych przedsięwzięć w trakcie realizacji.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group col2 w-[50%]">
+        <!-- wp:paragraph -->
+        <p>Przedsiębiorstwo Budowlane Górski to jedna z wiodących firm deweloperskich w Trójmieście, która nie boi się wyzwań i bezustannie stara się sprostać oczekiwaniom klientów.</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p>Działalność budowlaną firmy GÓRSKI uzupełnia mecenat nad kulturą oraz wsparcie dla drużyn sportowych.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
