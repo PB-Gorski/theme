a38d3e6b784b536719aa51c1db536a149e548da6
Tomek <TK-54-4B>
2023-09-07 11:22:29 +0200

    test

--- patterns/page-about-team.php
+++ patterns/page-about-team.php
@@ -13,13 +13,13 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] desktop:max-w-[680px]">
     <!-- wp:paragraph -->
-    <p class="mb-[30px ]text-[30px] text-textGray leading-[44px]">Rozmach i różnorodność inwestycji prowadzonych przez firmę Górski pokazuje odwagę i konsekwencję w wieloletnim działaniu przedsiębiorstwa.</p>
+    <p class="mb-[30px] text-[30px] text-textGray leading-[44px]">Rozmach i różnorodność inwestycji prowadzonych przez firmę Górski pokazuje odwagę i konsekwencję w wieloletnim działaniu przedsiębiorstwa.</p>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <p class="mb-[30px ]text-[18px] text-bgDarkGray leading-[30px]">Dziś aktywność dewelopera nie ogranicza się tylko do działań budowlanych. Firma GÓRSKI wspiera także sportowe talenty, sponsoruje wydarzenia sportowe i kulturalne.</p>
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]">Dziś aktywność dewelopera nie ogranicza się tylko do działań budowlanych. Firma GÓRSKI wspiera także sportowe talenty, sponsoruje wydarzenia sportowe i kulturalne.</p>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <p class="mb-[30px ]text-[18px] text-bgDarkGray leading-[30px]">Założycielem spółki PB Górski, ze stuprocentowym polskim kapitałem, jest Bogdan Górski, który dzięki swojej determinacji i ciężkiej pracy w przeciągu przeszło 40 lat stworzył prężną firmę budowlaną i deweloperską. Dzięki temu, przez wiele lat, firma zdążyła rozwinąć m.in. własną, niezależną bazę dystrybucyjną, która ułatwia cały proces realizacji inwestycji. Obecnie PB Górski jest w posiadaniu własnej żwirowni, maszyn do rozdrabniania kruszywa, jest także producentem betonu.</p>
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]">Założycielem spółki PB Górski, ze stuprocentowym polskim kapitałem, jest Bogdan Górski, który dzięki swojej determinacji i ciężkiej pracy w przeciągu przeszło 40 lat stworzył prężną firmę budowlaną i deweloperską. Dzięki temu, przez wiele lat, firma zdążyła rozwinąć m.in. własną, niezależną bazę dystrybucyjną, która ułatwia cały proces realizacji inwestycji. Obecnie PB Górski jest w posiadaniu własnej żwirowni, maszyn do rozdrabniania kruszywa, jest także producentem betonu.</p>
     <!-- /wp:paragraph -->
     <!-- wp:image -->
     <figure class="wp-block-image mb-[30px]">
@@ -27,11 +27,11 @@
     </figure>
     <!-- /wp:image -->
     <!-- wp:paragraph -->
-    <p class="mb-[30px ]text-[30px] text-textGray leading-[44px]">
+    <p class="mb-[30px] text-[30px] text-textGray leading-[44px]">
       Potwierdzeniem wysokiej jakości świadczonych usług, a także zadowolenia klientów są liczne nagrody i wyróżnienia zdobywane na przestrzeni lat.</p>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <p class="mb-[30px ]text-[18px] text-bgDarkGray leading-[30px]">To właśnie one dają powód do dumy dla Zarządu i przekonują, że obrana droga rozwoju jest słuszna i tylko wysoka jakość oraz solidność wykonywanych usług mogą być gwarancją sukcesu. Spółkę, ze stu procentowym kapitałem polskim reprezentują jej założyciele i obecni członkowie Zarządu: Bogdan Górski – Prezes, Iwona Górska – Wiceprezes oraz Marcin Górski - Wiceprezes.</p>
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]">To właśnie one dają powód do dumy dla Zarządu i przekonują, że obrana droga rozwoju jest słuszna i tylko wysoka jakość oraz solidność wykonywanych usług mogą być gwarancją sukcesu. Spółkę, ze stu procentowym kapitałem polskim reprezentują jej założyciele i obecni członkowie Zarządu: Bogdan Górski – Prezes, Iwona Górska – Wiceprezes oraz Marcin Górski - Wiceprezes.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
