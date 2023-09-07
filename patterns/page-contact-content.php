6fb7cf4c8347a2b55d400459992e937074f5eedc
Tomek <TK-54-4B>
2023-09-07 19:52:51 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -11,9 +11,9 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="content" class="wp-block-group content relative">
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px] flex gap-[50px]">
     <!-- wp:group -->
-    <div class="wp-block-group left-col">
+    <div class="wp-block-group left-col w-[50%]">
       <!-- wp:group -->
       <div class="wp-block-group section1 mb-[50px]">
         <!-- wp:paragraph -->
@@ -133,6 +133,95 @@
           </a>
         </li>
       </ul>
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group right-col w-[50%]">
+      <!-- wp:group -->
+      <div class="wp-block-group section1-drop-down mb-[50px]">
+
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group section2 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[30px] font-bold">Dane Spółki</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group address mb-[30px] text-[#8a8f99]">
+          <!-- wp:paragraph -->
+          <p>PB Górski Sp. z o.o. SKA</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>ul. Joachima Lelewela 37 lok. E</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>80-442 Gdańsk</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[30px] text-[15px] font-semibold">
+          <!-- wp:paragraph -->
+          <p>NIP 5890008712, REGON 190575747, KRS 0000710392, Sąd Rejonowy Gdańsk-Północ w Gdańsku, VII Wydział Gospodarczy Krajowego Rejestru Sądowego</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>Wysokość kapitału zakładowego i wpłaconego: 24.363.831,64 PLN</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group when-open mb-[30px] text-primaryRed font-bold">
+          <!-- wp:paragraph -->
+          <p>Poniedziałek - Czwartek: 8:00 - 17:00</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>Piątek: 8:00 - 16:00</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p>Soboty pracujące: 9:00 - 14:00</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:list -->
+      <ul class="wp-block-list font-bold">
+        <!-- wp-list-item -->
+        <li>
+          <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
+            <!-- wp:image -->
+            <figure class="wp-block-image">
+              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
+            </figure>
+            <!-- /wp:image -->
+            <!-- wp:paragraph -->
+            <p class="underline decoration-2">Zobacz na GoogleMaps</p>
+            <!-- /wp:paragraph -->
+          </a>
+        </li>
+        <!-- /wp-list-item -->
+        <!-- wp-list-item -->
+        <li>
+          <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
+            <!-- wp:image -->
+            <figure class="wp-block-image">
+              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="map-pin">
+            </figure>
+            <!-- /wp:image -->
+            <!-- wp:paragraph -->
+            <p class="underline decoration-2">Pokaż trasę dojazdu</p>
+            <!-- /wp:paragraph -->
+          </a>
+        </li>
+        <!-- /wplist-item -->
+      </ul>
+      <!-- /wplist -->
     </div>
     <!-- /wp:group -->
   </div>
