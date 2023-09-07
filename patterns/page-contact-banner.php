2f3d805fddbf7ec560808f2a02d503db7955bc28
Tomek <TK-54-4B>
2023-09-07 16:56:28 +0200

    test

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -99,10 +99,10 @@
       <!-- wp:group -->
       <div class="wp-block-group text-[#959ba6] text-[30px]">
         <!-- wp:paragraph -->
-        <a href="mailto:sprzedaz@pbgorski.pl" class="inline underline relative hover:text-[#2e384d] transition duration-[300ms] ease after:content-[''] after:absolute after:top-0 after:right-[30px] after:w-[2px] after:h-[70px] after:m-auto after:bg-[#e1e3e8] after:rotate-[25deg]">sprzedaz@pbgorski.pl</a>
+        <a href="mailto:sprzedaz@pbgorski.pl" class="inline underline relative hover:text-[#2e384d] transition duration-[300ms] ease after:content-[''] after:absolute after:top-[-10px] after:right-[-20px] after:w-[2px] after:h-[70px] after:m-auto after:bg-[#e1e3e8] after:rotate-[25deg]">sprzedaz@pbgorski.pl</a>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <div class="wp-block-group links-phones">
+        <div class="wp-block-group links-phones mt-[15px]">
           <!-- wp:paragraph -->
           <a href="tel:+48583407900" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease">(+48) 58 340 79 00, </a>
           <!-- /wp:paragraph -->
