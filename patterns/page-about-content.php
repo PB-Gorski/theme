93eeac539652ed3593b5a117523dfbbebae6855f
Tomek <TK-54-4B>
2023-09-07 10:38:18 +0200

    test

--- patterns/page-about-content.php
+++ patterns/page-about-content.php
@@ -13,7 +13,7 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px]">
     <!-- wp:group -->
-    <div class="wp-block-group row1 flex gap-[100px]">
+    <div class="wp-block-group row1 flex gap-[100px] mb-[80px]">
       <!-- wp:group -->
       <div class="wp-block-group col1 w-[50%] pl-[60px]">
         <!-- wp:paragraph -->
@@ -28,6 +28,27 @@
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
         <p>Działalność budowlaną firmy GÓRSKI uzupełnia mecenat nad kulturą oraz wsparcie dla drużyn sportowych.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group row2 flex gap-[100px] mb-[80px]">
+      <!-- wp:group -->
+      <div class="wp-block-group col1 w-[50%] pl-[60px]">
+        <!-- wp:group -->
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/img1.jpeg" alt="img">
+        </figure>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group col2 w-[50%] text-[18px] text-bgDarkGray leading-[30px]">
+        <!-- wp:paragraph -->
+        <p class="text-[18px] text-bgDarkGray leading-[30px]">Wśród realizacji dewelopera znajdują się eleganckie inwestycje w prestiżowych lokalizacjach Trójmiasta takie jak osiedle Browar Gdański, Kamienica Tobiasza, czy apartamenty Angielska Grobla na Starym Mieście w Gdańsku.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
