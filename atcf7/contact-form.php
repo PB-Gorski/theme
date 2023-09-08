844137496ed5b354b23bdb1df409bdffd91ed777
Tomek <TK-54-4B>
2023-09-08 16:48:00 +0200

    test

--- atcf7/contact-form.php
+++ atcf7/contact-form.php
@@ -7,15 +7,8 @@ if (!defined('ABSPATH')) {
  * */
 ?>
 
-<h2><?php // Example of using WP functionthe_title(); 
-    ?></h2>
 <table>
   <tbody>
-    <tr>
-      <th><label for="your-category" onclick="">Category</label></th>
-      <td> <?php            // Example of using get_field of ACF with checkbox            $cats = "'".get_field("cat1")."'";            $cats .= " '".get_field("cat2")."'";            $cats .= " '".get_field("cat3")."'";          
-            ?> [checkbox your-category id:your-category class:atcf7-form-radio-label exclusive use_label_element <?php echo $cats; ?>] </td>
-    </tr>
     <tr>
       <th><label for="your-name" onclick="">Name</label></th>
       <td> [text* your-name id:your-name] </td>
