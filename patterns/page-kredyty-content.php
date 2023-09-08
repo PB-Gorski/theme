468711f7da111017bbddbe03e63b276ecdfa2456
Tomek <TK-54-4B>
2023-09-08 11:50:28 +0200

    test

--- patterns/page-kredyty-content.php
+++ patterns/page-kredyty-content.php
@@ -14,146 +14,23 @@
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px] flex gap-[50px]">
     <!-- wp:group -->
     <div class="wp-block-group left-col w-[50%]">
+      <!-- wp:paragraph -->
+      <p class="mb-[30px] text-[24px] text-[#8a8f99]">Aby porównać ofertę kredytów w kilkunastu bankach należy poświęcić dużo czasu na przeglądanie stron w internecie, następnie na wizyty w placówkach i porównywanie poszczególnych ofert. Przychodząc na spotkanie z ekspertem kredytowym PB Górski możesz skrócić cały proces do minimum.</p>
+      <!-- /wp:paragraph -->
       <!-- wp:group -->
       <div class="wp-block-group section1 mb-[50px]">
         <!-- wp:paragraph -->
-        <p class="text-[30px] text-textGray mb-[30px] font-bold">Biuro Sprzedaży Mieszkań / Siedziba Firmy</p>
+        <p class="text-[30px] text-textGray mb-[30px] font-bold">Oszczędzaj czas</p>
         <!-- /wp:paragraph -->
         <!-- wp:group -->
-        <div class="wp-block-group address mb-[30px] text-[#8a8f99]">
+        <div class="wp-block-group mb-[30px] text-[#8a8f99]">
           <!-- wp:paragraph -->
-          <p>ul. Joachima Lelewela 37 lok. E,</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>80-442 Gdańsk</p>
+          <p>Aby porównać ofertę kredytów w kilkunastu bankach należy poświęcić dużo czasu na przeglądanie stron w internecie, następnie na wizyty w placówkach i porównywanie poszczególnych ofert. Przychodząc na spotkanie z ekspertem kredytowym PB Górski możesz skrócić cały proces do minimum.</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
-        <!-- wp:group -->
-        <div class="wp-block-group phone mb-[30px] font-semibold">
-          <!-- wp:paragraph -->
-          <a href="tel:+48607700555" class="block">kom. (+48) 607-700-555</a>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <a href="tel:+48691990112" class="block">kom. (+48) 691-990-112</a>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:group -->
-        <div class="wp-block-group when-open mb-[30px] text-primaryRed font-bold">
-          <!-- wp:paragraph -->
-          <p>Poniedziałek - Czwartek: 8:00 - 17:00</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>Piątek: 8:00 - 16:00</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>Sobota: 9:00 - 14:00</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:paragraph -->
-        <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
-        <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group section2 mb-[50px]">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-textGray mb-[30px] font-bold">Biuro Obsługi Klienta</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:group -->
-        <div class="wp-block-group address mb-[30px] text-[#8a8f99]">
-          <!-- wp:paragraph -->
-          <p>ul. Joachima Lelewela 6 lok. 1,</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>80-442 Gdańsk-Wrzeszcz</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:group -->
-        <div class="wp-block-group mb-[25px] phone flex gap-[40px]">
-          <!-- wp:group -->
-          <div class="wp-block-group labels">
-            <!-- wp:paragraph -->
-            <p class="block mb-[30px]">email:</p>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <p class="block mb-[20px]">tel.</p>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <p class="block mb-[20px]">tel.</p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-
-          <!-- wp:group -->
-          <div class="wp-block-group values">
-            <!-- wp:paragraph -->
-            <a href="mailto:sprzedaz@pbgorski.pl" class="mb-[30px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">sprzedaz@pbgorski.pl</a>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <a href="tel:+48583407900" class="mb-[10px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">(+48) 58 340-79-00</a>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <a href="tel:+483047101" class="mb-[10px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">(+48) 58 304-71-01</a>
-            <!-- /wp:paragraph -->
-
-          </div>
-          <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:group -->
-        <div class="wp-block-group when-open mb-[30px] text-primaryRed font-bold">
-          <!-- wp:paragraph -->
-          <p>Poniedziałek - Czwartek: 8:00 - 17:00</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>Piątek: 8:00 - 16:00</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p>Soboty pracujące: 9:00 - 14:00</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:paragraph -->
-        <p class="text-primaryRed">Wizyty na budowie – po wcześniejszym uzgodnieniu terminu.</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:list -->
-      <ul class="wp-block-list font-bold">
-        <!-- wp:list-item -->
-        <li>
-          <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
-            <!-- wp:image -->
-            <figure class="wp-block-image">
-              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
-            </figure>
-            <!-- /wp:image -->
-            <!-- wp:paragraph -->
-            <p class="underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">Zobacz na GoogleMaps</p>
-            <!-- /wp:paragraph -->
-          </a>
-        </li>
-        <!-- /wp:list-item -->
-        <!-- wp:list-item -->
-        <li>
-          <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
-            <!-- wp:image -->
-            <figure class="wp-block-image">
-              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="map-pin">
-            </figure>
-            <!-- /wp:image -->
-            <!-- wp:paragraph -->
-            <p class="underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">Pokaż trasę dojazdu</p>
-            <!-- /wp:paragraph -->
-          </a>
-        </li>
-        <!-- /wp:list-item -->
-      </ul>
-      <!-- /wp:list -->
     </div>
     <!-- /wp:group -->
 
