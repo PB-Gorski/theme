4ea98aaca40174e1db76d27521c38b075e92375a
Tomek <TK-54-4B>
2023-09-08 16:06:40 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -128,7 +128,7 @@
         <li>
           <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
-            <figure class="wp-block-image">
+            <figure class="wp-block-image w-[20px]">
               <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
             </figure>
             <!-- /wp:image -->
@@ -227,7 +227,7 @@
                 <li>
                   <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
-                    <figure class="wp-block-image">
+                    <figure class="wp-block-image w-[20px]">
                       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
                     </figure>
                     <!-- /wp:image -->
@@ -452,7 +452,7 @@
                 <li>
                   <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
-                    <figure class="wp-block-image">
+                    <figure class="wp-block-image w-[20px]">
                       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="map-pin">
                     </figure>
                     <!-- /wp:image -->
@@ -514,7 +514,7 @@
         <li>
           <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
-            <figure class="wp-block-image">
+            <figure class="wp-block-image w-[20px]">
               <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
             </figure>
             <!-- /wp:image -->
