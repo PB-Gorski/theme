e65752c7ea7aaa5404ec0d46e608e70d29e70187
Tomek <TK-54-4B>
2023-09-08 12:25:25 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -7,6 +7,7 @@ window.addEventListener("load", function() {
   // navbar mobile handling
   const btnOpenMenu = document.querySelector(".btn-open-menu");
   const mainNavMenu = document.querySelector(".mobile-nav-menu");
+  const logo = document.querySelector(".logo");
 
   btnOpenMenu.addEventListener("click", () => {
     if (mainNavMenu.classList.contains("opacity-[0]")) {
@@ -15,12 +16,14 @@ window.addEventListener("load", function() {
       mainNavMenu.classList.replace('z-[-2]','z-[3]');
       mainNavMenu.classList.remove('translate-x-[100%]');
       btnOpenMenu.classList.add("menu-active");
+      logo.classList.add("hidden");
     } else if (mainNavMenu.classList.contains("opacity-[1]")) {
       mainNavMenu.classList.remove("opacity-[1]");
       mainNavMenu.classList.add("opacity-[0]");
       mainNavMenu.classList.replace('z-[3]', 'z-[-2]');
       mainNavMenu.classList.add('translate-x-[100%]');
       btnOpenMenu.classList.remove("menu-active");
+      logo.classList.remove("hidden");
     }
   });
 
