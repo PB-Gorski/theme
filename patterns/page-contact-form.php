d3ae53e438a839546a81ac716fc11967261fc648
Tomek <TK-54-4B>
2023-09-08 10:21:29 +0200

    test

--- patterns/page-contact-form.php
+++ patterns/page-contact-form.php
@@ -13,5 +13,30 @@
   <!-- wp:paragraph -->
   <p> contact form</p>
   <!-- /wp:paragraph -->
+  <!-- wp:group -->
+  <div class="mapouter">
+    <div class="wp-block-image gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Joachima Lelewela 37  80-442 Gdańsk&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://gachanymph.com/">Gacha Nymph APK</a></div>
+    <style>
+      .mapouter {
+        position: relative;
+        text-align: right;
+        width: 600px;
+        height: 400px;
+      }
+
+      .gmap_canvas {
+        overflow: hidden;
+        background: none !important;
+        width: 600px;
+        height: 400px;
+      }
+
+      .gmap_iframe {
+        width: 600px !important;
+        height: 400px !important;
+      }
+    </style>
+  </div>
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
