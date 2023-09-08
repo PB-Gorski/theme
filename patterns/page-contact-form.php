181eb8ff58910b32ac8fe05589bcd83c711a5e41
Tomek <TK-54-4B>
2023-09-08 10:33:42 +0200

    test

--- patterns/page-contact-form.php
+++ patterns/page-contact-form.php
@@ -16,7 +16,7 @@
   <!-- wp:group -->
   <div class="wp-block-group mapouter">
     <!-- wp:group -->
-    <div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Joachima Lelewela 37  80-442 Gdańsk&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    <div class="wp-block-group gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Joachima Lelewela 37  80-442 Gdańsk&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
     </div>
     <!-- /wp:group -->
   </div>
