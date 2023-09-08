2bace868ec5d78313ca5324d635fe644a4bbc777
Tomek <TK-54-4B>
2023-09-08 10:36:01 +0200

    test

--- patterns/page-contact-form.php
+++ patterns/page-contact-form.php
@@ -13,14 +13,14 @@
   <!-- wp:paragraph -->
   <p> contact form</p>
   <!-- /wp:paragraph -->
-  <!-- wp:group -->
-  <div class="wp-block-group mapouter">
-    <!-- wp:group -->
-    <div class="wp-block-group gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Joachima Lelewela 37  80-442 Gdańsk&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
+  <!-- wp:list -->
+  <ul class="wp-block-list mapouter">
+    <!-- wp:list-item -->
+    <li class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Joachima Lelewela 37  80-442 Gdańsk&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    </li>
+    <!-- /wp:list-item -->
+  </ul>
+  <!-- /wp:list -->
   <!-- wp:contact-form-7/contact-form-selector {"id":46,"hash":"3c85cdb","title":"Formularz 1"} -->
   <div class="wp-block-contact-form-7-contact-form-selector">[contact-form-7 id="3c85cdb" title="Formularz 1"]</div>
   <!-- /wp:contact-form-7/contact-form-selector -->
