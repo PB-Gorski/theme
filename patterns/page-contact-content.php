639db8796ba0dabd305eb96d5a4a9465f1e40be7
Tomek <TK-54-4B>
2023-09-08 10:09:43 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -124,7 +124,7 @@
       <!-- /wp:group -->
       <!-- wp:list -->
       <ul class="wp-block-list font-bold">
-        <!-- wp-list-item -->
+        <!-- wp:list-item -->
         <li>
           <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
@@ -137,8 +137,8 @@
             <!-- /wp:paragraph -->
           </a>
         </li>
-        <!-- /wp-list-item -->
-        <!-- wp-list-item -->
+        <!-- /wp:list-item -->
+        <!-- wp:list-item -->
         <li>
           <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
@@ -221,7 +221,7 @@
 
               <!-- wp:list -->
               <ul class="wp-block-list font-bold">
-                <!-- wp-list-item -->
+                <!-- wp:list-item -->
                 <li>
                   <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
@@ -234,8 +234,8 @@
                     <!-- /wp:paragraph -->
                   </a>
                 </li>
-                <!-- /wp-list-item -->
-                <!-- wp-list-item -->
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
                 <li>
                   <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
@@ -430,7 +430,7 @@
 
               <!-- wp:list -->
               <ul class="wp-block-list font-bold">
-                <!-- wp-list-item -->
+                <!-- wp:list-item -->
                 <li>
                   <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
@@ -443,8 +443,8 @@
                     <!-- /wp:paragraph -->
                   </a>
                 </li>
-                <!-- /wp-list-item -->
-                <!-- wp-list-item -->
+                <!-- /wp:list-item -->
+                <!-- wp:list-item -->
                 <li>
                   <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
@@ -506,7 +506,7 @@
       <!-- /wp:group -->
       <!-- wp:list -->
       <ul class="wp-block-list font-bold">
-        <!-- wp-list-item -->
+        <!-- wp:list-item -->
         <li>
           <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
@@ -519,8 +519,8 @@
             <!-- /wp:paragraph -->
           </a>
         </li>
-        <!-- /wp-list-item -->
-        <!-- wp-list-item -->
+        <!-- /wp:list-item -->
+        <!-- wp:list-item -->
         <li>
           <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
             <!-- wp:image -->
