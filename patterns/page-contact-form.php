3a7c855c062d9c611491c6dbf5024a1fb4dd16e5
Tomek <TK-54-4B>
2023-09-08 10:04:20 +0200

    test

---
+++ patterns/page-contact-form.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: contact-form
+ * Slug: contact-form
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group contact-form relative">
+  contact form
+</div>
+<!-- /wp:group -->
