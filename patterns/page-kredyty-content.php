e8b73739588412c4e5b5470dd9fa92ecb686d9b6
Tomek <TK-54-4B>
2023-09-08 11:53:17 +0200

    test

--- patterns/page-kredyty-content.php
+++ patterns/page-kredyty-content.php
@@ -23,9 +23,23 @@
         <p class="text-[30px] text-textGray mb-[30px] font-bold">Oszczędzaj czas</p>
         <!-- /wp:paragraph -->
         <!-- wp:group -->
-        <div class="wp-block-group mb-[30px] text-[#8a8f99]">
+        <div class="wp-block-group mb-[10px] text-[#8a8f99]">
           <!-- wp:paragraph -->
           <p>Aby porównać ofertę kredytów w kilkunastu bankach należy poświęcić dużo czasu na przeglądanie stron w internecie, następnie na wizyty w placówkach i porównywanie poszczególnych ofert. Przychodząc na spotkanie z ekspertem kredytowym PB Górski możesz skrócić cały proces do minimum.</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group section2 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[10px] font-bold">Najkorzystniejsze oferty</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[30px] text-[#8a8f99]">
+          <!-- wp:paragraph -->
+          <p>Ekspert w przystępny sposób przedstawi Ci oferty wiodących banków, wspólnie dokonacie wyboru najkorzystniejszej dla Ciebie opcji, po czym otrzymasz dokładne instrukcje, jakie dokumenty musisz dostarczyć do złożenia wniosku.</p>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
