80b0e5d10b840ff8c1de126076388bb0c76c9552
Tomek <TK-54-4B>
2023-09-08 13:08:26 +0200

    test

--- patterns/page-about-banner.php
+++ patterns/page-about-banner.php
@@ -105,7 +105,7 @@
       <!-- wp:group -->
       <div class="wp-block-group desktop:flex">
         <!-- wp:group -->
-        <div class="wp-block-group col1 pr-[40px] w-fit h-[68px] flex gap-[15px] items-center border-r-[1px] border-[rgba(255,255,255,0.2)]">
+        <div class="wp-block-group col1 pl-[40px] desktop:pl-0 pr-[40px] w-fit h-[68px] flex gap-[15px] items-center border-r-[1px] border-[rgba(255,255,255,0.2)]">
           <!-- wp:paragraph -->
           <p id="count1" class="text-[48px] font-bold">40</p>
           <!-- /wp:paragraph -->
