6abc359826270f22f327139201f9b245f17e7bc9
Tomek <TK-54-4B>
2023-09-08 11:58:59 +0200

    test

--- patterns/page-kredyty-content.php
+++ patterns/page-kredyty-content.php
@@ -67,7 +67,42 @@
 
     <!-- wp:group -->
     <div class="wp-block-group right-col w-[50%]">
+      <!-- wp:group -->
+      <div class="wp-block-group section2 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[30px] font-bold">Umów się na spotkanie</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-[24px] text-[#7e878c]">Ewa Cel</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[25px] phone flex gap-[40px]">
+          <!-- wp:group -->
+          <div class="wp-block-group labels">
+            <!-- wp:paragraph -->
+            <p class="block mb-[20px]">tel.</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="block mb-[30px]">email:</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
 
+          <!-- wp:group -->
+          <div class="wp-block-group values">
+
+            <!-- wp:paragraph -->
+            <a href="tel:+48690099044" class="mb-[10px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">690 099 044</a>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <a href="mailto:kredyty@pbgorski.pl" class="mb-[30px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">kredyty@pbgorski.pl</a>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
   </div>
