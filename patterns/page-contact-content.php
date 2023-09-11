6154028be1524553cd52272f60878847b1746604
Tomek <TK-54-4B>
2023-09-11 12:21:08 +0200

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -578,38 +578,6 @@
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-      <!-- wp:list -->
-      <ul class="wp-block-list font-bold">
-        <!-- wp:list-item -->
-        <li>
-          <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
-            <!-- wp:image -->
-            <figure class="wp-block-image w-[20px]">
-              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
-            </figure>
-            <!-- /wp:image -->
-            <!-- wp:paragraph -->
-            <p class="underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">Zobacz na GoogleMaps</p>
-            <!-- /wp:paragraph -->
-          </a>
-        </li>
-        <!-- /wp:list-item -->
-        <!-- wp:list-item -->
-        <li>
-          <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
-            <!-- wp:image -->
-            <figure class="wp-block-image w-[20px]">
-              <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="nav-pin">
-            </figure>
-            <!-- /wp:image -->
-            <!-- wp:paragraph -->
-            <p class="underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">Pokaż trasę dojazdu</p>
-            <!-- /wp:paragraph -->
-          </a>
-        </li>
-        <!-- /wp:list-item -->
-      </ul>
-      <!-- /wp:list -->
     </div>
     <!-- /wp:group -->
   </div>
