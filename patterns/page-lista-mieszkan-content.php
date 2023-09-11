4aa71f8c3368eda6b530b47dc2b6e6a1008f6f4e
Tomek <TK-54-4B>
2023-09-11 13:28:52 +0200

    test

---
+++ patterns/page-lista-mieszkan-content.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Title: lista-mieszkan-content
+ * Slug: lista-mieszkan-content
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group content relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[50px] desktop:flex gap-[50px]">
+    <!-- wp:group -->
+    <div class="wp-block-group left-col desktop:w-[50%]">
+      <!-- wp:paragraph -->
+      <p class="mb-[30px] text-[24px] text-[#8a8f99]">Planując zakup nieruchomości najczęściej wspomagamy się kredytem hipotecznym. Po podjęciu decyzji o kupnie mieszkania, przeglądamy ofertę wybierając odpowiadający nam lokal. I co dalej?</p>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group section1 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[10px] font-bold">Oszczędzaj czas</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[30px] text-[#8a8f99] leading-[30px]">
+          <!-- wp:paragraph -->
+          <p>Aby porównać ofertę kredytów w kilkunastu bankach należy poświęcić dużo czasu na przeglądanie stron w internecie, następnie na wizyty w placówkach i porównywanie poszczególnych ofert. Przychodząc na spotkanie z ekspertem kredytowym PB Górski możesz skrócić cały proces do minimum.</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group section2 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[10px] font-bold">Najkorzystniejsze oferty</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[30px] text-[#8a8f99] leading-[30px]">
+          <!-- wp:paragraph -->
+          <p>Ekspert w przystępny sposób przedstawi Ci oferty wiodących banków, wspólnie dokonacie wyboru najkorzystniejszej dla Ciebie opcji, po czym otrzymasz dokładne instrukcje, jakie dokumenty musisz dostarczyć do złożenia wniosku.</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group section3 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[10px] font-bold">Minimum dokumentów</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[30px] text-[#8a8f99] leading-[30px]">
+          <!-- wp:paragraph -->
+          <p>Warto zauważyć, że będą to jedynie dokumenty dotyczące zatrudnienia/zarobków. Całą dokumentację dotyczącą inwestycji dostarczy ekspert, oszczędzając Twój czas. Po pozytywnej decyzji banku możesz udać się na podpisanie umowy kredytowej. Tutaj również będzie towarzyszył Ci nasz przedstawiciel, który odpowie na każde pytanie i rozwieje ewentualne wątpliwości.</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group right-col desktop:w-[50%]">
+      <!-- wp:group -->
+      <div class="wp-block-group section2 mb-[50px]">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-textGray mb-[30px] font-bold">Umów się na spotkanie</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="mb-[10px] text-[24px] text-[#7e878c] font-bold">Ewa Cel</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[25px] phone flex gap-[40px]">
+          <!-- wp:group -->
+          <div class="wp-block-group labels">
+            <!-- wp:paragraph -->
+            <p class="block mb-[20px]">tel.</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="block mb-[30px]">email:</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group values">
+
+            <!-- wp:paragraph -->
+            <a href="tel:+48690099044" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">690 099 044</a>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <a href="mailto:kredyty@pbgorski.pl" class="mb-[30px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">kredyty@pbgorski.pl</a>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
