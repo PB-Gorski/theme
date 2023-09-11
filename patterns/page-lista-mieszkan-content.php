0a4a21646aee4c17cf987c2703740e03bec2a1cb
Tomek <TK-54-4B>
2023-09-11 15:50:54 +0200

    test

--- patterns/page-lista-mieszkan-content.php
+++ patterns/page-lista-mieszkan-content.php
@@ -184,10 +184,10 @@
               <p class="mr-[20px] text-[16px] text-[#2e384d]"><?php the_title(); ?></p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
-              <p class="mr-[20px] text-[16px] text-[#2e384d]"><?php (get_terms(get_the_ID())); ?></p>
+              <p class="mr-[20px] text-[16px] text-[#2e384d]"><?php  ?></p>
               <!-- /wp:paragraph -->
               <!-- wp:paragraph -->
-              <p class="mr-[20px] text-[16px] text-[#2e384d]"><?php (get_taxonomies(get_the_ID())); ?></p>
+              <p class="mr-[20px] text-[16px] text-[#2e384d]"><?php ?></p>
               <!-- /wp:paragraph -->
             </a>
           </li>
