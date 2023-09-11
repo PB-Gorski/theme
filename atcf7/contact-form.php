1b2aa378a519cc2d0b12364cf87f619968bc431c
Tomek <TK-54-4B>
2023-09-11 09:40:31 +0200

    test

--- atcf7/contact-form.php
+++ atcf7/contact-form.php
@@ -11,7 +11,10 @@ if (!defined('ABSPATH')) {
   <tbody>
     <tr>
       <th><label for="your-name" onclick="">Name</label></th>
-      <td> [text* your-name id:your-name ] <input type="text" placeholder="test"></td>
+      <td>
+        [text* your-name id:your-name ]
+        <input id="your-name" name="your-name" type="text" placeholder="test" class="p-[10px_15px] w-[500px] h-[60px] text-[#2e384d]">
+      </td>
     </tr>
     <tr>
       <th><label for="your-email" onclick="">Mail Address</label></th>
