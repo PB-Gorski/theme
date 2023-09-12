627a67d060ffe050b869dd4da70c140e6a775ded
Tomek <TK-54-4B>
2023-09-12 16:56:25 +0200

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -130,7 +130,7 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group bar-info max-w-[800px] desktop:max-w-[1200px] mx-auto h-[120px] bg-[#2e384d] px-[50px] text-white text-[24px] flex items-center justify-between absolute top-[-60px]">
+  <div class="wp-block-group bar-info container mx-auto h-[120px] bg-[#2e384d] px-[50px] text-white text-[24px] flex items-center justify-between absolute bottom-[-60px]">
     <!-- wp:group -->
     <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
       <!-- wp:group -->
