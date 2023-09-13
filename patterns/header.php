c4c55f0c519aca9bd2dd690ce62f5b4a67f507ed
Tomek <TK-54-4B>
2023-09-13 11:27:06 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -126,7 +126,7 @@
                 <!-- /wp:list-item -->
                 <!-- wp:list-item -->
                 <li class="text-textGray mb-[10px] hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-[#e80d38] cursor-pointer">
-                  <a href="<?php echo home_url() . '/lokale' ?>">Lista lokali uytkowych &rarr;</a>
+                  <a href="<?php echo home_url() . '/lokale' ?>">Lista lokali użytkowych &rarr;</a>
                 </li>
                 <!-- /wp:list-item -->
               </ul>
