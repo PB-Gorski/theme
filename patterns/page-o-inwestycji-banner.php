aec8860128cdcb0536ac449d52fb99f8d4fabdb6
Tomek <TK-54-4B>
2023-09-13 17:01:09 +0200

    test

--- patterns/page-o-inwestycji-banner.php
+++ patterns/page-o-inwestycji-banner.php
@@ -100,7 +100,7 @@
   <!-- wp:group -->
   <div class="wp-block-group w-full h-full bg-banner relative flex desktop:block items-center">
     <!-- wp:image -->
-    <figure class="wp-block-image w-full h-full absolute top-0 left-0">
+    <figure class="wp-block-image w-full h-[770px]  absolute top-0 left-0">
       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji/osiedle-srebrniki-bg.jpeg" alt="banner-background">
     </figure>
     <!-- /wp:image -->
@@ -108,7 +108,7 @@
     <!-- wp:group -->
     <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pl-[80px] z-[0] relative text-white">
       <!-- wp:group -->
-      <div class="wp-block-group title text-[36px] desktop:text-[60px] desktop:pt-[150px] mb-[50px] leading-[44px] desktop:leading-[70px]">
+      <div class="wp-block-group title text-[36px] desktop:text-[60px] desktop:pt-[290px] mb-[50px] leading-[44px] desktop:leading-[70px]">
         <!-- wp:paragraph -->
         <p class="underline underline-offset-[20px]">Osiedle Srebrniki (manual)</p>
         <!-- /wp:paragraph -->
