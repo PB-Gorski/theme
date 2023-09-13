f3e61d8bc0ef1a6b0b0716b16a91f4f8bcf19ec8
Tomek <TK-54-4B>
2023-09-13 19:05:46 +0200

    test

--- patterns/page-o-inwestycji-content.php
+++ patterns/page-o-inwestycji-content.php
@@ -13,8 +13,28 @@
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
     <!-- wp:paragraph -->
-    <p class="py-[60px] text-[60px] text-center">Poznaj inwestycję</p>
+    <p class="py-[60px] text-[60px] text-center font-bold">Poznaj inwestycję</p>
     <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group row-links my-[30px] flex gap-[20px]">
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/o-inwestycji' ?>" class="active-o-inwest text-[8a8f99] hover:text-[#3e4759]">O inwestycji</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lokalizacje' ?>" class="text-[8a8f99] hover:text-[#3e4759]">Lokalizacja</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lista-mieszkan' ?>" class="text-[8a8f99] hover:text-[#3e4759]">Lista mieszkań</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/galeria' ?>" class="text-[8a8f99] hover:text-[#3e4759]">Galeria</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/kronika-budowy' ?>" class="text-[8a8f99] hover:text-[#3e4759]">Kronika budowy</a>
+      <!-- /wp:paragraph -->
+
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
