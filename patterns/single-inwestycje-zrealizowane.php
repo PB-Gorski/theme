7627ef03ab395f60bbf63b9fdcecc4a11a70ef74
Tomek <TK-54-4B>
2023-09-13 15:11:55 +0200

    test

--- patterns/single-inwestycje-zrealizowane.php
+++ patterns/single-inwestycje-zrealizowane.php
@@ -18,7 +18,7 @@
       <div class="wp-block-group group w-[56px] h-[56px]">
         <!-- wp:paragraph -->
         <p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
+          <svg width="56" height="56" viewBox="0 0 56 56" fill="none" class="rotate-180 scale-[0.65] fill-transparent group-hover:fill-[#FEC400]">
             <rect x="1.5" y="1.5" width="53" height="53" rx="26.5" stroke="#AAAAAA" stroke-width="3" class="group-hover:fill-[#FEC400] group-hover:stroke-primaryYellow transition-stroke ease-out duration-500" />
             <path d="M28 35L35 28.0189L28.0379 21" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
             <path d="M20 28L31 28" stroke="black" stroke-width="1.5" class="group-hover:stroke-black transition-stroke ease-out duration-500" />
@@ -34,7 +34,7 @@
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph -->
-    <p>single-inwestycje</p>
+    <p>single-inwestycje-zrealizowane.php</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
