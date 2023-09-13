b1d57a8bcc6576672298c3d3f6e4e0e86d7bbd62
Tomek <TK-54-4B>
2023-09-13 15:01:57 +0200

    test

--- patterns/page-inwestycje-zrealizowane.php
+++ patterns/page-inwestycje-zrealizowane.php
@@ -29,11 +29,11 @@
   <!-- wp:group -->
   <div class="wp-block-group title-wrapper bg-[#f6f7fa]">
     <!-- wp:group -->
-    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[20px] desktop:flex items-center">
+    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[60px] desktop:flex items-center">
       <!-- wp:group -->
       <div class="wp-block-group left-col desktop:w-[70%]">
         <!-- wp:paragraph -->
-        <p class="container mx-auto desktop:px-0 px-[20px] mb-[40px] text-[36px] desktop:text-[60px] text-[#2e384c] font-bold leading-[44px]">Inwestycje zrealizowane</p>
+        <p class="container mx-auto desktop:px-0 px-[20px] text-[36px] desktop:text-[60px] text-[#2e384c] font-bold leading-[44px]">Inwestycje zrealizowane</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
