f6d7a001638409565aa63c8cc507948ee7f9b3c7
Tomek <TK-54-4B>
2023-09-13 16:47:37 +0200

    test

--- patterns/page-o-inwestycji-banner.php
+++ patterns/page-o-inwestycji-banner.php
@@ -102,7 +102,7 @@ o inwestycji.php
   <div class="wp-block-group w-full h-full bg-banner relative flex desktop:block items-center">
     <!-- wp:image -->
     <figure class="wp-block-image w-full h-full absolute top-0 left-0">
-      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/banner.jpeg" alt="banner-background">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji/osiedle-srebrniki-bg.jpeg" alt="banner-background">
     </figure>
     <!-- /wp:image -->
 
