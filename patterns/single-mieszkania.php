2933e2f0b2cc46ed55bd2a014355c46b54373f4a
Tomek <TK-54-4B>
2023-09-13 09:28:56 +0200

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -15,7 +15,7 @@
     <!-- wp:group -->
     <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_20px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
       <!-- wp:image -->
-      <figure class="wp-block-image">
+      <figure class="wp-block-image m-0">
         <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
       </figure>
       <!-- /wp:image -->
