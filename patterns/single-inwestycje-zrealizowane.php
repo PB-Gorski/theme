7038ec3e0e33f1cd645d0a5001f0a90fb5880ea8
Tomek <TK-54-4B>
2023-09-13 15:43:12 +0200

    test

--- patterns/single-inwestycje-zrealizowane.php
+++ patterns/single-inwestycje-zrealizowane.php
@@ -127,11 +127,66 @@
     <p>single-inwestycje-zrealizowane.php</p>
     <!-- /wp:paragraph -->
 
-
-
     <!-- wp:post-featured-image /-->
 
     <!-- wp:post-content /-->
+
+    <!-- wp:list -->
+    <ul class="wp-block-list mb-[40px] flex gap-[30px]">
+      <?php
+      $args = array(
+        'post_type' => 'inwest-zrealizowane',
+        'posts_per_page' => 2,
+        'order' => 'ASC'
+      );
+      $post_query = new WP_Query($args);
+      $counter = 0;
+      $i = 0;
+
+      if ($post_query->have_posts()) {
+        while ($post_query->have_posts()) {
+          $post_query->the_post();
+          $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
+          $counter++; ?>
+          <!-- wp:list-item -->
+          <li class="invest-tile group w-full mb-[80px]">
+            <a href="<?php the_permalink(); ?>" class="relative group p-[25px] desktop:max-w-full max-w-[310px] h-full" data-aos="fade-up" data-aos-duration="300" data-aos-offset="30">
+              <!-- wp:group -->
+              <div class="wp-block-group wrapper w-full  <?php echo (!($counter % 2)) ? 'flex-row-reverse' : ''; ?> gap-[50px]">
+                <!-- wp:image -->
+                <figure class="wp-block-image w-full mb-[30px] h-[330px]">
+                  <img src="<?php echo $postImageUrl[0]; ?>" alt="services-background-image">
+                </figure>
+                <!-- /wp:image -->
+
+                <!-- wp:paragraph -->
+                <p class="mr-[20px] text-[18px] desktop:text-[24px] font-bold"><?php the_title(); ?></p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p class="relative text-[18px] desktop:text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] group-hover:before:min-h-[100%] before:bg-[#b3f1ff] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Zobacz więcej &rarr;</p>
+                <!-- /wp:paragraph -->
+              </div>
+              <!-- /wp:group -->
+            </a>
+          </li>
+          <!-- /wp:list-item -->
+        <?php
+          $i++;
+        };
+        wp_reset_query(); ?>
+        <!-- wp:query-pagination -->
+        <div class="wp-block-query-pagination">
+          <!-- wp:query-pagination-previous /-->
+          <!-- wp:query-pagination-next /-->
+        </div>
+        <!-- /wp:query-pagination -->
+      <?php
+      };
+
+      ?>
+    </ul>
+    <!-- /wp:list -->
 
 
 
