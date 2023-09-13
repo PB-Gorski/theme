210a7b31fc0b6c68796bdf3a94ffa7b12ae99551
Tomek <TK-54-4B>
2023-09-13 18:48:27 +0200

    test

---
+++ patterns/page-o-inwestycji-content.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Title: o-inwestycji-content
+ * Slug: o-inwestycji-content
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="Content" class="wp-block-group content h-[80vh] desktop:h-[90vh] relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:paragraph -->
+    <p class="py-[60px] text-[60px] text-center">Poznaj inwestycję</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
