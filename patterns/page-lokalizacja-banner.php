3c0bbab290c41d754266ec97b2c49854e5a518cd
Tomek <TK-54-4B>
2023-09-14 12:42:22 +0200

    test

--- patterns/page-lokalizacja-banner.php
+++ patterns/page-lokalizacja-banner.php
@@ -100,7 +100,7 @@
   <!-- wp:group -->
   <div class="wp-block-group w-full h-full bg-banner relative flex desktop:block items-center">
     <!-- wp:image -->
-    <figure class="wp-block-image w-full h-[770px]  absolute top-0 left-0">
+    <figure class="wp-block-image w-full h-[250px] desktop:h-[770px] absolute top-0 left-0">
       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji/osiedle-srebrniki-bg.jpeg" alt="banner-background">
     </figure>
     <!-- /wp:image -->
