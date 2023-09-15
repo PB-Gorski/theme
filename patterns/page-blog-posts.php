4b9f6091db714c8efc5ecfe6c51c0904f13b307b
Tomek <TK-54-4B>
2023-09-15 16:16:14 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -120,7 +120,7 @@
                         <p><?php echo $category->name; ?></p>
                         <!-- /wp:paragraph -->
                         <!-- wp:paragraph -->
-                        <p class="relative text-[18px] desktop:text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-[#b3f1ff] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Zobacz więcej &rarr;
+                        <p class="relative text-[18px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] hover:before:min-h-[100%] before:bg-[#b3f1ff] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Zobacz więcej &rarr;
                         </p>
                         <!-- /wp:paragraph -->
                       </li>
