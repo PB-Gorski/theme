d9cf8ee5e0f23cea454ca23cca245d4b8e277023
Tomek <TK-54-4B>
2023-09-15 16:50:51 +0200

    test

--- patterns/single-post-content.php
+++ patterns/single-post-content.php
@@ -133,6 +133,7 @@
       <div class="wp-block-group post-date text-[#8a8f99] text-[18px]">
         <!-- wp:post-date /-->
         <?php the_date('j F Y'); ?>
+        <?php the_time('j F Y'); ?>
         <?php echo get_the_date('j F Y'); ?>
       </div>
       <!-- /wp:group -->
