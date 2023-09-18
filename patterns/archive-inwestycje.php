9835b992e7140f7e836ea86932e88c1db276cacc
Tomek <TK-54-4B>
2023-09-18 13:47:15 +0200

    test

--- patterns/archive-inwestycje.php
+++ patterns/archive-inwestycje.php
@@ -54,7 +54,7 @@
       <!-- wp:group -->
       <div class="wp-block-group right-col desktop:w-[30%]">
         <!-- wp:paragraph -->
-        <p class="text-[30px] text-[#8a8f99]"><?php echo wp_count_terms('inwestycja'); ?> inwestycje / <?php echo wp_count_posts('mieszkania')->publish; ?> lokali</p>
+        <p class="text-[30px] text-[#8a8f99]" data-aos="fade-right" data-aos-duration="500" data-aos-offset="30"><?php echo wp_count_terms('inwestycja'); ?> inwestycje / <?php echo wp_count_posts('mieszkania')->publish; ?> lokali</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
