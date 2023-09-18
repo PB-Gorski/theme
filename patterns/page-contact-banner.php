43305b9758add4cdefe91a27aeb6236cd6c282b7
Tomek <TK-54-4B>
2023-09-18 15:59:11 +0200

    test

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -29,24 +29,26 @@
       <!-- wp:group -->
       <div class="wp-block-group text-[#959ba6] test text-[30px]">
         <!-- wp:paragraph -->
-        <a href="mailto:sprzedaz@pbgorski.pl" class="inline underline relative hover:text-[#2e384d] transition duration-[300ms] ease after:content-[''] after:absolute after:top-[-10px] after:right-[-20px] after:w-[2px] after:h-[70px] after:m-auto after:bg-[#e1e3e8] after:rotate-[25deg]" class="inline-block" data-aos="fade-right" data-aos-offset="30">sprzedaz@pbgorski.pl</a>
+        <p>
+          <a href="mailto:sprzedaz@pbgorski.pl" class="inline-block underline relative hover:text-[#2e384d] transition duration-[300ms] ease after:content-[''] after:absolute after:top-[-10px] after:right-[-20px] after:w-[2px] after:h-[70px] after:m-auto after:bg-[#e1e3e8] after:rotate-[25deg]" class="inline-block" data-aos="fade-right" data-aos-offset="30">sprzedaz@pbgorski.pl</a>
+        </p>
         <!-- /wp:paragraph -->
 
         <!-- wp:paragraph -->
         <div class="wp-block-group links-phones mt-[15px]">
           <!-- wp:paragraph -->
           <p>
-            <a href="tel:+48583407900" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">(+48) 58 340 79 00, </a>
+            <a href="tel:+48583407900" class="inline-block underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">(+48) 58 340 79 00, </a>
           </p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <p>
-            <a href="tel:+48607700555" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">607 700 555, </a>
+            <a href="tel:+48607700555" class="inline-block underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">607 700 555, </a>
           </p>
           <!-- /wp:paragraph -->
           <p>
             <!-- wp:paragraph -->
-            <a href="tel:+48691990112" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">691 990 112</a>
+            <a href="tel:+48691990112" class="inline-block underline hover:text-[#2e384d] transition duration-[300ms] ease" class="inline-block" data-aos="fade-right" data-aos-offset="30">691 990 112</a>
           </p>
           <!-- /wp:paragraph -->
         </div>
