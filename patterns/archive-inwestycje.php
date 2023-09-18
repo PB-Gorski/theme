f7f2e5b39fefcf224136ed40e259671cfa3a4094
Tomek <TK-54-4B>
2023-09-18 13:39:51 +0200

    TEST

--- patterns/archive-inwestycje.php
+++ patterns/archive-inwestycje.php
@@ -104,7 +104,7 @@
       ?>
         <!-- wp:list-item -->
         <li class="invest-tile max-w-full test desktop:h-[450px] mb-[80px]">
-          <div class="relative group desktop:max-w-full max-w-[310px] h-full">
+          <div class="relative group desktop:max-w-full max-w-[310px] h-full" data-aos="fade-up" data-aos-duration="500" data-aos-offset="30">
             <!-- wp:group -->
             <div class="wp-block-group wrapper w-full h-full desktop:flex <?php echo (!($counter % 2)) ? 'flex-row-reverse' : ''; ?> gap-[50px]">
               <!-- wp:image -->
