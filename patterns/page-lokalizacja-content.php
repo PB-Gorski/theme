770078033db9e094a1dee46f81ab65c637ced3d6
Tomek <TK-54-4B>
2023-09-18 18:24:39 +0200

    test

--- patterns/page-lokalizacja-content.php
+++ patterns/page-lokalizacja-content.php
@@ -64,7 +64,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   <!-- wp:group -->
   <div class="wp-block-group tiles-numbers px-[20px] mb-[100px] flex flex-col desktop:flex-row justify-center gap-[30px]">
     <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="wp-block-group tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-right" data-aos-offset="30" data-aos-delay="50">
+    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-right" data-aos-offset="30" data-aos-delay="50">
       <!-- wp:group -->
       <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#e90d38] rounded-full flex flex-col items-center justify-center">
         <!-- wp:paragraph -->
