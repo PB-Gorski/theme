9aeb29948ab4d1d459922c2df78dae9c8f7a80c0
Tomek <TK-54-4B>
2023-09-18 15:12:00 +0200

    test

--- patterns/page-about-row-content.php
+++ patterns/page-about-row-content.php
@@ -66,20 +66,24 @@
     <div class="wp-block-group row3 desktop:flex flex-row-reverse gap-[20px] mb-[80px]">
       <!-- wp:group -->
       <div class="wp-block-group col1 desktop:w-[60%] desktop:pl-[60px]">
-        <!-- wp:image -->
-        <figure class="wp-block-image w-full h-full">
-          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/img2.jpeg" alt="img">
-        </figure>
-        <!-- /wp:image -->
+        <!-- wp:paragraph -->
+        <a href="#" class="cursor-default inline-block" data-aos="fade-right" data-aos-offset="30">
+          <!-- wp:image -->
+          <figure class="wp-block-image w-full h-full">
+            <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/img2.jpeg" alt="img">
+          </figure>
+          <!-- /wp:image -->
+        </a>
+        <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:group -->
       <div class="wp-block-group col2 desktop:w-[40%] text-[18px] text-bgDarkGray leading-[30px]">
         <!-- wp:paragraph -->
-        <p class="mb-[30px] text-[30px] text-textGray leading-[44px]">Realizacje PB Górski to nie tylko mieszkania</p>
+        <p class="mb-[30px] text-[30px] text-textGray leading-[44px]" data-aos="fade-right" data-aos-offset="30">Realizacje PB Górski to nie tylko mieszkania</p>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="text-[18px] text-bgDarkGray leading-[30px]">Deweloper w ostatnim czasie postawił także na inwestycje handlowo-usługowe. Galeria Metropolia to unikalne centrum handlowe zintegrowane z dworcem kolejowym Gdańsk Wrzeszcz, gdzie do dyspozycji klientów oddane zostały różnorodne sklepy znanych marek, największy kompleks kinowy w okolicy, a także hotel.</p>
+        <p class="text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Deweloper w ostatnim czasie postawił także na inwestycje handlowo-usługowe. Galeria Metropolia to unikalne centrum handlowe zintegrowane z dworcem kolejowym Gdańsk Wrzeszcz, gdzie do dyspozycji klientów oddane zostały różnorodne sklepy znanych marek, największy kompleks kinowy w okolicy, a także hotel.</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
