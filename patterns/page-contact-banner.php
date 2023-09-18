5e916f6a5ad9710f36330e97df3b3a7141bb75a6
Tomek <TK-54-4B>
2023-09-18 11:07:00 +0200

    test

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -33,16 +33,9 @@
         <!-- /wp:paragraph -->
 
         <!-- wp:paragraph -->
-        <p class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease">paragraph1 (+48) 58 340 79 00, </p>
-        <!-- /wp:paragraph -->
-
-        <!-- wp:paragraph -->
         <div class="wp-block-group links-phones mt-[15px]">
           <!-- wp:paragraph -->
           <a href="tel:+48583407900" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease">(+48) 58 340 79 00, </a>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease">paragraph2 (+48) 58 340 79 00, </p>
           <!-- /wp:paragraph -->
           <!-- wp:paragraph -->
           <a href="tel:+48607700555" class="inline underline hover:text-[#2e384d] transition duration-[300ms] ease">607 700 555, </a>
