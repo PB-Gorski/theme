5b574a0b50aead9b226020e467dd5aaa374e0e24
Tomek <TK-54-4B>
2023-09-19 08:52:06 +0200

    test

--- patterns/post-count.php
+++ patterns/post-count.php
@@ -9,5 +9,5 @@
 ?>
 
 <!-- wp:paragraph -->
-<p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo wp_count_posts('mieszkania')->publish; ?> ofert pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
+<p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Z2naleziono <?php echo wp_count_posts('mieszkania')->publish; ?> ofert pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
 <!-- /wp:paragraph -->
