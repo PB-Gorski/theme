395b4d697c0ba70f9d063d3c2c718ecc7434d4be
Tomek <TK-54-4B>
2023-09-19 08:49:13 +0200

    test

---
+++ patterns/post-count.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Title: post-count
+ * Slug: post-count
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:paragraph -->
+<p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono X ofert pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń X)</span></p>
+<!-- /wp:paragraph -->
