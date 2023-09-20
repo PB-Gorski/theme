da01067645ec747c4061af850a38d5b4748217fd
Tomek <TK-54-4B>
2023-09-20 15:56:16 +0200

    test

--- patterns/post-count.php
+++ patterns/post-count.php
@@ -27,6 +27,8 @@ $count = $query->found_posts;
 modify_show_count(true, 'mieszkania');
 
 ?>
+<?php do_action('show_beautiful_filters', 'mieszkania'); ?>
+<?php do_action('modify_show_count', 'mieszkania'); ?>
 
 <!-- wp:paragraph -->
 <p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo $count - 1; ?> oferty pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
