9be4188721e96dcbac37b23a34c6a8256e0262c8
Tomek <TK-54-4B>
2023-09-20 11:50:20 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -403,15 +403,6 @@ window.addEventListener("load", function() {
           console.log('not current: ', i);
         }else{
           console.log('current: ', i);
-          if(btn.classList.contains('sort-arrow-up')){
-            console.log('should remove up');
-            btn.classList.remove('sort-arrow-up');
-            btn.classList.add('sort-arrow-down');
-          }else if( btn.classList.contains('sort-arrow-down')){
-            console.log('should remove down');
-            btn.classList.remove('sort-arrow-down');
-            btn.classList.add('sort-arrow-up');
-          }        
         };
         // btnsSort[i].classList.remove('sort-arrow-up', 'sort-arrow-down');
         console.log('--------------------------');
