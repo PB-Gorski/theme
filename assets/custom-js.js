b7d14e7605d890681b6675771f535e22b3472b79
Tomek <TK-54-4B>
2023-09-20 10:56:29 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -388,11 +388,11 @@ window.addEventListener("load", function() {
       // };
       indexFrom1 = index + 1;
       indexFrom1 == 1 ? sortListDir(indexFrom1) : sortListDir(indexFrom1 + index);
-      console.log(indexFrom1);
 
       console.log('before loop: ', indexFrom1 + index);
       for (let i = 0 ; i < (indexFrom1 + index) ; i++){
-        console.log('i: ', i, '/', indexFrom1 + index);
+        console.log('i: ', i);
+        console.log('index: ', index);
         console.log('indexFrom1: : ',indexFrom1);
         if(i != indexFrom1 + index){
           console.log( btnsSort[indexFrom1 + index]);
