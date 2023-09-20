992e436a4b270e2f1d77bae5574419cde9f417c2
Tomek <TK-54-4B>
2023-09-20 11:09:46 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -395,7 +395,7 @@ window.addEventListener("load", function() {
         console.log('index: ', index);
         console.log('indexFrom1: : ',indexFrom1);
         if(i != indexFrom1 + index){
-          console.log( btnsSort[indexFrom1 + i]);
+          console.log( btnsSort[i]);
           btnsSort[indexFrom1] ? btnsSort[indexFrom1].classList.remove('sort-arrow-up', 'sort-arrow-down') : null;
           console.log('i != clicked: ', i);
         };
