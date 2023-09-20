7de3a3c4a88c48a91fa43f44244184ab4a58ade5
Tomek <TK-54-4B>
2023-09-20 13:08:14 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -375,7 +375,7 @@ window.addEventListener("load", function() {
   const btnsSort = document.querySelectorAll('.js-sort');
   btnsSort.forEach((btn,index) => {
     btn.addEventListener('click', (e) => {
-      console.log(this.target);
+      console.log('e.taget: ',e.target);
       console.log(target);
       // indexFrom1 = index + 1;
       // indexFrom1 == 1 ? sortListDir(indexFrom1) : sortListDir(indexFrom1 + index);
