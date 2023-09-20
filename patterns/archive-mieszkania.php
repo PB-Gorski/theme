d3937cdd78444a2e5d286aaddf4f2803fdcc4443
Tomek <TK-54-4B>
2023-09-20 16:36:33 +0200

    test

--- patterns/archive-mieszkania.php
+++ patterns/archive-mieszkania.php
@@ -8,6 +8,23 @@
  */
 ?>
 
+<form action="#" method="post">
+  <select name="Color">
+    <option value="Red">Red</option>
+    <option value="Green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Pink">Pink</option>
+    <option value="Yellow">Yellow</option>
+  </select>
+  <input type="submit" name="submit" value="Get Selected Values" />
+</form>
+<?php
+if (isset($_POST['submit'])) {
+  $selected_val = $_POST['Color'];  // Storing Selected Value In Variable
+  echo "You have selected :" . $selected_val;  // Displaying Selected Value
+}
+?>
+
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="content" class="wp-block-group content relative ">
   <!-- wp:group -->
