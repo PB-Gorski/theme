93550a822b660541e6cb1d031ebef92301791779
Tomek <TK-54-4B>
2023-09-20 16:34:57 +0200

    test

--- patterns/post-count.php
+++ patterns/post-count.php
@@ -36,6 +36,23 @@ if (!$query->have_posts()) {
 
 ?>
 
+<form action="#" method="post">
+  <select name="Color">
+    <option value="Red">Red</option>
+    <option value="Green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Pink">Pink</option>
+    <option value="Yellow">Yellow</option>
+  </select>
+  <input type="submit" name="submit" value="Get Selected Values" />
+</form>
+<?php
+if (isset($_POST['submit'])) {
+  $selected_val = $_POST['Color'];  // Storing Selected Value In Variable
+  echo "You have selected :" . $selected_val;  // Displaying Selected Value
+}
+?>
+
 <!-- wp:paragraph -->
 <p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo $count - 1; ?> oferty pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
 <!-- /wp:paragraph -->
