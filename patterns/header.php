f90d8fffabfe9f9b67747c4256b22b77b483aa90
Tomek <TK-54-4B>
2023-09-21 16:11:04 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -111,7 +111,7 @@
                 ?>
                   <!-- wp:list-item -->
                   <li class="hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-[#e80d38] cursor-pointer">
-                    <a href="<?php echo home_url() . '/o-inwestycji' . ''; ?>" class="relative group inline-block cursor-default desktop:max-w-full w-full h-full">
+                    <a href="<?php echo home_url() . '/o-inwestycji' . ''; ?>" class="inline-block">
                       <!-- wp:paragraph -->
                       <p class=""><?php echo $tax->name; ?></p>
                       <!-- /wp:paragraph -->
