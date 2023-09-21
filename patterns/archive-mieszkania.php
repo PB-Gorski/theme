be3ed395334bc79cce75dffc025ec22f6d3eed3e
Tomek <TK-54-4B>
2023-09-21 08:25:25 +0200

    test

--- patterns/archive-mieszkania.php
+++ patterns/archive-mieszkania.php
@@ -110,7 +110,7 @@
       // echo $selected_val2  == true ? $selected_val2 : 'brak2';;
 
       // $taxonomy = $selected_val1 == true ?  $selected_val1 : 'miasto';
-      $term = $selected_val2 == true ? $selected_val2 : 'gdynia';
+      // $term = $selected_val2 == true ? $selected_val2 : 'gdynia';
 
       $query = new WP_Query(array(
         'miasto' => $term,
