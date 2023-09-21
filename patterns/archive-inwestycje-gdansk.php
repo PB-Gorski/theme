94a4d3aa12de205e9088625e423e46776d03a7a1
Tomek <TK-54-4B>
2023-09-21 14:35:23 +0200

    test

--- patterns/archive-inwestycje-gdansk.php
+++ patterns/archive-inwestycje-gdansk.php
@@ -29,7 +29,7 @@
             'paged' => 1,
             'order'   => 'ASC'
           );
-          $currentCategory = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 39, 20));
+          $currentCategory = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 42, 20));
           $cats = get_categories($args); ?>
           <!-- wp:list-item -->
           <li class="allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
@@ -37,7 +37,7 @@
           </li>
           <!-- /wp:list-item -->
           <?php
-          $currentCategory = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 39, 20));
+          $currentCategory = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 42, 20));
           foreach ($cats as $cat) {
             $catNoSpaces = str_replace(' ', '-', strtolower($cat->name));
             echo $catNoSpaces;
