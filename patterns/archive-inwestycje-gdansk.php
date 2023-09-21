d332290c67275cf4101e414f1301b63b1cb2c2bf
Tomek <TK-54-4B>
2023-09-21 10:57:59 +0200

    test

--- patterns/archive-inwestycje-gdansk.php
+++ patterns/archive-inwestycje-gdansk.php
@@ -97,7 +97,7 @@ $count = $query->found_posts;
 
 
     <!-- wp:paragraph -->
-    <p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo $count; ?> oferty pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
+    <p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo $count - 1; ?> oferty pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:list -->
