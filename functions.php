0a5685f643dc57972989c2f17fbbea3de4af2799
Tomek <TK-54-4B>
2023-09-22 16:44:01 +0200

    test

--- functions.php
+++ functions.php
@@ -571,7 +571,6 @@ add_filter('ymc_filter_custom_layout_148_1', 'my_custom_filter_layout', 10, 6);
 function my_custom_post_layout($layout, $post_id, $filter_id, $increment_post, $arrOptions)
 {
 	$layout  = '<h2>' . get_the_title($post_id) . '</h2>';
-	$layout .= '<p>' . wp_trim_words(get_the_content($post_id), 30) . '</p>';
 	$layout .= '<a href="' . get_the_permalink($post_id) . '">-></a>';
 	$layout .= '<p' . print_r(get_the_term_list($post_id, 'miasto')) . '>-></p>';
 	$layout .= '<p' . print_r(get_the_term_list($post_id, 'inwestycja')) . '>-></p>';
