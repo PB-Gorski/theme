09de10fec97ee9aacc0960638e65d7f002132a26
Tomek <TK-54-4B>
2023-10-11 22:03:28 +0200

    test

--- patterns/blog.php
+++ patterns/blog.php
@@ -26,7 +26,7 @@
       $args = array(
         'post_type' => 'post',
         'posts_per_page' => 4,
-        'order' => 'ASC'
+        'order' => 'DESC'
       );
       $post_query = new WP_Query($args);
       $counter2 = 0;
