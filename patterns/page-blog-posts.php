d55a4bd25b86f3e071b0d8cc7e45425d1328a96b
Tomek <TK-54-4B>
2023-10-11 11:51:54 +0200

    test

--- patterns/page-blog-posts.php
+++ patterns/page-blog-posts.php
@@ -218,7 +218,16 @@
         }; ?>
         <div class="nav-previous alignleft"><?php previous_posts_link('Older posts'); ?></div>
         <div class="nav-next alignright"><?php next_posts_link('Newer posts'); ?></div>
-      <?php };
+      <?php
+        posts_nav_link();
+        next_posts_link();
+        previous_posts_link();
+        get_next_posts_link();
+        get_previous_posts_link();
+        paginate_links();
+        the_posts_pagination();
+        get_the_posts_pagination();
+      };
       wp_reset_query();
       ?>
     </ul>
