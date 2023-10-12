78727b864731780e3730117327c12cdc217116b6
Tomek <TK-54-4B>
2023-10-12 12:53:05 +0200

    test

--- patterns/page-lokalizacja-sw-piotra-banner.php
+++ patterns/page-lokalizacja-sw-piotra-banner.php
@@ -110,10 +110,7 @@
       <!-- wp:group -->
       <div class="wp-block-group title text-[36px] desktop:text-[60px] desktop:pt-[290px] mb-[50px] leading-[44px] desktop:leading-[70px]">
         <!-- wp:paragraph -->
-        <p class="font-bold underline underline-offset-[20px]" data-aos="fade-right" data-aos-offset="30">Lokalizacja</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="uppercase text-[18px] trancking-[10px]" data-aos="fade-right" data-aos-offset="30">Lorem ipsum</p>
+        <p class="font-bold" data-aos="fade-right" data-aos-offset="30">Gdynia - Św. Piotra</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
