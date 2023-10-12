6f8f15c9cf54cbc72f0fa328e5d62e905006071a
Tomek <TK-54-4B>
2023-10-12 13:13:45 +0200

    test

--- patterns/page-lokalizacja-sw-piotra-content.php
+++ patterns/page-lokalizacja-sw-piotra-content.php
@@ -121,5 +121,13 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group plan-osiedla-wrapper mb-[100px] mx-auto max-w-[690px] w-full px-[20px] desktop:px-0">
+    <!-- wp:paragraph -->
+    <p class="mb-[60px] text-[24px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">Zaledwie 5 minut będzie dzielić mieszkańców Św. Piotra od Skweru Kościuszki, plaży miejskiej czy Mariny Gdyńskiej. Św. Piotra to także idealne miejsce dla miłośników miejskiego życia - w najbliższej okolicy znajduje się zróżnicowana oferta kawiarni, restauracji i barów, a także kluby fitness, kina i teatry.</p>
+    <!-- /wp:paragraph -->
+  </div>
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
