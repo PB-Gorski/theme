2e70db29f937eade641e459baef432c29452ca3f
Tomek <TK-54-4B>
2023-10-12 16:06:55 +0200

    test

---
+++ patterns/page-o-inwestycji-wiezycka-folwark-content.php
@@ -0,0 +1,199 @@
+<?php
+
+/**
+ * Title: o-inwestycji-wiezycka-folwark-content
+ * Slug: o-inwestycji-wiezycka-folwark-content
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+$currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", 35, 30));
+
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="Content" class="wp-block-group content relative">
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:paragraph -->
+    <p class="py-[30px] desktop:py-0 desktop:pt-[60px] text-[36px] desktop:text-[60px] text-center font-bold" data-aos="fade-right" data-aos-offset="30">Wieżycka Folwark</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="pb-[30px] desktop:pb-[60px] text-[20px] text-[#8a8f99] desktop:text-[24px] text-center" data-aos="fade-right" data-aos-offset="30">Wyjątkowe osiedle zlokalizowane w południowej części Gdańska, w sąsiedztwie zabytkowego dworku Wendtów</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group test row-links mb-[50px] flex flex-wrap justify-center items-center gap-[20px]">
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/o-inwestycji-wiezycka-folwark' ?>" data-aos="fade-up" data-aos-offset="30">O inwestycji</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lokalizacja-wiezycka-folwark' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lokalizacja</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lista-mieszkan' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lista mieszkań</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/galeria-lokalizacja' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Galeria</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/kronika-budowy' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Kronika budowy</a>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group row-content-container px-[20px] desktop:px-0">
+    <!-- wp:group -->
+    <div class="wp-block-group row-content desktop:flex">
+      <!-- wp:paragraph -->
+      <a href="javascript:void(0)" class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+        <!-- wp:image -->
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-wiezycka-folwark/img1.jpeg" alt="img">
+        </figure>
+        <!-- /wp:image -->
+      </a>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:px-[100px] desktop:w-1/2 flex flex-col justify-center">
+        <!-- wp:paragraph -->
+        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">W ramach inwestycji powstaną 2 kameralne budynki</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group mb-[100px] desktop:mb-0 desktop:grid grid-rows-2 grid-cols-2 gap-[30px]">
+          <!-- wp:group -->
+          <div class="wp-block-group info-box1 mb-[35px] desktop:mb-[30px]">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LOKALIZACJA</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">Gdańsk Śródmieście</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group info-box2 mb-[35px] desktop:mb-[30px]">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">TERMIN UKOŃCZENIA (BUDYNEK 1)</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">II kwartał 2024</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group info-box3 mb-[35px] desktop:mb-[30px]">
+            <!-- wp:paragraph -->
+
+            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LICZBA MIESZKAŃ W SPRZEDAŻY</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">29</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group info-box4 mb-[35px] desktop:mb-[30px]">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">POWIERZCHNIA MIESZKAŃ</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">31 m<sup>2</sup> - 97 m<sup>2</sup></p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group row-content2 desktop:flex flex-row-reverse">
+      <!-- wp:paragraph -->
+      <a href="javascript:void(0)" class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+        <!-- wp:image -->
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-wiezycka-folwark/img2.jpg" alt="img">
+        </figure>
+        <!-- /wp:image -->
+      </a>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2 flex flex-col justify-center">
+        <!-- wp:paragraph -->
+        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Apartamenty w pobliżu Motławy</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-[18px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">W dwóch budynkach będzie jedynie 29 apartamentów, co gwarantuje prywatność oraz spokój. Mieszkania charakteryzuje optymalny rozkład pokoi, umożliwiający dostowanie przestrzeni do swoich potrzeb. Przestronne balkony, winda, podziemny garaż - to wszystko czeka na mieszkańców Toruńskiej 16, zlokalizowanej w pobliżu Motławy. </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group row-content3 desktop:flex">
+      <!-- wp:paragraph -->
+      <a href="javascript:void(0)" class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+        <!-- wp:image -->
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-wiezycka-folwark/img3.jpeg" alt="img">
+        </figure>
+        <!-- /wp:image -->
+      </a>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2">
+        <!-- wp:paragraph -->
+        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Zamieszkaj w centrum Gdańska</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Toruńska 16 to miejsce idealne dla osób ceniących mieszkanie w centrum miasta. Spacerem w kilkanaście minut dojdziemy do Głównego Miasta, przystanku SKM czy galerii handlowej. Wszędzie będzie blisko!</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:group -->
+    <div class="wp-block-group row-content4 desktop:flex flex-row-reverse">
+      <!-- wp:paragraph -->
+      <a href="javascript:void(0)" class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+        <!-- wp:image -->
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-wiezycka-folwark/img4.jpeg" alt="img">
+        </figure>
+        <!-- /wp:image -->
+      </a>
+      <!-- /wp:paragraph -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2">
+        <!-- wp:paragraph -->
+        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Sprawdź dostępne mieszkania</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Znajdź odpowiednie mieszkanie już teraz - wybierz dogodny metraż dla siebie i rodziny.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
