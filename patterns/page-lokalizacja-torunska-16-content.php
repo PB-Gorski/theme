4e744a5f57d8795b9cb24726a79d0718e1b4838d
Tomek <TK-54-4B>
2023-10-12 15:56:04 +0200

    test

--- patterns/page-lokalizacja-torunska-16-content.php
+++ patterns/page-lokalizacja-torunska-16-content.php
@@ -64,12 +64,12 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#e90d38] rounded-full flex flex-col items-center justify-center">
         <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">1200 m</p>
+        <p class="text-[30px] text-white font-semibold">1000 m</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Plaża miejska</p>
+      <p class="text-[#8a8f99] text-center">Główne Miasto</p>
       <!-- /wp:paragraph -->
     </a>
     <!-- /wp:paragraph -->
@@ -78,21 +78,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#b11a3e] rounded-full flex flex-col items-center justify-center">
         <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">290 m</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Sklep spożywczy</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="150">
-      <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#7d2645] rounded-full flex flex-col items-center justify-center">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">1 km</p>
+        <p class="text-[30px] text-white font-semibold">650 m</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -102,16 +88,30 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     </a>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="200">
+    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="150">
       <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#52304a] rounded-full flex flex-col items-center justify-center">
+      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#7d2645] rounded-full flex flex-col items-center justify-center">
         <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">300 m</p>
+        <p class="text-[30px] text-white font-semibold">1300 m</p>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
       <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Apteka</p>
+      <p class="text-[#8a8f99] text-center">Galeria handlowa</p>
+      <!-- /wp:paragraph -->
+    </a>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="200">
+      <!-- wp:group -->
+      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#52304a] rounded-full flex flex-col items-center justify-center">
+        <!-- wp:paragraph -->
+        <p class="text-[30px] text-white font-semibold">20 min</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p class="text-[#8a8f99] text-center">Lotnisko</p>
       <!-- /wp:paragraph -->
     </a>
     <!-- /wp:paragraph -->
@@ -121,7 +121,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   <!-- wp:group -->
   <div class="wp-block-group plan-osiedla-wrapper mb-[100px] mx-auto max-w-[690px] w-full px-[20px] desktop:px-0">
     <!-- wp:paragraph -->
-    <p class="mb-[60px] text-[24px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">Zaledwie 5 minut będzie dzielić mieszkańców Św. Piotra od Skweru Kościuszki, plaży miejskiej czy Mariny Gdyńskiej. Św. Piotra to także idealne miejsce dla miłośników miejskiego życia - w najbliższej okolicy znajduje się zróżnicowana oferta kawiarni, restauracji i barów, a także kluby fitness, kina i teatry.</p>
+    <p class="mb-[60px] text-[24px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">Zaledwie 15 minut spacerem będzie dzielić mieszkańców Toruńskiej 16 od ścisłego centrum Gdańska. Położenie zapewnia dostęp do pełnej infrastruktury w pobliżu miejsca zamieszkania (sklepy, punkty usługowe, szkoły, przedszkola, przychodnia).</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[60px] text-[24px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">Zróżnicowana oferta kulturalna będzie w zasięgu ręki.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
