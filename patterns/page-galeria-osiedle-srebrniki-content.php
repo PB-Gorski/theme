c0cc1bc10edae3b2cb17429013849330b2effbdb
Tomek <TK-54-4B>
2023-10-13 17:42:42 +0200

    test

--- patterns/page-galeria-osiedle-srebrniki-content.php
+++ patterns/page-galeria-osiedle-srebrniki-content.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Title: galeria-lokalizacja-content
- * Slug: galeria-lokalizacja-content
+ * Title: galeria-osiedle-srebrniki-content
+ * Slug: galeria-osiedle-srebrniki-content
  * Categories: Banner, Front Page
  * Viewport Width: 1280
  */
@@ -17,16 +17,16 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- wp:group -->
     <div class="wp-block-group row-links my-[30px] flex flex-wrap justify-center items-center gap-[20px]">
       <!-- wp:paragraph -->
-      <a href="<?php echo home_url() . '/o-inwestycji' ?>" class="text-[#8a8f99] hover:text-[#3e4759]">O inwestycji</a>
+      <a href="<?php echo home_url() . '/o-inwestycji-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-[#3e4759]">O inwestycji</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
-      <a href="<?php echo home_url() . '/lokalizacja' ?>" class="text-[#8a8f99] hover:text-textGray">Lokalizacja</a>
+      <a href="<?php echo home_url() . '/lokalizacja-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-textGray">Lokalizacja</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
-      <a href="<?php echo home_url() . '/lista-mieszkan' ?>" class="text-[#8a8f99] hover:text-textGray">Lista mieszkań</a>
+      <a href="<?php echo home_url() . '/mieszkania' ?>" class="text-[#8a8f99] hover:text-textGray">Lista mieszkań</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
-      <a href="<?php echo home_url() . '/galeria-lokalizacja' ?>">Galeria</a>
+      <a href="<?php echo home_url() . '/galeria-osiedle-srebrniki' ?>">Galeria</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
       <a href="<?php echo home_url() . '/kronika-budowy' ?>" class="text-[#8a8f99] hover:text-textGray">Kronika budowy</a>
