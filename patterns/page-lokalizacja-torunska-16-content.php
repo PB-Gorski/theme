d41ea0b5f3d03aaba1b15849dc587f923073dc12
Tomek <TK-54-4B>
2023-10-13 11:05:51 +0200

    test

--- patterns/page-lokalizacja-torunska-16-content.php
+++ patterns/page-lokalizacja-torunska-16-content.php
@@ -37,7 +37,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- wp:group -->
     <div class="wp-block-group row-content mb-[60px] desktop:flex">
       <!-- wp:group -->
-      <div class="block cursor-default left-col desktop:w-[60%] m-0 mb-[30px]" data-aos="fade-right" data-aos-offset="30">
+      <div class="block cursor-default left-col desktop:w-[60%] m-0 mb-[30px]">
         <!-- wp:wpmapblock/wp-map-block {"map_id":"wpmapblock_cbd28970","map_marker_list":[{"lat":"54.350716","lng":"18.656485","title":"PB Górski","content":"","iconType":"default","customIconUrl":"","customIconWidth":25,"customIconHeight":40}]} /-->
       </div>
       <!-- /wp:group -->
