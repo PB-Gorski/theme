cd46589a7b94de4a7342cbc53b962ce655df83e1
Tomek <TK-54-4B>
2023-10-13 13:42:07 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
     },
+    
     on: {
       init: function () {
         const lg = lightGallery($lgSwiper, {
