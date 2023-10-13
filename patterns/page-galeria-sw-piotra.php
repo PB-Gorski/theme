d8b063639f14d40657696d63c24b59854f9666eb
Tomek <TK-54-4B>
2023-10-13 13:23:35 +0200

    test

--- patterns/page-galeria-sw-piotra.php
+++ patterns/page-galeria-sw-piotra.php
@@ -34,22 +34,114 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:group -->
-    <div class="wp-block-group row-content mb-[60px] desktop:flex">
+    <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+    <div id="Banner" class="wp-block-group page-lokalizacja relative">
       <!-- wp:group -->
-      <div class="wp-block-group block cursor-default left-col desktop:w-[60%] m-0 mb-[30px]">
-        <!-- wp:wpmapblock/wp-map-block {"map_id":"wpmapblock_cbd28970","map_marker_list":[{"lat":"54.5249383","lng":"18.5449658","title":"PB Górski","content":"","iconType":"default","customIconUrl":"","customIconWidth":25,"customIconHeight":40}]} /-->
+      <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
+        <!-- wp:group -->
+        <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_60px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
+          <!-- wp:image -->
+          <figure class="wp-block-image m-0">
+            <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
+          </figure>
+          <!-- /wp:image -->
+          <!-- wp:paragraph -->
+          <a href="tel:+48583407900" class="text-white ">(+48) 58 340-79-00</a>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group call-back-modal-bg hidden desktop:block opacity-0 z-[-1] bg-[rgba(61,75,102,0.5)] fixed top-0 left-0 w-screen h-screen transition duration-300 ease">
+      </div>
+      <!-- /wp:group -->
+      <!-- wp:group -->
+      <div class="wp-block-group call-back-modal hidden desktop:block opacity-0 w-[850px] h-[370px] p-[45px_60px] bg-white fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-[-1] transition duration-300 ease">
+        <!-- wp:group -->
+        <div class="wp-block-group btn-close absolute top-[-5px] right-[15px] cursor-pointer">
+          <!-- wp:paragraph -->
+          <p class="text-[40px] text-bgDarkGray opacity-[50%]">×</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <p class="text-[40px] text-center font-bold">Zamów darmowe połączenie</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:paragraph -->
+        <p class="text-center text-bgDarkGray mb-[35px]">Zostaw numer, nasz konsultant oddzwoni do Ciebie</p>
+        <!-- /wp:paragraph -->
+        <!-- wp:group -->
+        <div class="wp-block-group input mb-[25px] flex">
+          <!-- wp:paragraph -->
+          <p class="w-[370px] h-[60px] bg-white text-bgDarkGray text-[14px] leading-[150%]">Numer telefonu</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:group -->
+          <div class="wp-block-group group btn relative bg-[#e80d38] text-white w-[370px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+            <!-- wp:paragraph -->
+            <p class="font-bold z-[1]">Zamawiam rozmowę</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group -->
+        <div class="wp-block-group input checkbox">
+          <!-- wp:paragraph -->
+          <p class="text-bgDarkGray text-[12px] leading-[22px]">Wyrażam zgodę na używanie przez PB Górski sp. z o.o. S.K.A. telekomunikacyjnych urządzeń końcowych dla celów marketingu bezpośredniego, tj. aby przedstawić oferty PB Górski sp. z o.o. S.K.A. telefonicznie. Jednocześnie informujemy, że rozmowa będzie nagrywana. Administratorem Danych Osobowych jest PB Górski sp. z o.o. S.K.A. z siedzibą w Gdańsku (80-312) ul. Wita Stwosza 31A/31. tutaj</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div class="wp-block-group desktop:pl-[100px] desktop:w-[40%]">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] leading-[40px] mb-[40px]" data-aos="fade-right" data-aos-offset="30">Św. Piotra to prestiżowa inwestycja położona w samym sercu Gdyni - na Placu Kaszubskim.</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="text-[18px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">W najbliższym sąsiedztwie znajduje się nie tylko pełne zaplecze handlowe i edukacyjne, ale także przystanek PKM, który pozwala na dojazd do najważniejszych punktów w całym Trójmieście. </p>
-        <!-- /wp:paragraph -->
+      <div class="wp-block-group call-back-pop-up w-[200px] h-[280px] hidden desktop:block text-[16px] text-center bg-transparent flex flex-col jusify-center items-center fixed bottom-[30px] right-[20px] z-[2]">
+        <!-- wp:group -->
+        <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[10px] shadow-lg">
+          <!-- wp:paragraph -->
+          <p>Zostaw swój numer -</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph -->
+          <p class="font-bold">Odzwonimy do Ciebie</p>
+          <!-- /wp:paragraph -->
+          <!-- wp:group -->
+          <div class="wp-block-group group btn-leave-phone relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
+            <!-- wp:paragraph -->
+            <p class="font-bold z-[1]">Zostaw numer</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:image -->
+        <figure class="wp-block-image phone-icon mx-auto w-[92px] cursor-pointer">
+          <img src="<?php echo home_url() ?>//wp-content/themes/pbgorski/assets/img/frontpage/phone-icon.png" alt="phone-icon">
+        </figure>
+        <!-- /wp:image -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:group -->
+      <div class="wp-block-group w-full h-[250px] desktop:h-[770px] bg-banner relative flex desktop:block items-center">
+        <!-- wp:image -->
+        <figure class="wp-block-image w-full h-full absolute top-0 left-0">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/lokalizacja-bg.jpeg" alt="banner-background">
+        </figure>
+        <!-- /wp:image -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pl-[80px] z-[0] relative text-white">
+          <!-- wp:group -->
+          <div class="wp-block-group title text-[36px] desktop:text-[60px] desktop:pt-[160px] mb-[50px] leading-[44px] desktop:leading-[70px]">
+            <!-- wp:paragraph -->
+            <p class="font-bold" data-aos="fade-right" data-aos-offset="30">Galeria Św. Piotra</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
       </div>
       <!-- /wp:group -->
     </div>
@@ -57,73 +149,6 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   </div>
   <!-- /wp:group -->
 
-  <!-- wp:group -->
-  <div class="wp-block-group tiles-numbers px-[20px] mb-[100px] flex flex-col desktop:flex-row justify-center gap-[30px]">
-    <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="50">
-      <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#e90d38] rounded-full flex flex-col items-center justify-center">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">1200 m</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Plaża miejska</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="100">
-      <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#b11a3e] rounded-full flex flex-col items-center justify-center">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">290 m</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Sklep spożywczy</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="150">
-      <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#7d2645] rounded-full flex flex-col items-center justify-center">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">1 km</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Przystanek SKM</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-    <!-- wp:paragraph -->
-    <a href="javascript:void(0)" class="tile1 flex-1 p-[30px_20px] max-w-[375px] bg-[#f5f7fa]" data-aos="fade-up" data-aos-offset="30" data-aos-delay="200">
-      <!-- wp:group -->
-      <div class="wp-block-group circle mx-auto w-[170px] h-[170px] mb-[60px] bg-[#52304a] rounded-full flex flex-col items-center justify-center">
-        <!-- wp:paragraph -->
-        <p class="text-[30px] text-white font-semibold">300 m</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-      <!-- wp:paragraph -->
-      <p class="text-[#8a8f99] text-center">Apteka</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
 
-  <!-- wp:group -->
-  <div class="wp-block-group plan-osiedla-wrapper mb-[100px] mx-auto max-w-[690px] w-full px-[20px] desktop:px-0">
-    <!-- wp:paragraph -->
-    <p class="mb-[60px] text-[24px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">Zaledwie 5 minut będzie dzielić mieszkańców Św. Piotra od Skweru Kościuszki, plaży miejskiej czy Mariny Gdyńskiej. Św. Piotra to także idealne miejsce dla miłośników miejskiego życia - w najbliższej okolicy znajduje się zróżnicowana oferta kawiarni, restauracji i barów, a także kluby fitness, kina i teatry.</p>
-    <!-- /wp:paragraph -->
-  </div>
-  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
