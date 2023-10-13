88a2beb0375367839cda85ad418327122b2ceb6c
Tomek <TK-54-4B>
2023-10-13 08:57:25 +0200

    test

--- patterns/single-inwestycje-zrealizowane.php
+++ patterns/single-inwestycje-zrealizowane.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="Wpis" class="wp-block-group single-page-inwestycje-zrealizowane ooverflow-hidden">
+<div id="Wpis" class="wp-block-group single-page-post mb-[150px] ooverflow-hidden">
   <!-- wp:group -->
   <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
     <!-- wp:group -->
@@ -96,99 +96,58 @@
     <!-- /wp:image -->
   </div>
   <!-- /wp:group -->
-
   <!-- wp:group -->
-  <div class="wp-block-group title-wrapper px-[20px] bg-[#f6f7fa]">
-    <!-- wp:paragraph -->
-    <a href="/inwestycje-zrealizowane/">
-      <!-- wp:paragraph -->
-      <p class="mt-[20px] text-[18px]">&larr; Inwestycje zrealizowane</p>
-      <!-- /wp:paragraph -->
-    </a>
-    <!-- /wp:paragraph -->
+  <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
     <!-- wp:group -->
-    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] py-[30px] desktop:py-[60px] desktop:flex items-center">
-      <!-- wp:group -->
-      <div class="wp-block-group text-[36px] desktop:text-[60px] text-[#2e384c] font-bold leading-[44px]">
-        <!-- wp:paragraph -->
-        <!-- wp:post-title /-->
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
+    <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_60px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
+      <!-- wp:image -->
+      <figure class="wp-block-image m-0">
+        <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
+      </figure>
+      <!-- /wp:image -->
+      <!-- wp:paragraph -->
+      <a href="tel:+48583407900" class="text-white ">(+48) 58 340-79-00</a>
+      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px] py-[40px]">
+  <div class="wp-block-group title-wrapper py-[20px] desktop:py-[48px] bg-[#ebeef2] mb-[50px]">
+    <!-- wp:group -->
+    <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
+      <!-- wp:paragraph -->
+      <a href="/blog/" class="desktop:mt-[-30px] desktop:aabsolute left-0 ">
+        <!-- wp:paragraph -->
+        <p class="pb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">&larr; Inwestycje zrealizowane</p>
+        <!-- /wp:paragraph -->
+      </a>
+      <!-- /wp:paragraph -->
+      <!-- wp:group -->
+      <div class="wp-block-group text-[24px] desktop:text-[40px] text-[#2e384c] font-bold">
+        <!-- wp:post-title /-->
+        <!-- wp:group -->
+        <div class="wp-block-group post-date text-[#8a8f99] text-[18px] font-normal">
+          <!-- wp:post-date {"format":"j F Y"} /-->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+  <!-- wp:group -->
+  <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px]">
     <!-- wp:post-featured-image /-->
 
-    <!-- wp:post-content /-->
-
-    <!-- wp:paragraph -->
-    <p class="mb-[20px] mt-[60px]  text-[#8a8f99] text-[11px]">przeczytaj także</p>
-    <!-- /wp:paragraph -->
-
-    <!-- wp:list -->
-    <ul class="wp-block-list desktop:flex gap-[30px]">
-      <?php
-      $args = array(
-        'post_type' => 'inwest-zrealizowane',
-        'posts_per_page' => 2,
-        'order' => 'ASC'
-      );
-      $post_query = new WP_Query($args);
-      $counter = 0;
-      $i = 0;
-
-      if ($post_query->have_posts()) {
-        while ($post_query->have_posts()) {
-          $post_query->the_post();
-          $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id(), 'portrait');
-          $counter++; ?>
-          <!-- wp:list-item -->
-          <li class="invest-tile group w-full mb-[80px]">
-            <a href="<?php the_permalink(); ?>" class="relative group p-[25px] desktop:max-w-full max-w-[310px] h-full" data-aos="fade-up" data-aos-duration="300" data-aos-offset="30">
-              <!-- wp:group -->
-              <div class="wp-block-group wrapper w-full  <?php echo (!($counter % 2)) ? 'flex-row-reverse' : ''; ?> gap-[50px]">
-                <!-- wp:image -->
-                <figure class="wp-block-image w-full mb-[30px] h-[330px]">
-                  <img src="<?php echo $postImageUrl[0]; ?>" alt="services-background-image">
-                </figure>
-                <!-- /wp:image -->
-
-                <!-- wp:paragraph -->
-                <p class="mr-[20px] text-[18px] desktop:text-[24px] font-bold"><?php the_title(); ?></p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p class="relative text-[18px] desktop:text-[22px] inline before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[50%] group-hover:before:min-h-[100%] before:bg-[#b3f1ff] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Zobacz więcej &rarr;</p>
-                <!-- /wp:paragraph -->
-              </div>
-              <!-- /wp:group -->
-            </a>
-          </li>
-          <!-- /wp:list-item -->
-        <?php
-          $i++;
-        };
-        wp_reset_query(); ?>
-        <!-- wp:query-pagination -->
-        <div class="wp-block-query-pagination">
-          <!-- wp:query-pagination-previous /-->
-          <!-- wp:query-pagination-next /-->
-        </div>
-        <!-- /wp:query-pagination -->
-      <?php
-      };
-
-      ?>
-    </ul>
-    <!-- /wp:list -->
-
-
-
+    <!-- wp:group -->
+    <div class="wp-block-group max-w-[840px] w-full mx-auto pt-[40px]">
+      <!-- wp:post-content /-->
+    </div>
+    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 </div>
