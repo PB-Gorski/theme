4f53d640aa1c5693e07611354de697136ec1c70c
Tomek <TK-54-4B>
2023-10-15 18:27:21 +0200

    test

--- patterns/page-blog-title.php
+++ patterns/page-blog-title.php
@@ -129,7 +129,6 @@
             <a href="<?php echo get_home_url() . '/blog'; ?>" class="inline-block allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">Wszystkie</a>
           </span>
 
-          <!-- dsadwp:core/categories {"showPostCounts":false,"displayAsDropdown":false,"showHierarchy":false} /-->
         </div>
         <!-- /wp:group -->
       </div>
