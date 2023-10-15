1a5a8922471f46caa06939e92bdaae9109dc94f4
Tomek <TK-54-4B>
2023-10-15 11:56:15 +0200

    test

--- patterns/page-blog-title.php
+++ patterns/page-blog-title.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="blog-post" class="wp-block-group blog-post relative">
+<div id="blog-post" class="wp-block-group page-blog-test relative">
   <!-- wp:group -->
   <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
     <!-- wp:group -->
