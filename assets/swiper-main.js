af823eade7b6295fae3a9ae998abf77ebd90e281
Tomek <TK-54-4B>
2023-10-16 11:33:20 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -1,30 +1,30 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  new Swiper('#id', {
-    direction: "horizontal",
-    slidesPerView: 'auto',
-    clickable: true,
-    grabCursor: true,
-    navigation: {
-      nextEl: '#services-next',
-      prevEl: '#services-prev',
-    },
-  });
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  // new Swiper('#id', {
+  //   direction: "horizontal",
+  //   slidesPerView: 'auto',
+  //   clickable: true,
+  //   grabCursor: true,
+  //   navigation: {
+  //     nextEl: '#services-next',
+  //     prevEl: '#services-prev',
+  //   },
+  // });
+  // var swiper = new Swiper(".mySwiper", {
+  //   spaceBetween: 10,
+  //   slidesPerView: 4,
+  //   freeMode: true,
+  //   watchSlidesProgress: true,
+  // });
+  // var swiper2 = new Swiper(".mySwiper2", {
+  //   spaceBetween: 10,
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   thumbs: {
+  //     swiper: swiper,
+  //   },
+  // });
   
 });
