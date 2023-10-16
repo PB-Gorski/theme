bcb81fea540b1cccae61e494e435df3dea1a3c04
Tomek <TK-54-4B>
2023-10-16 14:53:53 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   for (let y = 0; y < closeButtons.length; ++y) {
     
       closeButtons[y].addEventListener('click', function(event) {
-        const params = galleryTop.params; params.slidesPerView = 3.2; 
+        const params = galleryTop.params; params.slidesPerView = 1; 
         galleryTop.update();
         let fullScreenElements = document.querySelectorAll('.fullscreen');
         for (let x = 0; x < fullScreenElements.length; ++x) {
