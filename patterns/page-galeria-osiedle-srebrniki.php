63ae9e81687eb70cb0031b6b053b501e2468df93
Tomek <TK-54-4B>
2023-10-16 10:06:58 +0200

    test

--- patterns/page-galeria-osiedle-srebrniki.php
+++ patterns/page-galeria-osiedle-srebrniki.php
@@ -235,11 +235,12 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
               <div class="swiper-wrapper">
                 <!-- <div class="swiper-slide bg-[url('<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/wizualizacje/2LsfL9YCN8rA2V8Zm1PB14RVfZiBKIerrcOPcS8YCHFwQXuIqnq9cwbM557t_3jpg-image(1200x_).jpg'; ?>')]" style="background-image:url(<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/wizualizacje/2LsfL9YCN8rA2V8Zm1PB14RVfZiBKIerrcOPcS8YCHFwQXuIqnq9cwbM557t_3jpg-image(1200x_).jpg'; ?>)"> -->
                 <!-- wp:image -->
-                <figure class="wp-block-image swiper-slide h-[240px]">
+                <figure class="wp-block-image hidden swiper-slide h-[240px]">
                   <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/wizualizacje/2LsfL9YCN8rA2V8Zm1PB14RVfZiBKIerrcOPcS8YCHFwQXuIqnq9cwbM557t_3jpg-image(1200x_).jpg'; ?>" alt="budowa">
                 </figure>
                 <!-- /wp:image -->
                 <!-- </div> -->
+                <div class="swiper-slide" style="background-image:url(https://unsplash.it/1920/1060)"></div>
                 <div class="swiper-slide" style="background-image:url(https://unsplash.it/1920/1060)"></div>
                 <div class="swiper-slide" style="background-image:url(https://unsplash.it/1920/1060)"></div>
               </div>
