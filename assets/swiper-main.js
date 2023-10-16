25489a04128f1cb84b57c2cf9d2f7108d12bf5c1
Tomek <TK-54-4B>
2023-10-16 11:48:03 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -27,50 +27,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
   //   },
   // });
 
-  var galleryTop = new Swiper('.gallery-top', {
+  const galleryTop = new Swiper('.gallery-top', {
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     spaceBetween: 10,
     keyboardControl: true
- });
- var galleryThumbs = new Swiper('.gallery-thumbs', {
-       nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    spaceBetween: 10,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    touchRatio: 0.2,
-    slideToClickedSlide: true
- });
- galleryTop.params.control = galleryThumbs;
- galleryThumbs.params.control = galleryTop;
- 
- var sliders = document.querySelectorAll('.swiper-slide');
- for (var i = 0; i < sliders.length; ++i) {
-    sliders[i].addEventListener('click', function(event) {
-       console.log('click');
-       event.target.parentNode.parentNode.parentNode.classList.add('fullscreen');
-       setTimeout(function() {
-          galleryTop.update();
-          galleryThumbs.update();
-       }, 200);
-    }, false);
- }
- 
- var closeButtons = document.querySelectorAll('.close-button');
- for (var y = 0; y < closeButtons.length; ++y) {
-    closeButtons[y].addEventListener('click', function(event) {
-       console.log(event);
-       var fullScreenElements = document.querySelectorAll('.fullscreen');
-       console.log(fullScreenElements);
-       for (var x = 0; x < fullScreenElements.length; ++x) {
-          fullScreenElements[x].classList.remove('fullscreen');
-          setTimeout(function() {
-             galleryTop.update();
-             galleryThumbs.update();
-          }, 200);
-       }
-    });
- }
+  });
+  const galleryThumbs = new Swiper('.gallery-thumbs', {
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      spaceBetween: 10,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      touchRatio: 0.2,
+      slideToClickedSlide: true
+  });
+  galleryTop.params.control = galleryThumbs;
+  galleryThumbs.params.control = galleryTop;
+  
+  let sliders = document.querySelectorAll('.swiper-slide');
+  for (let i = 0; i < sliders.length; ++i) {
+      sliders[i].addEventListener('click', function(event) {
+        console.log('click');
+        event.target.parentNode.parentNode.parentNode.classList.add('fullscreen');
+        setTimeout(function() {
+            galleryTop.update();
+            galleryThumbs.update();
+        }, 200);
+      }, false);
+  }
+  
+  let closeButtons = document.querySelectorAll('.close-button');
+  for (let y = 0; y < closeButtons.length; ++y) {
+      closeButtons[y].addEventListener('click', function(event) {
+        console.log(event);
+        let fullScreenElements = document.querySelectorAll('.fullscreen');
+        console.log(fullScreenElements);
+        for (let x = 0; x < fullScreenElements.length; ++x) {
+            fullScreenElements[x].classList.remove('fullscreen');
+            setTimeout(function() {
+              galleryTop.update();
+              galleryThumbs.update();
+            }, 200);
+        }
+      });
+  }
   
 });
