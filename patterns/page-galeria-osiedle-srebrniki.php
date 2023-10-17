6a1ef4156ac2e92f4a0f3b9f793599d58c4a9bc4
Tomek <TK-54-4B>
2023-10-17 08:38:55 +0200

    test

--- patterns/page-galeria-osiedle-srebrniki.php
+++ patterns/page-galeria-osiedle-srebrniki.php
@@ -265,7 +265,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
             <!-- /wp:group -->
 
             <!-- wp:group -->
-            <div class="wp-block-group js-gallery-top gallery-top">
+            <div class="wp-block-group js-gallery-top gallery-top2">
               <!-- wp:group -->
               <div class="wp-block-group swiper-wrapper">
                 <!-- wp:group -->
