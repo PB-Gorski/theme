5e5af77b2a56e8639f20cd694b8ec0646a961a00
Tomek <TK-54-4B>
2023-10-18 17:52:42 +0200

    test

--- patterns/page-galeria-wiezycka-folwark.php
+++ patterns/page-galeria-wiezycka-folwark.php
@@ -153,7 +153,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group col-sm-6 col-sm-push-3">
         <!-- wp:paragraph -->
-        <p class="mb-[20px] text-[#8a8f99]">Wizualizacje</p>
+        <p class="mb-[20px] text-[#8a8f99]">Wizualizacje i okolice</p>
         <!-- /wp:paragraph -->
         <!-- wp:group -->
         <div class="wp-block-group swiper-container">
