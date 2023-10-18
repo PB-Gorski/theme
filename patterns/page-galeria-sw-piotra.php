4385671faeb0f9d8afb3c5d067928d1ed1020bd8
Tomek <TK-54-4B>
2023-10-18 17:40:56 +0200

    test

--- patterns/page-galeria-sw-piotra.php
+++ patterns/page-galeria-sw-piotra.php
@@ -327,8 +327,51 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
                   <!-- /wp:image -->
                 </div>
                 <!-- /wp:group -->
-
-
+                <!-- wp:group -->
+                <div class="wp-block-group swiper-slide swiper-slide-hover">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image h-full w-full">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img7.jpg" alt="slide">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:group -->
+                <div class="wp-block-group swiper-slide swiper-slide-hover">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image h-full w-full">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img8.jpg" alt="slide">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:group -->
+                <div class="wp-block-group swiper-slide swiper-slide-hover">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image h-full w-full">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img9.jpg" alt="slide">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:group -->
+                <div class="wp-block-group swiper-slide swiper-slide-hover">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image h-full w-full">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img10.jpg" alt="slide">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:group -->
+                <div class="wp-block-group swiper-slide swiper-slide-hover">
+                  <!-- wp:image -->
+                  <figure class="wp-block-image h-full w-full">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img11.jpg" alt="slide">
+                  </figure>
+                  <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
               </div>
               <!-- /wp:group -->
 
@@ -359,253 +402,6 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- /wp:group -->
 
 
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
-  <div id="wizualizacje" class="wp-block-group row-wizualizacje mb-[80px]">
-    <!-- wp:group -->
-    <div class="wp-block-group col-sm-6 col-sm-push-3">
-      <!-- wp:paragraph -->
-      <p class="mb-[20px] text-[#8a8f99]">Wizualizacje</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:group -->
-      <div class="wp-block-group swiper-container">
-        <!-- wp:group -->
-        <div class="wp-block-group close-button"></div>
-        <!-- /wp:group -->
-
-        <!-- wp:group -->
-        <div class="wp-block-group js-gallery-top gallery-top">
-          <!-- wp:group -->
-          <div class="wp-block-group swiper-wrapper">
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img1.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img2.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img3.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img4.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img5.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/wizualizacje/img6.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide">
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide">
-            </div>
-            <!-- /wp:group -->
-          </div>
-          <!-- /wp:group -->
-
-          <!-- wp:group -->
-          <div class="wp-block-group js-swiper-button-next swiper-button-next pb-[4px] text-[36px] font-semibold">
-            <!-- wp:paragraph -->
-            <p>></p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-          <!-- wp:group -->
-          <div class="wp-block-group js-swiper-button-prev swiper-button-prev pb-[4px] text-[36px] font-semibold">
-            <!-- wp:paragraph -->
-            <p>&lt;</p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
-  <div id="zdjecia" class="wp-block-group row-zdjecia mb-[80px]">
-    <!-- wp:group -->
-    <div class="wp-block-group col-sm-6 col-sm-push-3">
-      <!-- wp:paragraph -->
-      <p class="mb-[20px] text-[#8a8f99]">Zdjęcia</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:group -->
-      <div class="wp-block-group swiper-container">
-        <!-- wp:group -->
-        <div class="wp-block-group close-button"></div>
-        <!-- /wp:group -->
-
-        <!-- wp:group -->
-        <div class="wp-block-group js-gallery-top gallery-top">
-          <!-- wp:group -->
-          <div class="wp-block-group swiper-wrapper">
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img1.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img2.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img3.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img4.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img5.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img6.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img7.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img8.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img9.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img10.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-            <!-- wp:group -->
-            <div class="wp-block-group swiper-slide swiper-slide-hover">
-              <!-- wp:image -->
-              <figure class="wp-block-image h-full w-full">
-                <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img11.jpg" alt="slide">
-              </figure>
-              <!-- /wp:image -->
-            </div>
-            <!-- /wp:group -->
-          </div>
-          <!-- /wp:group -->
-
-          <!-- wp:group -->
-          <div class="wp-block-group js-swiper-button-next swiper-button-next pb-[4px] text-[36px] font-semibold">
-            <!-- wp:paragraph -->
-            <p>></p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-          <!-- wp:group -->
-          <div class="wp-block-group js-swiper-button-prev swiper-button-prev pb-[4px] text-[36px] font-semibold">
-            <!-- wp:paragraph -->
-            <p>&lt;</p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
 
