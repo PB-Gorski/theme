ed96c8637eab0c708c31c314af8c5656fdd69a19
Tomek <TK-54-4B>
2023-10-18 17:07:22 +0200

    test

--- patterns/page-galeria-osiedle-srebrniki.php
+++ patterns/page-galeria-osiedle-srebrniki.php
@@ -398,15 +398,6 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
                   <!-- /wp:image -->
                 </div>
                 <!-- /wp:group -->
-                <!-- wp:group -->
-                <div class="wp-block-group swiper-slide swiper-slide-hover">
-                  <!-- wp:image -->
-                  <figure class="wp-block-image h-full w-full">
-                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/okolica/img4.jpg" alt="slide">
-                  </figure>
-                  <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
               </div>
               <!-- /wp:group -->
 
