630c153bf8ddc39718107df0c842059cc647b2c5
Tomek <TK-54-4B>
2023-10-18 17:44:06 +0200

    test

--- patterns/page-galeria-sw-piotra.php
+++ patterns/page-galeria-sw-piotra.php
@@ -363,15 +363,6 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
                   <!-- /wp:image -->
                 </div>
                 <!-- /wp:group -->
-                <!-- wp:group -->
-                <div class="wp-block-group swiper-slide swiper-slide-hover">
-                  <!-- wp:image -->
-                  <figure class="wp-block-image h-full w-full">
-                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/galeria/zdjecia/img11.jpg" alt="slide">
-                  </figure>
-                  <!-- /wp:image -->
-                </div>
-                <!-- /wp:group -->
               </div>
               <!-- /wp:group -->
 
