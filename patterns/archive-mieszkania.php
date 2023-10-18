4791d585e09d005a2466ac28787c393451b939c3
Tomek <TK-54-4B>
2023-10-18 13:48:08 +0200

    test

--- patterns/archive-mieszkania.php
+++ patterns/archive-mieszkania.php
@@ -61,7 +61,7 @@
     <!-- wp:list -->
     <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
       <!-- wp:list-item -->
-      <li class="short-code-ymc-filter [&>div>div>div]:container [&>div>div>div]:mx-auto">
+      <li class="short-code-ymc-filter">
         <?php echo do_shortcode('[ymc_filter id="148"]'); ?>
       </li>
       <!-- /wp:list-item -->
