d04980e2e6ded425e2e55c4f31196cd696af0672
Tomek <TK-54-4B>
2023-10-18 15:43:03 +0200

    new swiper

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -1,102 +1,43 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
   let swipers = document.querySelectorAll('.js-gallery-top');
-  let galleryTop = new Swiper('.js-gallery-top', {
-    navigation: {
-      nextEl: ".js-swiper-button-next",
-      prevEl: ".js-swiper-button-prev",
-    },
-    slidesPerView: 3.2,
-    spaceBetween: 20,
-    keyboardControl: true,
-  });
-// test
-  // galleryTop[0].on('click', function () {
-  //   console.log('slide [0] clicked');
-  // });
-  // let galleryTop2 = new Swiper('.js-gallery-top2', {
-  //   navigation: {
-  //     nextEl: ".js-swiper-button-next",
-  //     prevEl: ".js-swiper-button-prev",
-  //   },
-  //   slidesPerView: 3.2,
-  //   spaceBetween: 20,
-  //   keyboardControl: true
-  // });
-  let galleryTopTemp;
-  let sliders = document.querySelectorAll('.swiper-slide');
 
+  swipers.forEach(swiperInstance => {
+    new Swiper(swiperInstance, {
+      navigation: {
+        nextEl: swiperInstance.querySelector(".js-swiper-button-next"),
+        prevEl: swiperInstance.querySelector(".js-swiper-button-prev"),
+      },
+      slidesPerView: 3.2,
+      spaceBetween: 20,
+      keyboardControl: true,
+    });
 
-  
-  for (let i = 0; i < sliders.length; ++i) {
-    sliders[i].addEventListener('click', function(event) {
-
-      // console.log('clicked slider index from swiper index: ', galleryTop.clickedIndex);
-      // console.log('clicked slider index: ', i);
-      // console.log('clicked slider index swiper fun: ', galleryTop);
+    // click per slider
+    swiperInstance.addEventListener('click', '.swiper-slide', function(event) {
+      let clickedSlideIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
 
       event.target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add('fullscreen');
-
-      for (let j = 0 ; j<galleryTop.length ; ++j){
-        galleryTop[j].destroy();
-      };
-      // galleryTop[0].destroy();
-      // galleryTop2.destroy();
-
-      // const params = galleryTopTemp[l].params;
-      // params.slidesPerView = 3.2;
-
-      let galleryTopTemp2 = new Swiper('.js-gallery-top', {
-        navigation: {
-          nextEl: ".js-swiper-button-next",
-          prevEl: ".js-swiper-button-prev",
-        },
-        initialSlide: i,
-        activeIndex: i,
-        slidesPerView: 1,
-        spaceBetween: 20,
-        keyboardControl: true,
-        on: {
-          click: function () {
-            console.log(galleryTopTemp2.activeIndex);
-            console.log('swiper clicked form new Swiper');
-          },
-        },
-      });
-
-
-      for (let k = 0 ; k<galleryTop.length ; ++k){
-        galleryTopTemp2[k].update();
-      };
-      galleryTopTemp = galleryTopTemp2;
-    }, false);
-  }
-  
-  let closeButtons = document.querySelectorAll('.close-button');
-  for (let y = 0; y < closeButtons.length; ++y) {
-    closeButtons[y].addEventListener('click', function(event) {
-
-      for (let l = 0 ; l<galleryTop.length ; ++l){
-        // params.initialSlide = 0;
-      };      
-
-
-
-      for (let j = 0 ; j<galleryTop.length ; ++j){
-        const params = galleryTopTemp[j].params;
-        params.slidesPerView = 3.2;
-        params.initialSlide = 0;
-        galleryTopTemp[j].update();
-      };      
-
-      // galleryTopTemp[0].update();
-      galleryTop = galleryTopTemp;
       
-      let fullScreenElements = document.querySelectorAll('.fullscreen');
-      for (let x = 0; x < fullScreenElements.length; ++x) {
-        fullScreenElements[x].classList.remove('fullscreen');
-      };
+      const params = swiperInstance.params;
+      params.slidesPerView = 1;
+      swiperInstance.slideTo(clickedSlideIndex);
+      swiperInstance.update();
+    
+      swiperInstance.addEventListener('click','.close-button', function(event) {
+        const params2 = swiperInstance.params;
+        params2.slidesPerView = 3.2;
+        params2.initialSlide = 0;
+        swiperInstance.update();
+        swiperInstance.classList.remove('fullscreen');
+      });
     });
-  };
+  });
+
   
+
+
+
+
+
 });
