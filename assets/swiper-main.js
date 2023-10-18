6880acee86cc1e9b6f9d7d60a5f0b3e82c46b5cd
Tomek <TK-54-4B>
2023-10-18 15:57:18 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let swipers = document.querySelectorAll('.js-gallery-top');
 
-  swipers.forEach(swiperInstance => {
-    new Swiper(swiperInstance, {
+  swipers.forEach(swiperNode => {
+    let swiperInstance = new Swiper(swiperNode, {
       navigation: {
         nextEl: swiperInstance.querySelector(".js-swiper-button-next"),
         prevEl: swiperInstance.querySelector(".js-swiper-button-prev"),
@@ -14,18 +14,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     // click per slider
-    swiperInstance.querySelector('.swiper-slide').addEventListener('click', function(event) {
+    swiperNode.querySelector('.swiper-slide').addEventListener('click', function(event) {
       let clickedSlideIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
 
       event.target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add('fullscreen');
       
-      const params = swiperInstance.swiper.params;
+      const params = swiperInstance.params;
       params.slidesPerView = 1;
       swiperInstance.slideTo(clickedSlideIndex);
       swiperInstance.update();
     
-      swiperInstance.querySelector('.close-button').addEventListener('click', function(event) {
-        const params2 = swiperInstance.swiper.params;
+      swiperNode.querySelector('.close-button').addEventListener('click', function(event) {
+        const params2 = swiperInstance.params;
         params2.slidesPerView = 3.2;
         params2.initialSlide = 0;
         swiperInstance.update();
