074a0896306590dc718a3398e22cf76b3d556715
Tomek <TK-54-4B>
2023-10-18 16:10:47 +0200

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -14,26 +14,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     // click per slider
-    swiperNode.querySelectorAll('.swiper-slide').addEventListener('click', function(event) {
-      let clickedSlideIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
-// test
-// event.target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add('fullscreen');
 
-      swiperNode.parentNode.classList.add('fullscreen');
-      
-      const params = swiperInstance.params;
-      params.slidesPerView = 1;
-      swiperInstance.slideTo(clickedSlideIndex);
-      swiperInstance.update();
-    
-      swiperNode.parentNode.querySelector('.close-button').addEventListener('click', function(event) {
-        const params2 = swiperInstance.params;
-        params2.slidesPerView = 3.2;
-        params2.initialSlide = 0;
+    swiperNode.querySelectorAll('.swiper-slide').forEach(slide, function(event){
+      slide.querySelectorAll('.swiper-slide').addEventListener('click', function(event) {
+        let clickedSlideIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
+        // test
+        // event.target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add('fullscreen');
+  
+        swiperNode.parentNode.classList.add('fullscreen');
+        
+        const params = swiperInstance.params;
+        params.slidesPerView = 1;
+        swiperInstance.slideTo(clickedSlideIndex);
         swiperInstance.update();
-        swiperNode.parentNode.classList.remove('fullscreen');
+      
+        swiperNode.parentNode.querySelector('.close-button').addEventListener('click', function(event) {
+          const params2 = swiperInstance.params;
+          params2.slidesPerView = 3.2;
+          params2.initialSlide = 0;
+          swiperInstance.update();
+          swiperNode.parentNode.classList.remove('fullscreen');
+        });
       });
-    });
+    })
+
+    // swiperNode.querySelectorAll('.swiper-slide').addEventListener('click', function(event) {
+    //   let clickedSlideIndex = Array.prototype.indexOf.call(event.target.parentElement.children, event.target);
+    //   // test
+    //   // event.target.parentNode.parentNode.parentNode.parentNode.parentNode.classList.add('fullscreen');
+
+    //   swiperNode.parentNode.classList.add('fullscreen');
+      
+    //   const params = swiperInstance.params;
+    //   params.slidesPerView = 1;
+    //   swiperInstance.slideTo(clickedSlideIndex);
+    //   swiperInstance.update();
+    
+    //   swiperNode.parentNode.querySelector('.close-button').addEventListener('click', function(event) {
+    //     const params2 = swiperInstance.params;
+    //     params2.slidesPerView = 3.2;
+    //     params2.initialSlide = 0;
+    //     swiperInstance.update();
+    //     swiperNode.parentNode.classList.remove('fullscreen');
+    //   });
+    // });
   });
 
   
