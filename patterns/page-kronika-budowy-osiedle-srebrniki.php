57ce319388320cb1faae9272be9ed453e3b7edf8
Tomek <TK-54-4B>
2023-10-19 09:34:49 +0200

    test

--- patterns/page-kronika-budowy-osiedle-srebrniki.php
+++ patterns/page-kronika-budowy-osiedle-srebrniki.php
@@ -1082,7 +1082,7 @@
                 <div class="wp-block-group swiper-slide swiper-slide-hover">
                   <!-- wp:image -->
                   <figure class="wp-block-image h-full w-full">
-                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/kronika-budowy/kwiecien-2021/img51.jpeg" alt="slide">
+                    <img src="https://pbgorski.webo.design/pl/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/galeria/kronika-budowy/kwiecien-2021/img50.png" alt="slide">
                   </figure>
                   <!-- /wp:image -->
                 </div>
