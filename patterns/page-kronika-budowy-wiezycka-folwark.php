f01ca43c9407b1213e5f1b799bc291dc38a962c0
Tomek <TK-54-4B>
2023-10-19 13:33:57 +0200

    test

--- patterns/page-kronika-budowy-wiezycka-folwark.php
+++ patterns/page-kronika-budowy-wiezycka-folwark.php
@@ -332,11 +332,11 @@
       </div>
       <!-- /wp:group -->
 
-      <!-- wp:group -->
-      <div class="wp-block-group">
-        kwiecien - yt
-      </div>
-      <!-- /wp:group -->
+      <!-- wp:embed {"url":"https://www.youtube.com/watch?v=QZXc39hT8t4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+      <figure class="wp-block-embed video-yt mb-[80px] opacity-0 is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+        <div class="wp-block-embed__wrapper">https://www.youtube.com/watch?v=n-_Tu_AQOJw</div>
+      </figure>
+      <!-- /wp:embed -->
 
       <!-- wp:group -->
       <div id="styczen-2022" class="wp-block-group row-wrzesien mb-[80px]">
