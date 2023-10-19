24550a7af0271a60a7e8c97cc4f887a83dfb73c0
Tomek <TK-54-4B>
2023-10-19 15:07:24 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -384,11 +384,12 @@ window.addEventListener("load", function() {
   const postsFoundParentHTML = document.querySelector('.filter-layout3-148');
 
   let counter = false;
-  let postsCount = '';
-  wp.hooks.addAction('ymc_after_loaded_data_148_1', 'smartfilter', function(target, res){
+  
+  let postsCount = wp.hooks.addAction('ymc_after_loaded_data_148_1', 'smartfilter', function(target, res){
     postsCount = res.found;
     console.log(res.found);
     console.log('Complete loaded data ' + target + ' ' + res.found);
+    return res.found;
   });
 
     console.log('post found from js ', postsCount);
