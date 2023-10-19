88120d65e470c93cb0ba45e08d95e4f39eefec60
Tomek <TK-54-4B>
2023-10-19 19:34:17 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -379,9 +379,13 @@ window.addEventListener("load", function() {
   newArr.forEach((elem,index) => {
     let indexFrom2 = index + 2;
     console.log('name without spaces');
+    console.log(elem);
     console.log(parseInt(priceValueArrNodeList[indexFrom2].childNodes[1].dataset.name));
-    if (parseInt(priceValueArrNodeList[indexFrom2].childNodes[1].dataset.name) == elem){
-      newArrHTMLList.push(priceValueArrNodeList[indexFrom2].childNodes[1])
+
+    for(j = 2 ; j < priceValueArrNodeList.length ; j++){
+      if (parseInt(priceValueArrNodeList[j].childNodes[1].dataset.name) == elem){
+        newArrHTMLList.push(priceValueArrNodeList[j].childNodes[1]);
+      }
     }
 
   })
