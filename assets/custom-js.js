750f7903b72c403dbb137a31db7134049553add0
Tomek <TK-54-4B>
2023-10-19 17:18:48 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -369,6 +369,15 @@ window.addEventListener("load", function() {
 
   console.log('res');
   console.log(res);
+
+
+  let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (matching values)
+
+alert( arr ); // 5,3,8,1 (not modified)
   
 
 
