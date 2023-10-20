67a9b982531d5a5d123040179a0802967f44bff5
Tomek <TK-54-4B>
2023-10-20 19:06:01 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -416,8 +416,10 @@ window.addEventListener("load", function() {
   const mainDropDown = document.querySelectorAll('.dropdown__value');
   const dropDownList = document.querySelectorAll('.dropdown__list');
   const dropDowns = Array.from(document.querySelectorAll('.dropdown__link'));
-  let priceValueArrHTMLCol = dropDownFilters[6].childNodes[1].children;
-  let priceValueArrNodeList = dropDownFilters[6].childNodes[1].childNodes;
+  // let priceValueArrHTMLCol = dropDownFilters[6].childNodes[1].children;
+  // let priceValueArrNodeList = dropDownFilters[6].childNodes[1].childNodes;
+  let priceValueArrHTMLCol = 0
+  let priceValueArrNodeList = 0
   let priceMinValue = parseInt(document.querySelector('.dropdown__value-min').innerHTML);
   let priceMaxValue = parseInt(document.querySelector('.dropdown__value-max').innerHTML);
   // let priceValue = parseInt(priceValueArrNodeList[2].childNodes[1].dataset.name.split(' ').join(''));
