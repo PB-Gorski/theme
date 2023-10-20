c72d07680f4b0ebcdc888a65fd78c3f8fe3ef70d
Tomek <TK-54-4B>
2023-10-20 07:59:56 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -514,7 +514,10 @@ window.addEventListener("load", function() {
     });
 
     newArrHTMLList.forEach(htmlElemList => {
-      htmlElemList.click();
+      setTimeout(() => {
+        htmlElemList.click();
+        
+      },100);
     });
 
     // console.log(newArrHTMLList);
