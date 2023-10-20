f547d3eed6a890d06d453d6986fdd1609544cd58
Tomek <TK-54-4B>
2023-10-20 20:36:47 +0200

    test

--- filter-layout.php
+++ filter-layout.php
@@ -167,6 +167,16 @@ $tax_sort = $taxonomy;
       unset($result_tax);
       ?>
     </div>
+    <?php
+    $term = $selected_val2 == true ? $selected_val2 : '';
+
+    $query = new WP_Query(array(
+      'miasto' => $term,
+      'post_status' => 'publish'
+    ));
+    $count = $query->found_posts;
+    ?>
+
     <p class="mb-[50px] text-[24px] text-center" data-aos="fade-up" data-aos-offset="30">Znaleziono <?php echo $count - 1; ?> oferty pasujących do Twoich kryteriów <span class="text-[16px] text-[#8a8f99]">(wszystkich ogłoszeń <?php echo wp_count_posts('mieszkania')->publish; ?>)</span></p>
     <div class="posts-found"></div>
   <?php endif; ?>
