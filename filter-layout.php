ad3ab7ddf0796e49bd8ff3e9aaa1cfffbb569cb8
Tomek <TK-54-4B>
2023-10-21 11:42:42 +0200

    test

--- filter-layout.php
+++ filter-layout.php
@@ -18,13 +18,13 @@ $tax_sort = $taxonomy;
   ?>
     <div class="search_tabs container flex justify-center items-center">
       <div class="tab_mieszkania active">
-        <p class="text-[20px] font-semibold">Mieszkania</p>
+        <p class="text-[20px] font-medium">Mieszkania</p>
       </div>
       <div class="tab_mieszkania">
-        <p class="text-[20px] font-semibold">Lokale</p>
+        <p class="text-[20px] font-medium">Lokale</p>
       </div>
       <div class=" tab_mieszkania">
-        <p class="text-[20px] font-semibold">Biura</p>
+        <p class="text-[20px] font-medium">Biura</p>
       </div>
     </div>
 
