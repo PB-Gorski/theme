b2481fcd2cada290a13249498b97dbae090c39fc
Tomek <TK-54-4B>
2023-10-22 18:34:59 +0200

    test

--- filter-layout.php
+++ filter-layout.php
@@ -64,50 +64,50 @@ $tax_sort = $taxonomy;
             <p class="dropdown-filter-title">Cena od:</p>
             <div class="dropdown__value dropdown__value-min font-bold"><span>0</span></div>
             <ul class="dropdown__list">
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">50 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">50 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">100 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">100 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">150 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">150 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">200 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">200 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">250 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">250 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">300 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">300 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">350 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">350 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">400 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">400 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">450 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">450 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">500 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">750 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">750 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">1 000 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">1 000 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">1 500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">1 500 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">2 000 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">2 000 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">2 500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">2 500 000</span>
               </li>
             </ul>
           </div>
@@ -115,50 +115,50 @@ $tax_sort = $taxonomy;
             <p class="dropdown-filter-title">Cena do:</p>
             <div class="dropdown__value dropdown__value-max font-bold">Max</div>
             <ul class="dropdown__list">
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">50 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">50 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">100 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">100 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">150 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">150 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">200 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">200 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">250 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">250 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">300 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">300 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">350 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">350 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">400 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">400 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">450 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">450 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">500 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">750 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">750 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">1 000 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">1 000 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">1 500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">1 500 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">2 000 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">2 000 000</span>
               </li>
-              <li class="dropdown__item">
-                <span class="dropdown__link cursor-pointer">2 500 000</span>
+              <li class="dropdown__item menu-passive__item">
+                <span class="dropdown__link menu-link cursor-pointer">2 500 000</span>
               </li>
             </ul>
           </div>
