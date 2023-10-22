22aca9070f2e2278e8c32476f4660b00a12bb242
Tomek <TK-54-4B>
2023-10-22 13:35:45 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -580,7 +580,7 @@ window.addEventListener("load", function() {
   if(document.body.classList.contains('post-type-archive-mieszkania')){
     btnMore.addEventListener('click',()=>{
       (window.innerWidth > 480) ? searchBar.classList.toggle('desktop:h-[260px]') : searchBar.classList.toggle('h-[450px]');
-      foundedPostOnStart.classList.add('top-[320px]')
+      foundedPostOnStart.classList.toggle('top-[320px]')
 
     });
   };
