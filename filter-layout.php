a2b8736543192595bf72d52a32b7dcb1d5ba804a
Tomek <TK-54-4B>
2023-10-22 16:34:33 +0200

    test

--- filter-layout.php
+++ filter-layout.php
@@ -148,12 +148,8 @@ $tax_sort = $taxonomy;
         ?>
           <div class="dropdown-filter flex-col bg-white h-fit">
             <p class="dropdown-filter-title"><?php echo get_taxonomy($select_term)->label . ':' ?></p>
-            <div class="menu-active font-bold">
-              <span><?php echo get_taxonomy($select_term)->label ?></span>
-              <span class="arrow-down"></span>
-            </div>
-            <div class="menu-passive">
-              <i class="btn-close">x</i>
+            <div class="menu-active font-bold"><span><?php echo get_taxonomy($select_term)->label ?></span><span class="arrow-down"></span></div>
+            <div class="menu-passive"><i class="btn-close">x</i>
               <?php
               $terms_icons = null;
               foreach ($terms_selected as $term) {
