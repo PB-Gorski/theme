a1e8f58019855b35559434d033c3611881dbbb9d
Tomek <TK-54-4B>
2023-10-22 13:54:21 +0200

    test

--- filter-layout.php
+++ filter-layout.php
@@ -104,7 +104,7 @@ $tax_sort = $taxonomy;
           </div>
 
           <div class="btn-wrapper bg-white h-[120px] flex items-center justify-center" style="margin-right:2px;">
-            <div class="wp-block-group btn-more relative mt-[5px] w-[80px] bg-[#ebeef2] flex gap-[10px] justify-center items-center cursor-pointer border-b-[3px] border-b-[#dcdee3] before:content-[``] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#e1e1e1] before:transition-height before:duration-300 before:ease cursor-pointer" style="height:50px;width:170px">
+            <div class="wp-block-group btn-more relative mt-[5px] w-[80px] bg-[#ebeef2] flex gap-[10px] justify-center items-center cursor-pointer border-b-[3px] border-b-[#dcdee3] before:content-[``] before:inline-block apply-btn-more-hover" style="height:50px;width:170px">
               <p class="text-[18px] font-bold">Więcej opcji</p>
             </div>
           </div>
