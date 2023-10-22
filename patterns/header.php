76c357fd5458b7af5280b0bae0bc3a5c1dbeb9d9
Tomek <TK-54-4B>
2023-10-22 19:13:47 +0200

    test

--- patterns/header.php
+++ patterns/header.php
@@ -406,7 +406,7 @@
         <!-- /wp:group -->
 
         <!-- wp:group -->
-        <div class="wp-block-group group btn-find-more relative bg-[#3d4b66] text-white w-[230px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
+        <div class="wp-block-group group btn-find-more relative bg-transparent text-white w-[230px] h-[55px] flex items-center justify-center border-[2px] border-white rounded-full cursor-pointer">
           <!-- wp:paragraph -->
           <a href="<?php echo home_url() . '/mieszkania'; ?>" class="font-bold z-[1]">Znajdź mieszkanie</a>
           <!-- /wp:paragraph -->
