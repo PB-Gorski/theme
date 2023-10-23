6905045da2b86723d5f10991dafd70745a4a1a42
Tomek <TK-54-4B>
2023-10-23 08:18:12 +0200

    test

--- patterns/blog.php
+++ patterns/blog.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="blog" class="wp-block-group blog h-[600pxpx]">
+<div id="blog" class="wp-block-group blog">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
     <!-- wp:group -->
@@ -75,7 +75,7 @@
     </ul>
     <!-- /wp:list -->
     <!-- wp:paragraph -->
-    <a href="<?php echo home_url() . '/blog' ?>" class="wp-block-group desktop:absolute bottom-[-70px] left-[50%] translate-x-[-50%]" data-aos="fade-right" data-aos-duration="500" data-aos-offset="30">
+    <a href="<?php echo home_url() . '/blog' ?>" class="wp-block-group" data-aos="fade-right" data-aos-duration="500" data-aos-offset="30">
       <!-- wp:group -->
       <div class="wp-block-group btn-find-more relative bg-[#3d4b66] text-white desktop:w-[315px] w-full h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer">
         <!-- wp:paragraph -->
