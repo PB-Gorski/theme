44bf05a0b777202ab2f86adc222127931971d4ab
Tomek <TK-54-4B>
2023-10-24 15:01:34 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -819,12 +819,41 @@ window.addEventListener("load", function() {
       }).apiTermUpdate(); 
       console.log('after YMC api update');
     }
-    if(document.cookie.match(/^(.*;)?\s*MyCookie\s*=\s*[^;]+(.*)?$/)){
-      console.log('cookie exist');
-      setTimeout(() => {
-        runFromCookies()
-      }, 1000);
-    };
+
+    function getCookie(name) {
+      var dc = document.cookie;
+      var prefix = name + "=";
+      var begin = dc.indexOf("; " + prefix);
+      if (begin == -1) {
+          begin = dc.indexOf(prefix);
+          if (begin != 0) return null;
+      }
+      else
+      {
+          begin += 2;
+          var end = document.cookie.indexOf(";", begin);
+          if (end == -1) {
+          end = dc.length;
+          }
+      }
+      // because unescape has been deprecated, replaced with decodeURI
+      //return unescape(dc.substring(begin + prefix.length, end));
+      return decodeURI(dc.substring(begin + prefix.length, end));
+  } 
+  
+  function doSomething() {
+      var myCookie = getCookie("MyCookie");
+  
+      if (myCookie == null) {
+          // do cookie doesn't exist stuff;
+          console.log('not cookie');
+      }
+      else {
+        setTimeout(() => {
+          runFromCookies()
+        }, 1000);
+      }
+  }
 
     wp.hooks.addAction('ymc_after_loaded_data_148_1', 'smartfilter', function(){
       // YMCTools({
