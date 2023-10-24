faee50c06cd79d605bf029e13d51937f32188aaf
Tomek <TK-54-4B>
2023-10-24 09:01:17 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -757,26 +757,27 @@ window.addEventListener("load", function() {
     btn.addEventListener('click', () => {
       indexFrom1 = index + 1;
       let currentFiltr = indexFrom1 + index;
+      currentFiltr = indexFrom1;
       console.log('current filtr: ', currentFiltr);
 
       sortListDir(index);
       
-      // for (let i = 1 ; i <= (17) ; i=i+2){
+      for (let i = 1 ; i <= (17) ; i=i+2){
         if (i == currentFiltr){
-          if( btnsSort.classList.contains('sort-arrow-up')){
-            btnsSort.classList.remove('sort-arrow-up');
-            btnsSort.classList.add('sort-arrow-down');
+          if( btnsSort[currentFiltr - counter].classList.contains('sort-arrow-up')){
+            btnsSort[currentFiltr - counter].classList.remove('sort-arrow-up');
+            btnsSort[currentFiltr - counter].classList.add('sort-arrow-down');
           }else{
-            btnsSort.classList.add('sort-arrow-up');
-            btnsSort.classList.remove('sort-arrow-down');
+            btnsSort[currentFiltr - counter].classList.add('sort-arrow-up');
+            btnsSort[currentFiltr - counter].classList.remove('sort-arrow-down');
           }
         }else if(i != currentFiltr){
           console.log('i rozny od current filtr');
           // btnsSort[i - counter].classList.remove('sort-arrow-up','sort-arrow-down');
         }
         counter++
-      // };
-
+      };
+      
       counter=1;
     });
   });  
