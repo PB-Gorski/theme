9e1797a7b84e444f4ce5ed5e8f53e86a3b20e95a
Tomek <TK-54-4B>
2023-10-24 15:19:46 +0200

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -789,7 +789,18 @@ window.addEventListener("load", function() {
   
     // cookie data handle
     document.cookie = "miasto=; inwestycja=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-
+    function deleteAllCookies() {
+        const cookies = document.cookie.split(";");
+    
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const eqPos = cookie.indexOf("=");
+            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
+    };
+    deleteAllCookies();
+    
     function getCookie(cname) {
       let name = cname + "=";
       let decodedCookie = decodeURIComponent(document.cookie);
