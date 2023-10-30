9ad65d53c5810f7fb9ddd19eb6cbab5410fb41fa
Tomek <TK-54-4B>
2023-10-30 16:43:16 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -543,7 +543,7 @@ window.addEventListener("load", function() {
       const dropDownFilters = document.querySelectorAll(".dropdown-filter");
       let priceValueArrNodeList = dropDownFilters[5].childNodes[1].childNodes;
       priceMinValue = parseInt(document.querySelector('.dropdown__value-min').childNodes[0].innerHTML.split(' ').join(''));
-      priceMaxValue = document.querySelector('.dropdown__value-max').innerHTML == 'Max' ? 10000000 : cenaDo;
+      priceMaxValue = document.querySelector('.dropdown__value-max').innerHTML == 'Max' ? 10000000 : parseInt(document.querySelector('.dropdown__value-max').childNodes[0].innerHTML.split(' ').join(''));
       
       // console.log('no spaces', priceMinValue,priceMaxValue);
 
