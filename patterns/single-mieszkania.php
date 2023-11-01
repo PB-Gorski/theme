0ea0a09dc2918090945d1c731de70d85daabfc2a
Tomek <TK-54-4B>
2023-11-01 17:26:14 +0100

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -233,7 +233,7 @@
         <!-- wp:group -->
         <div class="wp-block-group group mb-[40px] btn-download relative bg-[#f5f7fa] text-textGray w-full desktop:w-[230px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#e9ecf2] before:transition-height before:duration-300 before:ease cursor-pointer">
           <!-- wp:paragraph -->
-          <?php echo do_shortcode('[pdf_attachment file="2" name="Pobierz kartę lokalu2"]'); ?>
+          <?php echo do_shortcode('[pdf_attachment file="1" name="Pobierz kartę lokalu2"]'); ?>
           <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
