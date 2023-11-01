01f1bb7ad2f9fe7b78b0c7ecb9776d9d8d4058b8
Tomek <TK-54-4B>
2023-11-01 11:07:32 +0100

    test

--- filter-layout.php
+++ filter-layout.php
@@ -163,12 +163,12 @@ $tax_sort = $taxonomy;
             </ul>
           </div>
 
-          <div class="btn-wrapper bg-white h-[120px] flex items-center justify-center" style="border-right:2px solid #ebeef2;border-bottom:2px solid #ebeef2">
+          <div class="btn-wrapper bg-white h-[120px] flex items-center justify-center" style="width:100%;border-right:2px solid #ebeef2;border-bottom:2px solid #ebeef2">
             <div class="wp-block-group btn-more relative mt-[5px] bg-[#ebeef2] flex gap-[10px] justify-center items-center cursor-pointer border-b-[3px] border-b-[#dcdee3] " style="height:50px;width:140px;">
               <p class="text-[18px] font-bold z-[1]">Więcej opcji</p>
             </div>
           </div>
-          <div class="btn-search btn-wrapper bg-white h-[120px] flex items-center justify-center" style="border-bottom:2px solid #ebeef2">
+          <div class="btn-search btn-wrapper bg-white h-[120px] flex items-center justify-center" style="width:100%;border-bottom:2px solid #ebeef2">
             <div class="wp-block-group group btn-search mt-[5px] relative bg-primaryRed text-white h-[50px] flex items-center justify-center gap-[15px] before:content-[``] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#9C0927] before:transition-height before:duration-300 before:ease cursor-pointer" style="height:50px;width:140px">
               <p class="font-bold z-[1] text-white">Szukaj</p>
             </div>
