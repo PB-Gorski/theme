d12fabcbcd5a88682860bb9055cdc323bd2ff777
Tomek <TK-54-4B>
2023-11-02 10:06:50 +0100

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -74,11 +74,15 @@
       <!-- wp:group -->
       <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px] py-[30px] desktop:py-0 flex items-center desktop:gap-[110px] flex-wrap">
         <!-- wp:group -->
-        <div class="wp-block-group col basis-1/2 desktop:basis-auto">
+        <div class="wp-block-group col mb-[10px] basis-1/2 desktop:basis-auto">
           <!-- wp:paragraph -->
           <p class="mb-[10px] uppercase text-[10px] text-[rgba(255,255,255,0.6)]">odbiór</p>
           <!-- /wp:paragraph -->
-          <!-- wp:post-terms {"term":"termin"} /-->
+          <!-- wp:group -->
+          <div class="wp-block-group text-[20px]">
+            <!-- wp:post-terms {"term":"termin"} /-->
+          </div>
+          <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group -->
