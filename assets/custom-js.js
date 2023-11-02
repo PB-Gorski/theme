4b9ce6ed231b85b029915faf65d5e2d22cab87e3
Tomek <TK-54-4B>
2023-11-02 14:35:03 +0100

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -9,14 +9,14 @@ window.addEventListener("load", function() {
     if (mainNavMenu.classList.contains("opacity-[0]")) {
       mainNavMenu.classList.remove("opacity-[0]");
       mainNavMenu.classList.add("opacity-[1]");
-      mainNavMenu.classList.replace('z-[-2]','z-[3]');
+      // mainNavMenu.classList.replace('z-[-2]','z-[3]');
       mainNavMenu.classList.remove('translate-x-[100%]');
       btnOpenMenu.classList.add("menu-active");
       logo.classList.add("opacity-0");
     } else if (mainNavMenu.classList.contains("opacity-[1]")) {
       mainNavMenu.classList.remove("opacity-[1]");
       mainNavMenu.classList.add("opacity-[0]");
-      mainNavMenu.classList.replace('z-[3]', 'z-[-2]');
+      // mainNavMenu.classList.replace('z-[3]', 'z-[-2]');
       mainNavMenu.classList.add('translate-x-[100%]');
       btnOpenMenu.classList.remove("menu-active");
       logo.classList.remove("opacity-0");
