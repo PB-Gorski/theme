d5c062663b1531d30f7290c11a4d6b5d45cff53b
Tomek <TK-54-4B>
2023-11-03 07:35:54 +0100

    test

--- functions.php
+++ functions.php
@@ -538,7 +538,7 @@ function pbgorski_custom_filter_layout2($layout, $terms, $taxonomy, $multiple, $
 {
 	$filepath_filter = get_stylesheet_directory() . '/filter-layout2.php';
 	$filter_id = '1850';
-	$layout_id = '2';
+	$layout_id = '3';
 	$layout  = ''; //Override demo message
 	ob_start();
 
@@ -551,7 +551,7 @@ function pbgorski_custom_filter_layout2($layout, $terms, $taxonomy, $multiple, $
 
 	return $layout;
 }
-add_filter('ymc_filter_custom_layout_1850_2', 'pbgorski_custom_filter_layout3', 10, 6);
+add_filter('ymc_filter_custom_layout_1850_3', 'pbgorski_custom_filter_layout3', 10, 6);
 
 function my_custom_post_layout($layout, $post_id, $filter_id, $increment_post, $arrOptions)
 {
