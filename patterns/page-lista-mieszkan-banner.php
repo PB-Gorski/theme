3f0d447460ddf4c4e64280a31ad3c740de5adeef
Tomek <TK-54-4B>
2023-11-04 15:35:08 +0100

    test

--- patterns/page-lista-mieszkan-banner.php
+++ patterns/page-lista-mieszkan-banner.php
@@ -69,34 +69,6 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group call-back-pop-up w-[200px] h-[280px] hidden desktop:block text-[16px] text-center bg-transparent flex flex-col jusify-center items-center fixed bottom-[30px] right-[20px] z-[4]">
-    <!-- wp:group -->
-    <div class="wp-block-group text mb-[10px] w-full h-[160px] bg-white flex flex-col justify-center items-center gap-y-[10px] shadow-lg">
-      <!-- wp:paragraph -->
-      <p>Zostaw swój numer -</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p class="font-bold">Odzwonimy do Ciebie</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:group -->
-      <div class="wp-block-group group btn-leave-phone relative bg-primaryRed text-white w-[170px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
-        <!-- wp:paragraph -->
-        <p class="font-bold z-[1]">Zostaw numer</p>
-        <!-- /wp:paragraph -->
-      </div>
-      <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-
-    <!-- wp:image -->
-    <figure class="wp-block-image phone-icon mx-auto w-[92px] cursor-pointer">
-      <img src="<?php echo home_url() ?>//wp-content/themes/pbgorski/assets/img/frontpage/phone-icon.png" alt="phone-icon">
-    </figure>
-    <!-- /wp:image -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
   <div class="wp-block-group w-full h-[600px] relative py-[60px]">
     <!-- wp:image -->
     <figure class="wp-block-image w-full h-full absolute top-0 left-0 z-[-1]">
