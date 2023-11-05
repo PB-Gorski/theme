c78c765e82588923af536cd94645be7a9e832db6
Tomek <TK-54-4B>
2023-11-05 18:28:13 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -488,7 +488,7 @@ window.addEventListener("load", function() {
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         }
     };
-    // deleteAllCookies();
+    deleteAllCookies();
 
     function getCookie(cname) {
       let name = cname + "=";
@@ -786,7 +786,7 @@ window.addEventListener("load", function() {
           runFromCookies()
         }, 1000);
       };
-      deleteAllCookies();
+      // deleteAllCookies();
     }else{
       console.log('no cookies');
     };
