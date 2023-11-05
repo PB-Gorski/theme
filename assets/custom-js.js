66ad3a0327550374a4928ae0915fa84a755afe9a
Tomek <TK-54-4B>
2023-11-05 18:46:03 +0100

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -297,16 +297,16 @@ window.addEventListener("load", function() {
   // cookies from page o-inwestycji
   const clickedLink = document.querySelector('.js-srebrniki-cookie');
 
-  function deleteAllCookies() {
-    const cookies = document.cookie.split(";");
+  // function deleteAllCookies() {
+  //   const cookies = document.cookie.split(";");
 
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf("=");
-        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-  };
+  //   for (let i = 0; i < cookies.length; i++) {
+  //       const cookie = cookies[i];
+  //       const eqPos = cookie.indexOf("=");
+  //       const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+  //       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  //   }
+  // };
 
   if(this.document.body.classList.contains('page-id-1711')){
     document.cookie = "username=John Doe";
