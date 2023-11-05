4269885b4c9153853a71d2e1906ac3971feb6165
Tomek <TK-54-4B>
2023-11-05 21:34:53 +0100

    test

--- patterns/blog.php
+++ patterns/blog.php
@@ -27,6 +27,25 @@
     </div>
     <!-- /wp:group -->
 
+
+    <!-- wp:group -->
+    <div class="wp-block-group above-wp-query">
+      <!-- wp:query {"query":{"perPage":4,"pages":0,"offset":0,"postType":"post"},"isLink":true} -->
+      <!-- wp:group -->
+      <div class="wp-block-group above-post-template-frontpage-blog ul-wrapper-frontpage-blog mx-auto w-[100%]">
+        <!-- wp:post-template {"className":"swiper-wrapper"} -->
+
+        <!-- wp:group -->
+        <div class="wp-block-group swiper-slide22222 group desktop:w-[535px] w-[300px] h-[500px] desktop:h-[740px]">
+          <!-- wp:post-title {"isLink":true} /-->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+
     <!-- wp:list -->
     <ul class="wp-block-list mb-[40px] flex desktop:flex-row flex-col justify-between desktop:gap-[30px] gap-[60px]">
       <?php
