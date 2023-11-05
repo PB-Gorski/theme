5c3315af38fce3027a7a6b8e7ec0041b8a561dd2
Tomek <TK-54-4B>
2023-11-05 11:07:06 +0100

    test

--- patterns/page-o-inwestycji-osiedle-srebrniki-content.php
+++ patterns/page-o-inwestycji-osiedle-srebrniki-content.php
@@ -27,7 +27,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <a href="<?php echo home_url() . '/lokalizacja-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lokalizacja</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
-      <a href="<?php echo home_url() . '/mieszkania' ?>" class="js-srebrniki-cookie text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lista mieszkań</a>
+      <a href="#" class="js-srebrniki-cookie text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lista mieszkań</a>
       <!-- /wp:paragraph -->
       <!-- wp:paragraph -->
       <a href="<?php echo home_url() . '/galeria-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Galeria</a>
