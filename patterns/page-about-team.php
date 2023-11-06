a27ef2daa201cb445ecdcdffbd31c2113f879f4d
Tomek <TK-54-4B>
2023-11-06 08:22:42 +0100

    test

--- patterns/page-about-team.php
+++ patterns/page-about-team.php
@@ -37,10 +37,10 @@
   <!-- wp:group -->
   <div class="wp-block-group mb-[80px] container mx-auto desktop:px-0 px-[20px] desktop:max-w-[680px]">
     <!-- wp:paragraph -->
-    <span class="mb-[30px] text-[30px] text-textGray leading-[44px]" data-aos="fade-right" data-aos-offset="30">Potwierdzeniem wysokiej jakości świadczonych usług, a także zadowolenia klientów są liczne nagrody i wyróżnienia zdobywane na przestrzeni lat.</span>
+    <span class="block mb-[30px] text-[30px] text-textGray leading-[44px]" data-aos="fade-right" data-aos-offset="30">Potwierdzeniem wysokiej jakości świadczonych usług, a także zadowolenia klientów są liczne nagrody i wyróżnienia zdobywane na przestrzeni lat.</span>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <span class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">To właśnie one dają powód do dumy dla Zarządu i przekonują, że obrana droga rozwoju jest słuszna i tylko wysoka jakość oraz solidność wykonywanych usług mogą być gwarancją sukcesu. Spółkę, ze stu procentowym kapitałem polskim reprezentują jej założyciele i obecni członkowie Zarządu: Bogdan Górski – Prezes, Iwona Górska – Wiceprezes oraz Marcin Górski - Wiceprezes.</span>
+    <span class="block mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">To właśnie one dają powód do dumy dla Zarządu i przekonują, że obrana droga rozwoju jest słuszna i tylko wysoka jakość oraz solidność wykonywanych usług mogą być gwarancją sukcesu. Spółkę, ze stu procentowym kapitałem polskim reprezentują jej założyciele i obecni członkowie Zarządu: Bogdan Górski – Prezes, Iwona Górska – Wiceprezes oraz Marcin Górski - Wiceprezes.</span>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
