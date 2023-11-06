db1029f3f31b0ea20d04efb206fb9d33bf1a94a0
Tomek <TK-54-4B>
2023-11-06 19:26:55 +0100

    test

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -323,7 +323,7 @@ window.addEventListener("load", function() {
 
   let categoryList = document.querySelector('.wp-block-categories-list')
   link =`
-  <a href="<?php echo get_home_url() . '/blog'; ?>" class="inline-block allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">Wszystkie</a>
+  <a href="` +  window.location.origin + ` '/pl/blog" class="inline-block allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">Wszystkie JS</a>
   `
   
   if(document.body.classList.contains('page-id-189')){
