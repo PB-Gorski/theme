a762daaa8a2de48873a7490843919b23048eb283
Tomek <TK-54-4B>
2023-11-06 18:27:16 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -4,20 +4,7 @@ window.addEventListener("load", function() {
   document.body.classList.contains('page-parent') ||
   document.body.classList.contains('page-child')){
 
-    document.body.addEventListener('click', () => {
-      document.querySelectorAll('.menu-passive').forEach(activeItem =>{
-        activeItem.style.display = 'none';  
-      });
-
-      document.querySelectorAll('.dropdown__list').forEach(activeItem2 =>{
-        if(activeItem2.classList.contains('dropdown__list_active-metraz') || activeItem2.classList.contains('dropdown__list_active')){
-          // activeItem2.classList.remove('dropdown__list_active-metraz');  
-          // activeItem2.classList.remove('dropdown__list_active');  
-          activeItem2.style.display = 'none';  
-        }
-      });
-      
-    });
+ 
 
     console.log('home');
     // document.cookie = "miasto=; inwestycja=; pokoje=; cenaOd=; cenaDo=;";
@@ -429,6 +416,21 @@ window.addEventListener("load", function() {
 
     // -------------------------------------------------------------------------------------
   };
+
+  document.body.addEventListener('click', () => {
+    document.querySelectorAll('.menu-passive').forEach(activeItem =>{
+      activeItem.style.display = 'none';  
+    });
+
+    document.querySelectorAll('.dropdown__list').forEach(activeItem2 =>{
+      if(activeItem2.classList.contains('dropdown__list_active-metraz') || activeItem2.classList.contains('dropdown__list_active')){
+        // activeItem2.classList.remove('dropdown__list_active-metraz');  
+        // activeItem2.classList.remove('dropdown__list_active');  
+        activeItem2.style.display = 'none';  
+      }
+    });
+    
+  });
 
 
 
