78fd061bfa0311e67c3580cbb89c0f5c9af807ba
Tomek <TK-54-4B>
2023-11-06 21:07:05 +0100

    removed mobile call bar

--- patterns/_pag-not-blog-posts.php
+++ patterns/_pag-not-blog-posts.php
@@ -11,22 +11,6 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="blog-post" class="wp-block-group page-blog-test relative">
   <!-- wp:group -->
-  <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
-    <!-- wp:group -->
-    <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_60px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
-      <!-- wp:image -->
-      <figure class="wp-block-image m-0">
-        <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
-      </figure>
-      <!-- /wp:image -->
-      <!-- wp:paragraph -->
-      <a href="tel:+48583407900" class="text-white ">(+48) 58 340-79-00</a>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-  <!-- wp:group -->
   <div class="wp-block-group call-back-modal-bg hidden desktop:block opacity-0 z-[-1] bg-[rgba(61,75,102,0.5)] fixed top-0 left-0 w-screen h-screen transition duration-300 ease">
   </div>
   <!-- /wp:group -->
@@ -63,23 +47,6 @@
     <div class="wp-block-group input checkbox">
       <!-- wp:paragraph -->
       <p class="text-bgDarkGray text-[12px] leading-[22px]">Wyrażam zgodę na używanie przez PB Górski sp. z o.o. S.K.A. telekomunikacyjnych urządzeń końcowych dla celów marketingu bezpośredniego, tj. aby przedstawić oferty PB Górski sp. z o.o. S.K.A. telefonicznie. Jednocześnie informujemy, że rozmowa będzie nagrywana. Administratorem Danych Osobowych jest PB Górski sp. z o.o. S.K.A. z siedzibą w Gdańsku (80-312) ul. Wita Stwosza 31A/31. tutaj</p>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
-  <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
-    <!-- wp:group -->
-    <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_60px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
-      <!-- wp:image -->
-      <figure class="wp-block-image m-0">
-        <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
-      </figure>
-      <!-- /wp:image -->
-      <!-- wp:paragraph -->
-      <a href="tel:+48583407900" class="text-white ">(+48) 58 340-79-00</a>
       <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -11,22 +11,6 @@
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
 <div id="Banner" class="wp-block-group banner h-[35vh] desktop:h-[80vh] relative">
   <!-- wp:group -->
-  <div class="wp-block-group mobile-fixed-phone block desktop:hidden flex justify-center items-center bg-primaryRed w-full h-[87px] fixed bottom-0 left-0 z-[2]">
-    <!-- wp:group -->
-    <div class="wp-block-group flex gap-[30px] items-center h-[47px] p-[5px_60px] rounded-full border-[2px] border-[rgba(255,255,255,0.33)]">
-      <!-- wp:image -->
-      <figure class="wp-block-image m-0">
-        <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/frontpage/phone-2.png'; ?>" alt="phone-icon">
-      </figure>
-      <!-- /wp:image -->
-      <!-- wp:paragraph -->
-      <a href="tel:+48583407900" class="text-white ">(+48) 58 340-79-00</a>
-      <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-  <!-- wp:group -->
   <div class="wp-block-group call-back-modal-bg hidden desktop:block opacity-0 z-[-1] bg-[rgba(61,75,102,0.5)] fixed top-0 left-0 w-screen h-screen transition duration-300 ease">
   </div>
   <!-- /wp:group -->
