50747b2e7bd02d6501c974cb6c8a4119d4332d1f
Tomek <TK-54-4B>
2023-11-07 00:56:35 +0100

    test

--- atcf7/contact-form2.php
+++ atcf7/contact-form2.php
@@ -9,8 +9,8 @@ if (!defined('ABSPATH')) {
 
 <table class="w-full cta2">
   <tbody>
-    <tr class="flex gap-[20px]">
-      <td class="w-[50%] leading-[0px]">
+    <tr class="flex flex-col desktop:flex-row gap-[20px]">
+      <td class="w-[100%] desktop:w-[50%] leading-[0px]">
         <!-- [text* your-name id:your-name ] -->
         <input id="your-name" name="your-name" type="text" placeholder="Imię i nazwisko" class="px-[15px] py-[10px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575] focus:border-white active:border-black drop-shadow-lg">
         <!-- <div> [email* your-email id:your-email ] </div> -->
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
         <!-- <div> [email* your-email id:your-email ] </div> -->
         <input id="your-phone" name="your-phone" type="text" placeholder="Numer telefonu" class="px-[15px] py-[10px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]">
       </td>
-      <td class="w-[50%]">
+      <td class="w-[100%] desktop:w-[50%]">
         <textarea id="your-name" cols="50" row="10" name="your-name" type="textarea" placeholder="Treść wiadomości" class="px-[15px] py-[10px] mb-[10px] h-[240px] text-[#2e384d] placeholder:text-[#757575]"></textarea>
       </td>
     </tr>
