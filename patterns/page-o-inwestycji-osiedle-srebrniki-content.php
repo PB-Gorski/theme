1278157222d928c14bb2d7cb5c61ebc9dc7d0853
Tomek <TK-54-4B>
2023-11-06 09:32:30 +0100

    test

--- patterns/page-o-inwestycji-osiedle-srebrniki-content.php
+++ patterns/page-o-inwestycji-osiedle-srebrniki-content.php
@@ -46,13 +46,13 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- wp:group -->
     <div class="wp-block-group row-content desktop:flex">
       <!-- wp:paragraph -->
-      <p class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+      <span class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
         <!-- wp:image -->
-      <figure class="wp-block-image w-full h-full">
-        <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/img1.jpeg" alt="img">
-      </figure>
-      <!-- /wp:image -->
-      </p>
+        <figure class="wp-block-image w-full h-full">
+          <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/img1.jpeg" alt="img">
+        </figure>
+        <!-- /wp:image -->
+      </span>
       <!-- /wp:paragraph -->
 
       <!-- wp:group -->
