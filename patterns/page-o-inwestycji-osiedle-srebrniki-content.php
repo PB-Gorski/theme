1c3b92e5c1012af1f78d1ecbbfb6968e3bcdf1a7
Tomek <TK-54-4B>
2023-11-06 09:55:56 +0100

    test

--- patterns/page-o-inwestycji-osiedle-srebrniki-content.php
+++ patterns/page-o-inwestycji-osiedle-srebrniki-content.php
@@ -122,7 +122,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- wp:group -->
     <div class="wp-block-group row-content2 desktop:flex flex-row-reverse">
       <!-- wp:group -->
-      <div class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0">
+      <div class="wp-block-group block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0">
         <!-- wp:image -->
         <figure class="wp-block-image w-full h-full">
           <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/img2.jpeg" alt="img">
@@ -148,7 +148,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
     <!-- wp:group -->
     <div class="wp-block-group row-content3 desktop:flex">
       <!-- wp:group -->
-      <div class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0">
+      <div class="wp-block-group block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0">
         <!-- wp:image -->
         <figure class="wp-block-image w-full h-full">
           <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/img3.jpeg" alt="img">
