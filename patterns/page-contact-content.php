dae05222881c0321808f145cd3e3051eb2f212ad
Tomek <TK-54-4B>
2023-11-06 23:05:36 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -460,7 +460,7 @@
                 <!-- wp:group -->
                 <div class="wp-block-group right-col-text text-black">
                   <!-- wp:paragraph -->
-                  <p class="text-[18px] mb-[25px]">Ewa Cel</p>
+                  <p class="text-[18px] mb-[25px]">Elżbieta Lipska</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:group -->
                   <div class="wp-block-group phone flex gap-[40px]">
@@ -477,14 +477,14 @@
                       <p class="block mb-[20px]">tel</p>
                       <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
-                      <a href="tel:+48690099044" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> (+48) 690 099 044</a>
+                      <a href="tel:+48691990926" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> (+48) 691 990 926</a>
                       <!-- /wp:paragraph -->
 
                       <!-- wp:paragraph -->
                       <p class="block">email:</p>
                       <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
-                      <a href="mailto:kredyty@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300"> kredyty@pbgorski.pl</a>
+                      <a href="mailto:e.lipska@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300"> e.lipska@pbgorski.pl</a>
                       <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
