028f510fb4fdff632f1cda7ce5a0214108dfcb94
Tomek <TK-54-4B>
2023-11-06 14:16:23 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -755,7 +755,7 @@ window.addEventListener("load", function() {
         priceMinUpdate = getCookie("PriceMinFromCookies");
         priceMaxUpdate = getCookie("PriceMaxFromCookies");
 
-        console.log('prices from cookies: ',priceMinUpdate, priceMaxTest);
+        console.log('prices from cookies: ',priceMinUpdate, priceMaxUpdate);
 
         document.querySelector('.dropdown__value-min').childNodes[0].innerHTML = priceMinUpdate;
         document.querySelector('.dropdown__value-max').childNodes[0].innerHTML = priceMaxUpdate;
