350b38b68e97223316aebd6237c3d95b0adf445e
Tomek <TK-54-4B>
2023-11-06 21:17:38 +0100

    test

--- patterns/investments.php
+++ patterns/investments.php
@@ -57,9 +57,9 @@
       ?>
           <!-- wp:list-item -->
           <li class="invest-tile max-w-full w-full inline-block">
-            <a href="<?php echo home_url() . '/o-inwestycji-' . $alias; ?>" class="inline-block" data-aos="fade-up" data-aos-offset="30" data-aos-delay="<?php echo $counter * 50; ?>">
+            <a href="<?php echo home_url() . '/o-inwestycji-' . $alias; ?>" class="inline-block w-full" data-aos="fade-up" data-aos-offset="30" data-aos-delay="<?php echo $counter * 50; ?>">
               <!-- wp:group -->
-              <div class="wp-block-group relative group desktop:max-w-full max-w-[310px] h-full">
+              <div class="wp-block-group relative group desktop:max-w-full  h-full">
                 <!-- wp:group -->
                 <div class="wp-block-group wrapper w-full h-full">
                   <!-- wp:image -->
