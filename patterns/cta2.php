444ccc2e4e66050426680fcdfd437e996798becd
Tomek <TK-54-4B>
2023-11-06 23:43:24 +0100

    test

--- patterns/cta2.php
+++ patterns/cta2.php
@@ -34,8 +34,9 @@
       <span class="block text-[28px] desktop:text-[38px] font-bold mb-[60px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Napisz!?</span>
       <!-- /wp:paragraph -->
 
-      <div class="wp-block-contact-form-7-contact-form-selector">[contact-form-7 id="3358ba5" title="test"]</div>
+      <div class="wp-block-contact-form-7-contact-form-selector">[contact-form-7 id="dfa8bcf" title="Wpis - mieszkanie"]</div>
       <!-- /wp:contact-form-7/contact-form-selector -->
+      <?php echo do_shortcode('[contact-form-7 id="dfa8bcf" title="Wpis - mieszkanie"]'); ?>
 
       <!-- wp:paragraph -->
       <span class="group btn-find-more block relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer" data-aos="fade-right" data-aos-offset="30"><span class="font-bold text-white z-[1]"><a href="<?php echo home_url() . '/contact' ?>"> Wyślij</a></span></span>
