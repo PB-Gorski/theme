28d2746bdab3bab1e4983bb8388c53a59a1b3cb8
Tomek <TK-54-4B>
2023-11-06 11:27:29 +0100

    test

--- patterns/page-o-inwestycji-torunska-16-content.php
+++ patterns/page-o-inwestycji-torunska-16-content.php
@@ -15,10 +15,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px]">
     <!-- wp:paragraph -->
-    <p class="py-[30px] desktop:py-0 desktop:pt-[60px] text-[36px] desktop:text-[60px] text-center font-bold" data-aos="fade-right" data-aos-offset="30">Toruńska 16</p>
+    <span class="block py-[30px] desktop:py-0 desktop:pt-[60px] text-[36px] desktop:text-[60px] text-center font-bold" data-aos="fade-right" data-aos-offset="30">Toruńska 16</span>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
-    <p class="pb-[30px] desktop:pb-[60px] text-[20px] text-[#8a8f99] desktop:text-[24px] text-center" data-aos="fade-right" data-aos-offset="30">Kameralna inwestycja zlokalizowana w Śródmieściu Gdańska</p>
+    <span class="block pb-[30px] desktop:pb-[60px] text-[20px] text-[#8a8f99] desktop:text-[24px] text-center" data-aos="fade-right" data-aos-offset="30">Kameralna inwestycja zlokalizowana w Śródmieściu Gdańska</span>
     <!-- /wp:paragraph -->
 
     <!-- wp:group -->
@@ -44,20 +44,20 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   <div class="wp-block-group row-content-container px-[20px] desktop:px-0">
     <!-- wp:group -->
     <div class="wp-block-group row-content desktop:flex">
-      <!-- wp:paragraph -->
-      <a href="javascript:void(0)" class="block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0 mb-[20px] desktop:mb-0" data-aos="fade-right" data-aos-offset="30">
+      <!-- wp:group -->
+      <div class="wp-block-group block cursor-default left-col desktop:w-1/2 m-0 mb-[20px] desktop:mb-0 mb-[20px] desktop:mb-0">
         <!-- wp:image -->
         <figure class="wp-block-image w-full h-full">
           <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-torunska-16/img1.jpeg" alt="img">
         </figure>
         <!-- /wp:image -->
-      </a>
-      <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
 
       <!-- wp:group -->
       <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:px-[100px] desktop:w-1/2 flex flex-col justify-center">
         <!-- wp:paragraph -->
-        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">W ramach inwestycji powstaną 2 kameralne budynki</p>
+        <span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">W ramach inwestycji powstaną 2 kameralne budynki</span>
         <!-- /wp:paragraph -->
 
         <!-- wp:group -->
@@ -65,10 +65,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
           <!-- wp:group -->
           <div class="wp-block-group info-box1 mb-[35px] desktop:mb-[30px]">
             <!-- wp:paragraph -->
-            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LOKALIZACJA</p>
+            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LOKALIZACJA</span>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
-            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">Gdańsk Śródmieście</p>
+            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">Gdańsk Śródmieście</span>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -76,10 +76,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
           <!-- wp:group -->
           <div class="wp-block-group info-box2 mb-[35px] desktop:mb-[30px]">
             <!-- wp:paragraph -->
-            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">TERMIN UKOŃCZENIA (BUDYNEK 1)</p>
+            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">TERMIN UKOŃCZENIA (BUDYNEK 1)</span>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
-            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">II kwartał 2024</p>
+            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">II kwartał 2024</span>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -88,10 +88,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
           <div class="wp-block-group info-box3 mb-[35px] desktop:mb-[30px]">
             <!-- wp:paragraph -->
 
-            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LICZBA MIESZKAŃ W SPRZEDAŻY</p>
+            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LICZBA MIESZKAŃ W SPRZEDAŻY</span>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
-            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">29</p>
+            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">29</span>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -99,10 +99,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
           <!-- wp:group -->
           <div class="wp-block-group info-box4 mb-[35px] desktop:mb-[30px]">
             <!-- wp:paragraph -->
-            <p class="mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">POWIERZCHNIA MIESZKAŃ</p>
+            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">POWIERZCHNIA MIESZKAŃ</span>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
-            <p class="text-[24px]" data-aos="fade-right" data-aos-offset="30">31 m<sup>2</sup> - 97 m<sup>2</sup></p>
+            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">31 m<sup>2</sup> - 97 m<sup>2</sup></span>
             <!-- /wp:paragraph -->
           </div>
           <!-- /wp:group -->
@@ -128,10 +128,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2 flex flex-col justify-center">
         <!-- wp:paragraph -->
-        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Apartamenty w pobliżu Motławy</p>
+        <span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Apartamenty w pobliżu Motławy</span>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="text-[18px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">W dwóch budynkach będzie jedynie 29 apartamentów, co gwarantuje prywatność oraz spokój. Mieszkania charakteryzuje optymalny rozkład pokoi, umożliwiający dostowanie przestrzeni do swoich potrzeb. Przestronne balkony, winda, podziemny garaż - to wszystko czeka na mieszkańców Toruńskiej 16, zlokalizowanej w pobliżu Motławy. </p>
+        <span class="block text-[18px] text-[#8a8f99]" data-aos="fade-right" data-aos-offset="30">W dwóch budynkach będzie jedynie 29 apartamentów, co gwarantuje prywatność oraz spokój. Mieszkania charakteryzuje optymalny rozkład pokoi, umożliwiający dostowanie przestrzeni do swoich potrzeb. Przestronne balkony, winda, podziemny garaż - to wszystko czeka na mieszkańców Toruńskiej 16, zlokalizowanej w pobliżu Motławy. </span>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -154,10 +154,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2">
         <!-- wp:paragraph -->
-        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Zamieszkaj w centrum Gdańska</p>
+        <span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Zamieszkaj w centrum Gdańska</span>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Toruńska 16 to miejsce idealne dla osób ceniących mieszkanie w centrum miasta. Spacerem w kilkanaście minut dojdziemy do Głównego Miasta, przystanku SKM czy galerii handlowej. Wszędzie będzie blisko!</p>
+        <span class="block text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Toruńska 16 to miejsce idealne dla osób ceniących mieszkanie w centrum miasta. Spacerem w kilkanaście minut dojdziemy do Głównego Miasta, przystanku SKM czy galerii handlowej. Wszędzie będzie blisko!</span>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
@@ -180,10 +180,10 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- wp:group -->
       <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[140px_100px] desktop:w-1/2">
         <!-- wp:paragraph -->
-        <p class="mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Sprawdź dostępne mieszkania</p>
+        <span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Sprawdź dostępne mieszkania</span>
         <!-- /wp:paragraph -->
         <!-- wp:paragraph -->
-        <p class="text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Znajdź odpowiednie mieszkanie już teraz - wybierz dogodny metraż dla siebie i rodziny.</p>
+        <span class="block text-[18px] text-[#8a8f99] mb-[30px]" data-aos="fade-right" data-aos-offset="30">Znajdź odpowiednie mieszkanie już teraz - wybierz dogodny metraż dla siebie i rodziny.</span>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
