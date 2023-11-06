9b6f792f1600bb4c8c8cd1013970307e2a88f9a8
Tomek <TK-54-4B>
2023-11-07 00:30:02 +0100

    test

--- atcf7/contact-form2.php
+++ atcf7/contact-form2.php
@@ -34,7 +34,7 @@ if (!defined('ABSPATH')) {
       <td class="">
         <!-- [checkbox* your-content x5 id:your-content ] -->
         <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="inline" />
-        <p class="ml-[10px] mb-[20px] inline text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        <p class="ml-[10px] mb-[20px] text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
     <tr>
@@ -42,7 +42,7 @@ if (!defined('ABSPATH')) {
       <td class=" gap-[6px]">
         <!-- [checkbox* your-content x5 id:your-content ] -->
         <input id="check2" name="check2" type="checkbox" class="inline" />
-        <p class="ml-[10px] inline text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        <p class="ml-[10px] text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
     <tr>
