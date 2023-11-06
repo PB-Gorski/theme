2d8b22986ae3ab7204e534cb6c080300db2fc6c1
Tomek <TK-54-4B>
2023-11-06 19:16:44 +0100

    test

--- patterns/page-blog-title.php
+++ patterns/page-blog-title.php
@@ -116,7 +116,7 @@
             global $post;
             echo $post->ID;
             echo 'get id:';
-            echo get_the_ID();
+            echo get_page_link();
             ?>
             <!-- wp:paragraph -->
             <a href="<?php echo get_home_url() . '/blog'; ?>" class="inline-block allTabBtn text-[#8A8F99] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">Wszystkie</a>
