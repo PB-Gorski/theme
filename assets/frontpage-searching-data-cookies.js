c42ca9d03fbc1043b61dc9937fe56e395b67ce78
Tomek <TK-54-4B>
2023-11-06 11:46:34 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", function() {
   //  frontpage - filtrowanie i sortowanie listy mieszkan
-  if(document.body.classList.contains('home')){
+  if(document.body.classList.contains('home') &&
+  document.body.classList.contains('page-parent') &&
+  document.body.classList.contains('page-child')){
     console.log('home');
     // document.cookie = "miasto=; inwestycja=; pokoje=; cenaOd=; cenaDo=;";
     function deleteAllCookies() {
