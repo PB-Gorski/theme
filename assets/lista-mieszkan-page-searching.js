304a79823bd9764338e721eaf7af26ad2343b38b
Tomek <TK-54-4B>
2023-11-06 12:19:09 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -516,15 +516,15 @@ window.addEventListener("load", function() {
     //           document.referrer == 'https://pbgorski.webo.design/pl/o-inwestycji-wiezycka-folwark/kronika-budowy-wiezycka-folwark/'){
     //           document.cookie = "filteredTermsFromCookies=81";
     // };
-    if (document.referrer.search('o-inwestycji-osiedle-srebrniki' > 20) ||
-        document.referrer.search('lokalizacja-osiedle-srebrniki' > 20) ||
-        document.referrer.search('galeria-osiedle-srebrniki' > 20) ||
-        document.referrer.search('kronika-budowy-osiedle-srebrniki' > 20)){
+    if (document.referrer.search('o-inwestycji-osiedle-srebrniki') > 20 ||
+        document.referrer.search('lokalizacja-osiedle-srebrniki') > 20 ||
+        document.referrer.search('galeria-osiedle-srebrniki') > 20 ||
+        document.referrer.search('kronika-budowy-osiedle-srebrniki') > 20){
         document.cookie = "filteredTermsFromCookies=72";
-    }else if (document.referrer.search('o-inwestycji-sw-piotra' > 20) ||
-        document.referrer.search('lokalizacja-sw-piotra' > 20) ||
-        document.referrer.search('galeria-sw-piotra' > 20) ||
-        document.referrer.search('kronika-budowy-sw-piotra' > 20)){
+    }else if (document.referrer.search('o-inwestycji-sw-piotra') > 20 ||
+        document.referrer.search('lokalizacja-sw-piotra' > 20 ||
+        document.referrer.search('galeria-sw-piotra') > 20 ||
+        document.referrer.search('kronika-budowy-sw-piotra') > 20)){
               document.cookie = "filteredTermsFromCookies=71";
     }else if (document.referrer == 'https://pbgorski.webo.design/pl/o-inwestycji-torunska-16/' ||
               document.referrer == 'https://pbgorski.webo.design/pl/o-inwestycji-torunska-16/lokalizacja-torunska-16/' ||
