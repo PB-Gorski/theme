266f5ce655ae7f3e418dd35c2da389ca64b3971a
Tomek <TK-54-4B>
2023-11-07 13:18:53 +0100

    test

--- filter-layout.php
+++ filter-layout.php
@@ -28,7 +28,7 @@ $tax_sort = $taxonomy;
       </div>
     </div>
 
-    <div class="js-heightAnimation filterContainer h-[720px]">
+    <div class="js-heightAnimation filterContainer transition-height h-[720px]">
       <div class="filter-entry container mx-auto justify-center" data-terms="<?php echo esc_attr($all_terms); ?>">
         <?php
         $type_multiple = ((bool) $ymc_multiple_filter) ? 'multiple' : '';
