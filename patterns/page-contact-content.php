d6ca8e56f84272cb01f3635b1e714ed2f1e7add7
Tomek <TK-54-4B>
2023-11-07 08:31:09 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -209,11 +209,11 @@
         <!-- wp:list -->
         <ul class="wp-block-list contact-drop-down mt-[50px] ml-0">
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full w-full text-bgDarkGray flex transition ease-out duration-[500ms]">
+              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex transition ease-out duration-[500ms]">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] pb-[5px] flex-none click-bar-icon py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -303,11 +303,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex">
+              <div class="wp-block-group bar-title-wrapper text-bgDarkGray flex">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] click-bar-icon flex-none pb-[5px] py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -322,7 +322,7 @@
             <!-- wp:list -->
             <ul class="wp-block-group step-paragraph paragraph-wrapper px-[20px] overflow-hidden   transition-height ease-out duration-[500ms] contact-inactive">
               <!-- wp:list-item -->
-              <li class="flex items-center gap-[20px] desktop:gap-[40px] leading-[21px] mb-[30px]">
+              <li class="flex items-center gap-[40px] leading-[21px] mb-[30px]">
                 <!-- wp:group -->
                 <div class="wp-block-group left-col-avatar">
                   <!-- wp:image -->
@@ -336,43 +336,40 @@
                 <!-- wp:group -->
                 <div class="wp-block-group right-col-text text-black">
                   <!-- wp:paragraph -->
-                  <p class="text-[18px] mb-[20px]">Kinga Piekarska</p>
+                  <p class="text-[18px] mb-[25px]">Kinga Piekarska</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:group -->
-                  <div class="wp-block-group flex gap-[40px] text-[16px]">
+                  <div class="wp-block-group phone flex gap-[40px]">
+                    <!-- wp:group -->
+                    <div class="wp-block-group labels">
+                      <!-- wp:paragraph -->
+                      <p class="block mb-[20px]">tel</p>
+                      <!-- /wp:paragraph -->
+                      <!-- wp:paragraph -->
+                      <p class="block">email:</p>
+                      <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->
+
                     <!-- wp:group -->
                     <div class="wp-block-group values">
-                      <!-- wp:group -->
-                      <div class="wp-block-group desktop:flex desktop:gap-[40px]">
-                        <!-- wp:paragraph -->
-                        <p class="block mb-[5px]">tel.</p>
-                        <!-- /wp:paragraph -->
-                        <!-- wp:paragraph -->
-                        <a href="tel:+48607700555" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 607 700 555</a>
-                        <!-- /wp:paragraph -->
-                      </div>
-                      <!-- wp:group -->
-                      <div class="wp-block-group desktop:flex desktop:gap-[16px]">
-                        <!-- wp:paragraph -->
-                        <p class="block mb-[5px]">email:</p>
-                        <!-- /wp:paragraph -->
-                        <!-- wp:paragraph -->
-                        <a href="mailto:k.piekarska@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300">k.piekarska@pbgorski.pl </a>
-                        <!-- /wp:paragraph -->
-                      </div>
-                      <!-- /wp:group -->
+                      <!-- wp:paragraph -->
+                      <a href="tel:+48607700555" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 607 700 555</a>
+                      <!-- /wp:paragraph -->
+
+                      <!-- wp:paragraph -->
+                      <a href="mailto:k.piekarska@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300">k.piekarska@pbgorski.pl </a>
+                      <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
                   </div>
                   <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
-
               </li>
               <!-- /wp:list-item -->
-
               <!-- wp:list-item -->
-              <li class="flex items-center gap-[20px] desktop:gap-[40px] leading-[21px]">
+              <li class="flex items-center gap-[40px] leading-[21px]">
                 <!-- wp:group -->
                 <div class="wp-block-group left-col-avatar">
                   <!-- wp:image -->
@@ -386,38 +383,36 @@
                 <!-- wp:group -->
                 <div class="wp-block-group right-col-text text-black">
                   <!-- wp:paragraph -->
-                  <p class="text-[18px] mb-[20px]">Bartłomiej Jahn</p>
+                  <p class="text-[18px] mb-[25px]">Bartłomiej Jahn</p>
                   <!-- /wp:paragraph -->
                   <!-- wp:group -->
-                  <div class="wp-block-group flex gap-[40px] text-[16px]">
+                  <div class="wp-block-group phone flex gap-[40px]">
+                    <!-- wp:group -->
+                    <div class="wp-block-group labels">
+                      <!-- wp:paragraph -->
+                      <p class="block mb-[20px]">tel</p>
+                      <!-- /wp:paragraph -->
+                      <!-- wp:paragraph -->
+                      <p class="block">email:</p>
+                      <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->
+
                     <!-- wp:group -->
                     <div class="wp-block-group values">
-                      <!-- wp:group -->
-                      <div class="wp-block-group desktop:flex desktop:gap-[40px]">
-                        <!-- wp:paragraph -->
-                        <p class="block mb-[5px]">tel.</p>
-                        <!-- /wp:paragraph -->
-                        <!-- wp:paragraph -->
-                        <a href="tel:+48691992538" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300">691 992 538</a>
-                        <!-- /wp:paragraph -->
-                      </div>
-                      <!-- wp:group -->
-                      <div class="wp-block-group desktop:flex desktop:gap-[16px]">
-                        <!-- wp:paragraph -->
-                        <p class="block mb-[5px]">email:</p>
-                        <!-- /wp:paragraph -->
-                        <!-- wp:paragraph -->
-                        <a href="mailto:b.jahn@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300">b.jahn@pbgorski.pl</a>
-                        <!-- /wp:paragraph -->
-                      </div>
-                      <!-- /wp:group -->
+                      <!-- wp:paragraph -->
+                      <a href="tel:+48691992538" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 691 992 538</a>
+                      <!-- /wp:paragraph -->
+
+                      <!-- wp:paragraph -->
+                      <a href="mailto:b.jahn@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[2e384d] transition ease duration-300"> b.jahn@pbgorski.pl </a>
+                      <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
                   </div>
                   <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
-
               </li>
               <!-- /wp:list-item -->
             </ul>
@@ -425,11 +420,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex">
+              <div class="wp-block-group bar-title-wrapper text-bgDarkGray flex">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] click-bar-icon flex-none pb-[5px] py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -495,11 +490,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex">
+              <div class="wp-block-group bar-title-wrapper text-bgDarkGray flex">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] click-bar-icon flex-none pb-[5px] py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -565,11 +560,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex">
+              <div class="wp-block-group bar-title-wrapper text-bgDarkGray flex">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] click-bar-icon flex-none pb-[5px] py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -635,11 +630,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex">
+              <div class="wp-block-group bar-title-wrapper text-bgDarkGray flex">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] click-bar-icon flex-none pb-[5px] py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
@@ -709,11 +704,11 @@
           </li>
           <!-- /wp:list-item -->
           <!-- wp:list-item -->
-          <li class="list-content-wrapper w-full transition ease-out duration-[500ms]">
+          <li class="list-content-wrapper transition ease-out duration-[500ms]">
             <!-- wp:group -->
             <div class="wp-block-group click-bar group flex justify-between items-center cursor-pointer">
               <!-- wp:group -->
-              <div class="wp-block-group bar-title-wrapper w-full w-full text-bgDarkGray flex transition ease-out duration-[500ms]">
+              <div class="wp-block-group bar-title-wrapper w-full text-bgDarkGray flex transition ease-out duration-[500ms]">
                 <!-- wp:paragraph -->
                 <span class="block w-[22px] pb-[5px] flex-none click-bar-icon py-[20px] border-b-[1px] border-[#e8e8e8] group-hover:text-primaryYellow uppercase text-[16px] desktop:text-[35px] font-semibold leading-[30px] transition ease-out duration-500" data-aos="fade-up" data-aos-offset="30">+</span>
                 <!-- /wp:paragraph -->
