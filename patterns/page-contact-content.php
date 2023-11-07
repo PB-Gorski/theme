62d6089cc3d044256826540b8c8f3bf071c1bbfe
Tomek <TK-54-4B>
2023-11-07 08:17:33 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -340,26 +340,29 @@
                   <!-- /wp:paragraph -->
                   <!-- wp:group -->
                   <div class="wp-block-group phone flex gap-[40px] text-[16px]">
-                    <!-- wp:group -->
-                    <div class="wp-block-group labels">
-                      <!-- wp:paragraph -->
-                      <p class="block mb-[20px]">tel</p>
-                      <!-- /wp:paragraph -->
-                      <!-- wp:paragraph -->
-                      <p class="block">email:</p>
-                      <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:group -->
+
 
                     <!-- wp:group -->
                     <div class="wp-block-group values">
-                      <!-- wp:paragraph -->
-                      <a href="tel:+48607700555" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 607 700 555</a>
-                      <!-- /wp:paragraph -->
-
-                      <!-- wp:paragraph -->
-                      <a href="mailto:k.piekarska@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300">k.piekarska@pbgorski.pl </a>
-                      <!-- /wp:paragraph -->
+                      <!-- wp:group -->
+                      <div class="wp-block-group desktop:flex desktop:gap-[40px]">
+                        <!-- wp:paragraph -->
+                        <p class="block mb-[5px]">tel.</p>
+                        <!-- /wp:paragraph -->
+                        <!-- wp:paragraph -->
+                        <a href="tel:+48607700555" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 607 700 555</a>
+                        <!-- /wp:paragraph -->
+                      </div>
+                      <!-- wp:group -->
+                      <div class="wp-block-group desktop:flex desktop:gap-[16px]">
+                        <!-- wp:paragraph -->
+                        <p class="block mb-[5px]">email:</p>
+                        <!-- /wp:paragraph -->
+                        <!-- wp:paragraph -->
+                        <a href="mailto:k.piekarska@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[#2e384d] transition ease duration-300">k.piekarska@pbgorski.pl </a>
+                        <!-- /wp:paragraph -->
+                      </div>
+                      <!-- /wp:group -->
                     </div>
                     <!-- /wp:group -->
                   </div>
