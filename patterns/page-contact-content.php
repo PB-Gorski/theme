977733a2df57984cd1b3686241c07dc88765c5bf
Tomek <TK-54-4B>
2023-11-07 08:38:40 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -675,9 +675,9 @@
                       <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
                       <a href="tel:583407900" class="mb-[20px] inline-block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> (+48) 58 340-79-00, </a>
+                      <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
                       <a href="tel:+48693034715" class="mb-[20px] inline-block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 693 034 715</a>
-                      <!-- /wp:paragraph -->
                       <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
                       <p class="block">email:</p>
