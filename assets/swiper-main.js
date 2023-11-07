b7fc31283881fada6fc5350f196df3e1cc975f96
Tomek <TK-54-4B>
2023-11-07 15:31:08 +0100

    test

--- assets/swiper-main.js
+++ assets/swiper-main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         nextEl: swiperNode.querySelector(".js-swiper-button-next"),
         prevEl: swiperNode.querySelector(".js-swiper-button-prev"),
       },
-      slidesPerView: 3.2,
+      slidesPerView: 2.2,
       spaceBetween: 20,
       keyboardControl: true,
     });
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       
         swiperNode.parentNode.querySelector('.close-button').addEventListener('click', function(event) {
           let params2 = swiperInstance.params;
-          params2.slidesPerView = 3.2;
+          params2.slidesPerView = 2.2;
           swiperInstance.slideTo(clickedSlideIndex,0);
           swiperInstance.update();
           swiperNode.parentNode.classList.remove('fullscreen');
