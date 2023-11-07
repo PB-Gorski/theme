0cd363ba078c6143f2cb1675fbbb9a128301633b
Tomek <TK-54-4B>
2023-11-07 08:56:35 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -388,22 +388,17 @@
                   <!-- wp:group -->
                   <div class="wp-block-group phone flex gap-[40px]">
                     <!-- wp:group -->
-                    <div class="wp-block-group labels">
+                    <div class="wp-block-group values">
+                      <!-- wp:paragraph -->
                       <!-- wp:paragraph -->
                       <p class="block mb-[20px]">tel</p>
                       <!-- /wp:paragraph -->
-                      <!-- wp:paragraph -->
-                      <p class="block">email:</p>
-                      <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:group -->
-
-                    <!-- wp:group -->
-                    <div class="wp-block-group values">
-                      <!-- wp:paragraph -->
                       <a href="tel:+48691992538" class="mb-[20px] block font-bold underline decoration-2 hover:decoration-[#acafb8] transition ease duration-300"> 691 992 538</a>
                       <!-- /wp:paragraph -->
 
+                      <!-- wp:paragraph -->
+                      <p class="block">email:</p>
+                      <!-- /wp:paragraph -->
                       <!-- wp:paragraph -->
                       <a href="mailto:b.jahn@pbgorski.pl" class="block font-bold underline decoration-2 hover:decoration-[2e384d] transition ease duration-300"> b.jahn@pbgorski.pl </a>
                       <!-- /wp:paragraph -->
