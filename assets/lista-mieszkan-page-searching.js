d676e3ca2bcdbfbda909f7a365534e6355b955bd
Tomek <TK-54-4B>
2023-11-08 22:35:02 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -782,7 +782,9 @@ window.addEventListener("load", function() {
         // console.log('prices from cookies: ',priceMinUpdate, priceMaxUpdate);
 
         document.querySelector('.dropdown__value-min').childNodes[0].innerHTML = filteredPriceMinFromCookies == '' ? 0 : filteredPriceMinFromCookies;
-        document.querySelector('.dropdown__value-max').childNodes[0].innerHTML = filteredPriceMaxFromCookies == 10000000 ? 'Max' :  filteredPriceMaxFromCookies;
+        document.querySelector('.dropdown__value-max').childNodes[0].innerHTML = filteredPriceMaxFromCookies == '' ? 'Max' : (filteredPriceMaxFromCookies == 10000000 ? 'Max' :  filteredPriceMaxFromCookies);
+        
+        
         document.querySelector('.dropdown__value-min-metraz').childNodes[0].innerHTML = filteredMetrazMinFromCookies;
         document.querySelector('.dropdown__value-max-metraz').childNodes[0].innerHTML = filteredMetrazMaxFromCookies == 10000000 ? 'Max' :  filteredMetrazMaxFromCookies;
           
