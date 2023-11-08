f5153d0b5a5ee3dc15e84f1a2938f5276769e395
Tomek <TK-54-4B>
2023-11-08 13:20:10 +0100

    test

---
+++ patterns/page-polityka-cookies.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Title: polityka-cookies
+ * Slug: polityka-cookies
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="Banner" class="wp-block-group banner-polityka-cookies relative">
+
+  <!-- wp:group -->
+  <div class="wp-block-group w-full bg-[#f5f7fa] relative py-[80px]">
+    <!-- wp:group -->
+    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pl-[50px]">
+      <!-- wp:group -->
+      <div class="wp-block-group title text-[36px] desktop:text-[60px] font-bold mb-[50px] leading-[50px] desktop:leading-[70px]">
+        <!-- wp:paragraph -->
+        <span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">Polityka cookies</span>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
