4f7ab743ddc6e522c95de4dcc1c6d3e9cbf85fe5
Tomek <TK-54-4B>
2023-11-09 20:01:16 +0100

    test

--- filter-layout2.php
+++ filter-layout2.php
@@ -333,7 +333,7 @@ $tax_sort = $taxonomy;
           <div class="dropdown-filter flex-col bg-white">
             <p class="dropdown-filter-title"><?php echo get_taxonomy($select_term)->label . ':' ?></p>
             <div class="menu-active font-bold">
-              Wybierz<?php get_taxonomy($select_term)->label ?><span class="arrow-down"></span>
+              <span class="arrow-down">Wybierz<?php get_taxonomy($select_term)->label ?></span>
             </div>
             <div class="menu-passive">
               <!-- <i class="btn-close">x</i> -->
