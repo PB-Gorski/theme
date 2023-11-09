816017e68c5d20f60373bf9ead3891c823f1a70b
Tomek <TK-54-4B>
2023-11-09 09:35:06 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -337,20 +337,7 @@ window.addEventListener("load", function() {
       item.addEventListener('click', choiseMetraz)
     });
 
-    document.body.addEventListener('click', () => {
-      document.querySelectorAll('.menu-passive').forEach(activeItem =>{
-        activeItem.style.display = 'none';  
-      });
-  
-      document.querySelectorAll('.dropdown__list').forEach(activeItem2 =>{
-        if(activeItem2.classList.contains('dropdown__list_active-metraz') || activeItem2.classList.contains('dropdown__list_active')){
-          activeItem2.classList.remove('dropdown__list_active-metraz');  
-          activeItem2.classList.remove('dropdown__list_active');  
-          activeItem2.style.display = 'none';  
-        }
-      });
-      
-    });
+
 
     function choiseMetraz(e){
       e.preventDefault();
@@ -1141,6 +1128,27 @@ window.addEventListener("load", function() {
           }
         });
       })
+    });
+
+    document.body.addEventListener('click', () => {
+      console.log('body click');
+      document.querySelectorAll('.menu-passive').forEach(activeItem =>{
+        activeItem.style.display = 'none';  
+      });
+  
+      let closeDropDownCouter = 0
+      document.querySelectorAll('.dropdown__list').forEach(activeItem2 =>{
+        if(activeItem2.classList.contains('dropdown__list_active-metraz') || activeItem2.classList.contains('dropdown__list_active')){
+          if(closeDropDownCouter>0){
+
+            activeItem2.classList.remove('dropdown__list_active-metraz');  
+            activeItem2.classList.remove('dropdown__list_active');  
+            activeItem2.style.display = 'none';  
+          }
+        }
+      });
+
+      closeDropDownCouter++;
     });
 
 
