c62101693241e93b6ccdbb9f99661d903bfb0517
Tomek <TK-54-4B>
2023-11-09 16:10:40 +0100

    tesst

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -495,6 +495,24 @@ window.addEventListener("load", function() {
     //     });
     //   })
     // });
+
+    document.body.addEventListener('click', () => {
+      console.log('body click');
+      document.querySelectorAll('.menu-passive').forEach(activeItem =>{
+        activeItem.style.display = 'none';  
+
+      });
+  
+      // document.querySelectorAll('.dropdown__list').forEach(activeItem2 =>{
+      //   if(activeItem2.classList.contains('dropdown__list_active-metraz') || activeItem2.classList.contains('dropdown__list_active')){
+      //     activeItem2.classList.remove('dropdown__list_active-metraz');  
+      //     activeItem2.classList.remove('dropdown__list_active');  
+      //     activeItem2.style.display = 'none';  
+      //   }
+      // });
+  
+  
+    });
     
 
     // -------------------------------------------------------------------------------------
