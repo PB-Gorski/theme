255921b8772b7af604020f95bfcd10d0a858a53a
Tomek <TK-54-4B>
2023-11-09 08:28:41 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -620,7 +620,7 @@ window.addEventListener("load", function() {
         let returnChoosenFromCookiesPietro = [];
         let returnChoosenFromCookiesTermin = [];
         let returnChoosenFromCookiesInne = [];
-        tempArr.join(',').split(',').join(',').split(',').forEach(item=>{
+        tempArr.join(',').split(',').forEach(item=>{
           // console.log('item form filtered cookies: ', item);
 
           if(miastoArr.includes(+item)){
