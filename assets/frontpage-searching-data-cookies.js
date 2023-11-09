0e6e5b7dfc844566be801c3b4cde154a37ecb09f
Tomek <TK-54-4B>
2023-11-09 16:29:53 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -166,28 +166,16 @@ window.addEventListener("load", function() {
             menuActiveSpan.innerHTML = 'Wybrano1: ' + (choosenPokojeArr.join(', ')!=' ' ? choosenPokojeArr.join(', ') : 'Wybrano2: 0');
           }else if(pietroArr.includes(+choosenOptionID)){
             // pietro
-            if(tempPietroArr.includes(item)){removeItemAll(tempPietroArr,item);}else{tempPietroArr.push(item)};
+            // pietro
+            if(tempPietroArr.includes(choosenOptionID)){removeItemAll(tempPietroArr,choosenOptionID);}else{tempPietroArr.push(choosenOptionID)};
 
-            choosenPietroArr = [];
             tempPietroArr.forEach(choosenItem =>{
               passiveOptions.forEach(item => {
-                if(item.childNodes[1].dataset.termid == choosenItem){
-                  choosenPietroArr.push(item.childNodes[1].dataset.name);
-                  item.childNodes[1].classList.add('active');
-                }
+                if(item.childNodes[1].dataset.termid == choosenItem){choosenPietroArr.push(item.childNodes[1].dataset.name)}
               });
             });
-            
-            passiveOptions.forEach(option => {
-              if(option.childNodes[1].dataset.termid == item){
-                console.log('option from cookies for finding parent: ',option);
-                returnChoosenFromCookiesPietro.push(option)
-              };              
-            });
 
-            returnChoosenFromCookiesPietro.forEach(el => {
-              el.parentNode.previousElementSibling.childNodes[1].innerHTML = choosenPietroArr.join(', ');
-            });
+            menuActiveSpan.innerHTML = 'Wybrano1: ' + (choosenPietroArr.join(', ')!=' ' ? choosenPietroArr.join(', ') : 'Wybrano2: 0');
           }else if(terminArr.includes(+choosenOptionID)){
             // termin
             if(tempTerminArr.includes(choosenOptionID)){removeItemAll(tempTerminArr,choosenOptionID);}else{tempTerminArr.push(choosenOptionID)};
