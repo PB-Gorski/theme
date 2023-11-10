f8b61e88f03703ff5d055e705d6a0b32c9740347
Tomek <TK-54-4B>
2023-11-10 16:15:41 +0100

    test

---
+++ patterns/page-lista-mieszkan-sw-piotra-filtr.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Title: page-lista-mieszkan-sw-piotra-filtr
+ * Slug: page-lista-mieszkan-sw-piotra-filtr
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group content bg-[#ebeef2] relative">
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:list -->
+    <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
+      <!-- wp:list-item -->
+      <li class="short-code-ymc-filter applyContainer1 applyContainer2">
+        <?php echo do_shortcode('[ymc_filter id="148"]'); ?>
+      </li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:list -->
+    <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
+      <!-- wp:list-item -->
+      <li class="short-code-ymc-filter applyContainer1 applyContainer2">
+        <?php echo do_shortcode('[ymc_filter id="148"]'); ?>
+      </li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
