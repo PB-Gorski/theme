eae6b142e93ba6ea9e6ce88564ce87d5d391f1ff
Tomek <TK-54-4B>
2023-11-10 14:14:25 +0100

    test

--- patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
+++ patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
@@ -16,7 +16,7 @@
     <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
       <!-- wp:list-item -->
       <li class="short-code-ymc-filter applyContainer1 applyContainer2">
-        <?php echo do_shortcode('[ymc_filter id="1928"]'); ?>
+
       </li>
       <!-- /wp:list-item -->
     </ul>
