91a1b6a115c4d5b4e21e84d297682b23981ff28b
Tomek <TK-54-4B>
2023-11-10 11:23:13 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -504,7 +504,7 @@ window.addEventListener("load", function() {
     document.querySelectorAll('.menu-passive__item').forEach(passive =>{
       passive.addEventListener('click', (e) => {
         e.target.parentNode.parentNode.style.display = 'none';
-        e.target.classList.toggle('newAfter');
+        e.target.parentNode.parentNode.previousElementSibling.childNodes[4].classList.toggle('newAfter');
 
       })
   
