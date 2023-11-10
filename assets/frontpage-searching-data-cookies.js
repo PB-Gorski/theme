5ae4525c08546a1c15f98c0d7ffbc940569a75b5
Tomek <TK-54-4B>
2023-11-10 09:06:22 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -532,15 +532,17 @@ window.addEventListener("load", function() {
 
         // allDropsArrr
 
-        var thirdNav = document.querySelectorAll('.dropdown__list')
+        let thirdNav = document.querySelectorAll('.dropdown__list')
         for (var i = 0; i < thirdNav.length; i++){
             thirdNav[i].addEventListener("click", thirdNavFuntion);
         }
 
         function thirdNavFuntion() {
+          console.log('third fun');
           const elements = Array.from(thirdNav).filter(el => el !== this)
           elements.forEach(el => el.parentElement.className = "hidden")
         }
+
       })
     }); 
 
