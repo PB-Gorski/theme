865ddf5d343e70006672dd23d757bea720c736ec
Tomek <TK-54-4B>
2023-11-10 15:04:14 +0100

    test

--- patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
+++ patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
@@ -21,6 +21,15 @@
       <!-- /wp:list-item -->
     </ul>
     <!-- /wp:list -->
+    <!-- wp:list -->
+    <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
+      <!-- wp:list-item -->
+      <li class="short-code-ymc-filter applyContainer1 applyContainer2">
+        <?php echo do_shortcode('[ymc_filter id="148"]'); ?>
+      </li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
   </div>
   <!-- /wp:group -->
 </div>
