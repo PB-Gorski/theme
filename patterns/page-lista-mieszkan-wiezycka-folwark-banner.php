aee8c4a659caf443ce6d94e903d85695394fe068
Tomek <TK-54-4B>
2023-11-10 17:02:51 +0100

    test

---
+++ patterns/page-lista-mieszkan-wiezycka-folwark-banner.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Title: lista-mieszkan-wiezycka-folwark-banner
+ * Slug: lista-mieszkan-wiezycka-folwark-banner
+ * Categories: Banner, Front Page
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="Banner" class="wp-block-group banner-lista-mieszkan-wiezycka-folwark relative">
+  <!-- wp:group -->
+  <div class="wp-block-group w-full h-[480px] desktop:h-[600px] relative py-[60px]">
+    <!-- wp:image -->
+    <figure class="wp-block-image w-full h-full absolute top-0 left-0 z-[-1]">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-wiezycka-folwark/wiezycka-folwark-bg2.jpg" alt="banner-bg">
+    </figure>
+    <!-- /wp:image -->
+    <!-- wp:group -->
+    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pt-[100px]">
+      <!-- wp:group -->
+      <div class="wp-block-group title-wrapper">
+        <!-- wp:group -->
+        <div class="wp-block-group title text-white text-[36px] desktop:text-[60px] font-bold mb-[20px] leading-[45px] desktop:leading-[70px]">
+          <!-- wp:paragraph -->
+          <p data-aos="fade-right" data-aos-offset="30">Wieżycka Folwark - zobacz dostępne mieszkania</p>
+          <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+      </div>
+      <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
