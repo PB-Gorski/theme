7baa08eaa646c652d8462b5116970359c4a7eb95
Tomek <TK-54-4B>
2023-11-11 21:17:30 +0100

    test

--- patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
+++ patterns/page-lista-mieszkan-osiedle-srebrniki-filtr.php
@@ -12,6 +12,26 @@
 <div id="content" class="wp-block-group content bg-[#ebeef2] relative">
   <!-- wp:group -->
   <div class="wp-block-group">
+    <!-- wp:group -->
+    <div class="wp-block-group test row-links my-[40px] flex flex-wrap justify-center items-center gap-[20px]">
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/o-inwestycji' ?>" class="text-[#8a8f99] hover:text-[#3e4759]" data-aos="fade-up" data-aos-offset="30">O inwestycji</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lokalizacja-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Lokalizacja</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/lista-mieszkan-osiedle-srebrniki' ?>" class="" data-aos="fade-up" data-aos-offset="30">Lista mieszkań</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/galeria-osiedle-srebrniki' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Galeria</a>
+      <!-- /wp:paragraph -->
+      <!-- wp:paragraph -->
+      <a href="<?php echo home_url() . '/kronika-budowy' ?>" class="text-[#8a8f99] hover:text-textGray" data-aos="fade-up" data-aos-offset="30">Kronika budowy</a>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
     <!-- wp:list -->
     <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
       <!-- wp:list-item -->
