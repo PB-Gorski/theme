ff73ca2f96e32f285f588f19c7762cb66fbc15d5
Tomek <TK-54-4B>
2023-11-13 22:10:54 +0100

    test

--- assets/single-mieszkania-cookie-data.js
+++ assets/single-mieszkania-cookie-data.js
@@ -4,10 +4,14 @@ window.addEventListener("load", function() {
     console.log('single mieszkania');
 
     let inwestycjaName = document.querySelector('.taxonomy-inwestycja').childNodes[0].innerHTML;
+    let btnBackToMieszkaniaList = document.querySelector('.js-backToList');
 
     console.log('Strona inwestycji: ',inwestycjaName);
 
     document.cookie = "filteredTermsFromCookies=72;";
+
+    btnBackToMieszkaniaList.href = window.location.origin + '/pl/o-inwestycji-osiedle-srebrniki/lista-mieszkan-osiedle-srebrniki/'
+
 
   
   
