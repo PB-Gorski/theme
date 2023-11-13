4d34ec934dc95912259567851391f0da76e70ad1
Tomek <TK-54-4B>
2023-11-13 08:54:36 +0100

    test

--- patterns/archive-inwestycje-gdansk.php
+++ patterns/archive-inwestycje-gdansk.php
@@ -58,7 +58,7 @@ $my_terms    = wp_get_object_terms($my_post_ids, 'inwestycja');
             $catNoSpaces = str_replace(' ', '-', strtolower($cat->name));
           ?>
             <!-- wp:list-item -->
-            <li class="CityTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer <?php echo ($currentCategory == $catNoSpaces) ? 'tab-active' : ''; ?>"><a href="<?php echo home_url() . '/inwestycje-' . $catNoSpaces; ?>" class=""><?php echo $cat->name; ?></a></li>
+            <li class="CityTabBtn text-[#959ba6] hover:text-textGray hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer <?php echo ($currentCategory == $catNoSpaces) ? 'tab-active' : ''; ?>"><a href="<?php echo home_url() . '/inwestycje-' . $catNoSpaces; ?>"><?php echo $cat->name; ?></a></li>
             <!-- /wp:list-item -->
           <?php }; ?>
         </ul>
