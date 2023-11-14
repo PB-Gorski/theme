b4a477a86640d687fcd3bdf21877375259a5351b
Tomek <TK-54-4B>
2023-11-14 12:59:51 +0100

    test

--- assets/o-inwestycji-lista-mieszkan-page-searching.js
+++ assets/o-inwestycji-lista-mieszkan-page-searching.js
@@ -1282,28 +1282,39 @@ window.addEventListener("load", function() {
     });
 
     wp.hooks.addAction('ymc_after_loaded_data_148_2', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     });
 
     wp.hooks.addAction('ymc_after_loaded_data_148_3', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     });
     wp.hooks.addAction('ymc_before_loaded_data_148_3', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     });
 
     if(document.body.classList.contains('page-id-1711')){
+      document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     };
     if(document.body.classList.contains('page-id-1926')){
+      document.cookie = "filteredTermsFromCookies=; path=";
+      document.cookie = "filteredTermsFromCookies=; path=/";
+      document.cookie = "filteredTermsFromCookies=; path=/pl";
+      console.log('cookies cleared');
+    };
+    if(document.body.classList.contains('post-type-archive-mieszkania')){
+      document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
