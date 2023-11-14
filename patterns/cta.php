d40983775331f24f98436811159d357659cde4fc
Tomek <TK-54-4B>
2023-11-14 09:40:01 +0100

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -28,9 +28,9 @@
 
     <!-- wp:group -->
     <div class="wp-block-group col2 w-[100%] desktop:w-[55%] py-[60px] fflex flex-col gap-[30px] items-center jusify-center">
-      <!-- wp:paragraph -->
-      <p class="text-[28px] desktop:text-[38px] font-bold mb-[60px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</p>
-      <!-- /wp:paragraph -->
+      <!-- wp:heading -->
+      <h2><span class="text-[28px] desktop:text-[38px] font-bold mb-[60px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
+      <!-- /wp:heading -->
 
       <!-- wp:group -->
       <div class="wp-block-group inner-cols-wrapper mb-[10px] desktop:h-[100px] desktop:flex">
