2b91d926aca60ee73647dbd1ad4f73eec5f9776c
Tomek <TK-54-4B>
2023-11-14 12:15:07 +0100

    test

--- patterns/page-o-inwestycji-torunska-16-content.php
+++ patterns/page-o-inwestycji-torunska-16-content.php
@@ -56,54 +56,53 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
 
       <!-- wp:group -->
       <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:px-[100px] desktop:w-1/2 flex flex-col justify-center">
-    <!-- wp:heading {"level":4} -->
-    <h4><span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">W ramach inwestycji powstaną 2 kameralne budynki</span></h4>
+        <!-- wp:heading {"level":4} -->
+        <h4><span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">W ramach inwestycji powstaną 2 kameralne budynki</span></h4>
         <!-- /wp:heading -->
 
         <!-- wp:group -->
         <div class="wp-block-group mb-[30px] desktop:mb-[100px] desktop:mb-0 desktop:grid grid-rows-2 grid-cols-2 gap-[30px]">
           <!-- wp:group -->
           <div class="wp-block-group info-box1 mb-[35px] desktop:mb-[30px]">
-            <!-- wp:paragraph -->
-            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LOKALIZACJA</span>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">Gdańsk Śródmieście</span>
-            <!-- /wp:paragraph -->
+            <!-- wp:heading {"level":6} -->
+            <h6><span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LOKALIZACJA</span></h6>
+            <!-- /wp:heading -->
+            <!-- wp:heading {"level":5} -->
+            <h5><span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">Gdańsk Śródmieście</span></h5>
+            <!-- /wp:heading -->
           </div>
           <!-- /wp:group -->
 
           <!-- wp:group -->
           <div class="wp-block-group info-box2 mb-[35px] desktop:mb-[30px]">
-            <!-- wp:paragraph -->
-            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">TERMIN UKOŃCZENIA (BUDYNEK 1)</span>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">II kwartał 2024</span>
-            <!-- /wp:paragraph -->
+            <!-- wp:heading {"level":6} -->
+            <h6><span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">TERMIN UKOŃCZENIA (BUDYNEK 1)</span></h6>
+            <!-- /wp:heading -->
+            <!-- wp:heading {"level":5} -->
+            <h5><span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">II kwartał 2024</span></h5>
+            <!-- /wp:heading -->
           </div>
           <!-- /wp:group -->
 
           <!-- wp:group -->
           <div class="wp-block-group info-box3 mb-[35px] desktop:mb-[30px]">
-            <!-- wp:paragraph -->
-
-            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LICZBA MIESZKAŃ W SPRZEDAŻY</span>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">29</span>
-            <!-- /wp:paragraph -->
+            <!-- wp:heading {"level":6} -->
+            <h6><span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">LICZBA MIESZKAŃ W SPRZEDAŻY</span></h6>
+            <!-- /wp:heading -->
+            <!-- wp:heading {"level":5} -->
+            <h5><span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">29</span></h5>
+            <!-- /wp:heading -->
           </div>
-          <!-- /wp:group -->
+          <!-- /wp:group -->q
 
           <!-- wp:group -->
           <div class="wp-block-group info-box4 mb-[35px] desktop:mb-[30px]">
-            <!-- wp:paragraph -->
-            <span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">POWIERZCHNIA MIESZKAŃ</span>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">31 m<sup>2</sup> - 97 m<sup>2</sup></span>
-            <!-- /wp:paragraph -->
+            <!-- wp:heading {"level":6} -->
+            <h6><span class="block mb-[10px] uppercase text-[#8a8f99] text-[11px] tracking-[1px]" data-aos="fade-right" data-aos-offset="30">POWIERZCHNIA MIESZKAŃ</span></h6>
+            <!-- /wp:heaading -->
+            <!-- wp:heading {"level":5} -->
+            <h5><span class="block text-[24px]" data-aos="fade-right" data-aos-offset="30">31 m<sup>2</sup> - 97 m<sup>2</sup></span></h5>
+            <!-- /wp:heaading -->
           </div>
           <!-- /wp:group -->
         </div>
