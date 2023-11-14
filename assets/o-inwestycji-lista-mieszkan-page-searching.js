ddd5de1591e4de606e51d444751a94b1aa30beda
Tomek <TK-54-4B>
2023-11-14 13:01:51 +0100

    test

--- assets/o-inwestycji-lista-mieszkan-page-searching.js
+++ assets/o-inwestycji-lista-mieszkan-page-searching.js
@@ -1282,6 +1282,7 @@ window.addEventListener("load", function() {
     });
 
     wp.hooks.addAction('ymc_after_loaded_data_148_2', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
@@ -1289,18 +1290,21 @@ window.addEventListener("load", function() {
     });
 
     wp.hooks.addAction('ymc_after_loaded_data_148_3', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     });
     wp.hooks.addAction('ymc_before_loaded_data_148_2', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     });
     wp.hooks.addAction('ymc_before_loaded_data_148_3', 'smartfilter', function(class_name, response){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
@@ -1308,18 +1312,21 @@ window.addEventListener("load", function() {
     });
 
     if(document.body.classList.contains('page-id-1711')){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     };
     if(document.body.classList.contains('page-id-1926')){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
       console.log('cookies cleared');
     };
     if(document.body.classList.contains('post-type-archive-mieszkania')){
+      document.cookie = "filteredTermsFromCookies=;";
       document.cookie = "filteredTermsFromCookies=; path=";
       document.cookie = "filteredTermsFromCookies=; path=/";
       document.cookie = "filteredTermsFromCookies=; path=/pl";
