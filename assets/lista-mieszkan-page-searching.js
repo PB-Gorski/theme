c62058b911f6dc39556a40d6280d261c76fb6e7a
Tomek <TK-54-4B>
2023-11-15 10:41:09 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -545,12 +545,12 @@ window.addEventListener("load", function() {
         document.referrer.search('lokalizacja-osiedle-srebrniki') > 20 ||
         document.referrer.search('galeria-osiedle-srebrniki') > 20 ||
         document.referrer.search('kronika-budowy-osiedle-srebrniki') > 20){
-        // document.cookie = "filteredTermsFromCookies=72";
+        document.cookie = "filteredTermsFromCookies=72";
     }else if (document.referrer.search('o-inwestycji-sw-piotra') > 20 ||
               document.referrer.search('lokalizacja-sw-piotra') > 20 ||
               document.referrer.search('galeria-sw-piotra') > 20 ||
               document.referrer.search('kronika-budowy-sw-piotra') > 20){
-              // document.cookie = "filteredTermsFromCookies=71";
+              document.cookie = "filteredTermsFromCookies=71";
     }else if (document.referrer.search('o-inwestycji-torunska-16') > 20 ||
               document.referrer.search('lokalizacja-torunska-16') > 20 ||
               document.referrer.search('galeria-torunska-16') > 20 ||
