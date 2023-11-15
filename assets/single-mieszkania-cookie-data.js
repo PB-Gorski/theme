22c7748da57fc89b3180284110c5570fc1affe2f
Tomek <TK-54-4B>
2023-11-15 17:56:25 +0100

    test

--- assets/single-mieszkania-cookie-data.js
+++ assets/single-mieszkania-cookie-data.js
@@ -7,9 +7,13 @@ window.addEventListener("load", function() {
     let btnBackToMieszkaniaList = document.querySelector('.js-backToList');
 
     if(inwestycjaName == 'Osiedle Srebrniki'){
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/pl; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
       document.cookie ='filteredTermsFromCookies=72; path=/';
       btnBackToMieszkaniaList.href = window.location.origin + '/pl/o-inwestycji-osiedle-srebrniki/lista-mieszkan-osiedle-srebrniki/';
     }else if(inwestycjaName == 'Św. Piotra'){
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/pl; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
       document.cookie ='filteredTermsFromCookies=71; path=/';
       btnBackToMieszkaniaList.href = window.location.origin + '/pl/o-inwestycji-sw-piotra/lista-mieszkan-sw-piotra/';
     }else if(inwestycjaName == 'Toruńska 16'){
@@ -19,6 +23,8 @@ window.addEventListener("load", function() {
       
       btnBackToMieszkaniaList.href = window.location.origin + '/pl/o-inwestycji-torunska-16/lista-mieszkan-torunska-16/';
     }else if(inwestycjaName == 'Wieżycka Folwark'){
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      document.cookie = 'filteredTermsFromCookies' +"=; Path=/pl; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
       document.cookie ='filteredTermsFromCookies=81; path=/pl';
       btnBackToMieszkaniaList.href = window.location.origin + '/pl/o-inwestycji-wiezycka-folwark/lista-mieszkan-wiezycka-folwark/';
     }
