d18bbc4a50d58cdd31f9a57f41d1b8380b55b3e0
Tomek <TK-54-4B>
2023-11-16 09:13:22 +0100

    test

--- atcf7/newsletter.php
+++ atcf7/newsletter.php
@@ -7,25 +7,18 @@ if (!defined('ABSPATH')) {
  * */
 ?>
 
-<table class="w-full newsletter">
-  <tbody>
-    <tr class="flex flex-col desktop:flex-row gap-[20px]">
-      <td class="w-[100%] desktop:w-[50%] leading-[0px]">
-        <!-- <div> [email* your-email id:your-email ] </div> -->
-        <input id="your-email" name="your-email" type="text" placeholder="Email" class="w-full mb-[20px] desktop:w-[370px] h-[60px] bg-white text-white text-[40px] leading-[150%]">
-      </td>
-    </tr>
-
-    <tr class="w-full">
-      <td class="w-full flex">
-        <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="w-full" />
-        <p class="ml-[10px] mb-[20px] w-full text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych.Więcej.</p>
-      </td>
-    </tr>
-    <input type="submit" value="Submit">
-  </tbody>
+<div class="newsletter wrapper">
+  <div class="leading-[0px]">
+    <!-- <div> [email* your-email id:your-email ] </div> -->
+    <input id="your-email" name="your-email" type="text" placeholder="Email" class="w-full mb-[20px] desktop:w-[370px] h-[60px] bg-white text-white text-[40px] leading-[150%]">
+  </div>
+  <div class="">
+    <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="w-full" />
+    <p class="ml-[10px] mb-[20px] w-full text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych.Więcej.</p>
+  </div>
+  <input type="submit" value="Submit">
   <div>
     <!-- [submit "Submit"] -->
     <button type="submit" class="group btn-find-more relative bg-primaryRed text-white w-full desktop:w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer"><span class="z-[1] relative">Zapisz się</span></button>
   </div>
-</table>
+</div>
