0c5ec46266fd5c6328a29be93a86e17d158b0a11
Tomek <TK-54-4B>
2023-11-16 15:39:43 +0100

    test

--- patterns/page-rodo.php
+++ patterns/page-rodo.php
@@ -32,12 +32,32 @@
     <!-- wp:paragraph -->
     <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Jako administrator Państwa danych osobowych, działając zgodnie z art. 13 ust. 1 i ust. 2 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych) (Dz.U.UE.L.2016.119.1 z dnia 04.05.2016), dalej zwanym RODO, poniżej przedstawiamy Państwu informacje z zakresu ochrony danych osobowych.</p>
     <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Administratorem Państwa danych osobowych jest Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku (80-442) przy ul. Joachima Lelewela 37 lok. E, wpisana w Rejestrze Przedsiębiorców Krajowego Rejestru Sądowego pod numerem KRS: 0000710392, posiadająca numer NIP: 5890008712, REGON: 190575747, dalej zwana Przedsiębiorstwo Budowlane Górski.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">W zakresie, w jakim Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku realizuje zadania inwestycyjne z wykorzystaniem spółek powiązanych kapitałowo i osobowo (spółek celowych) takich jak: Angielska Grobla Górski spółka z ograniczona odpowiedzialnością spółka komandytowa z siedzibą w Gdańsku (ul. Joachima Lelewela 37 lok. E, 80-442 Gdańsk, wpisaną do Krajowego Rejestru Sądowego pod numerem 599870), dochodzi do współadministrowania danymi osobowymi przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna oraz wymienioną spółkę celową.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Względem osób, których dane osobowe są przetwarzane przez współadministratorów, podmiotem odpowiedzialnym za wypełnianie obowiązków związanych z ochroną danych osobowych jest Przedsiębiorstwo Budowalne Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna zgodnie z treścią uzgodnień zawartych w umowie o współadministrowanie. Zasadnicza treść uzgodnień pomiędzy współadministratorami zostanie udostępniona na wniosek osoby, której dane są lub będą przetwarzane. Niezależnie od wskazanych uzgodnień, osoba, której dane dotyczą, ma prawo wykonywać przysługujące jej prawa wynikające z RODO wobec każdego z administratorów.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Z Administratorem danych (i współadministratorem) może się Pani/Pan skontaktować: listownie na adres: (80-442) Gdańsk ul. <span class="text-textGray">Joachima Lelewela 37 lok. E.</span></p>
+    <!-- /wp:paragraph -->
+
 
     <!-- wp:paragraph -->
     <p class="mb-[30px] text-[18px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">I Administrator danych osobowych</p>
     <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
     <p class="block text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">„Cookie” to mały plik tekstowy, który strona internetowa zapisuje na komputerze lub urządzeniu przenośnym internauty w chwili, gdy ten ją przegląda. Strona ta może w ten sposób zapamiętać na dłużej czynności i preferencje internauty (takie jak nazwa użytkownika, język, rozmiar czcionki i inne opcje). Dzięki temu użytkownik nie musi wpisywać tych samych informacji za każdym razem, gdy powróci na tę stronę lub przejdzie z jednej strony na inną.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">II Inspektor ochrony danych</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="block text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Przedsiębiorstwo Budowlane Górski wyznaczyło Inspektora ochrony danych w osobie pana Marcina Sokoła. W razie jakichkolwiek wątpliwości związanych z przetwarzaniem dotyczących Państwa danych osobowych, prosimy o kontakt z Inspektorem ochrony danych, listownie na adres Przedsiębiorstwa Budowlanego Górski z dopiskiem „Inspektor Ochrony Danych”, bądź na adres e-mail: iod@pbgorski.pl.</p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
