62a4cab4864dde8888c9131e9f8e616835280acc
Tomek <TK-54-4B>
2023-11-16 11:56:37 +0100

    test

--- atcf7/newsletter.php
+++ atcf7/newsletter.php
@@ -8,12 +8,12 @@ if (!defined('ABSPATH')) {
 ?>
 
 <div class="newsletter wrapper">
-  <div class="leading-[0px] desktop:flex">
+  <div class="mb-[30px] desktop:flex">
     <!-- <div> [email* your-email id:your-email ] </div> -->
     <input id="your-email" name="your-email" type="text" placeholder="Email" class="mb-[20px] w-[370px] h-[60px] bg-white text-[20px] leading-[150%]">
     <div>
       <!-- [submit "Submit"] -->
-      <button type="submit" class="group relative bg-primaryRed text-white w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-primaryRed before:transition-height before:duration-300 before:ease cursor-pointer"><span class="z-[1] relative">Zapisz się</span></button>
+      <button type="submit" class="group relative bg-primaryRed text-white w-full desktop:w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-primaryRed before:transition-height before:duration-300 before:ease cursor-pointer"><span class="z-[1] relative">Zapisz się</span></button>
     </div>
   </div>
   <div class="flex">
