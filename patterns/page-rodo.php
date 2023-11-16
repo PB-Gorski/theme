0f3a9fc827bce98641579a38dbe0d475bfb3a32a
Tomek <TK-54-4B>
2023-11-16 15:29:49 +0100

    test

--- patterns/page-rodo.php
+++ patterns/page-rodo.php
@@ -18,7 +18,7 @@
       <!-- wp:group -->
       <div class="wp-block-group title text-[36px] desktop:text-[60px] font-bold mb-[50px] leading-[50px] desktop:leading-[70px]">
         <!-- wp:heading {"level":1} -->
-        <h1><span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">Polityka cookies</span></h1>
+        <h1><span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">OCHRONA DANYCH OSOBOWYCH</span></h1>
         <!-- /wp:heading {"level":1} -->
       </div>
       <!-- /wp:group -->
@@ -29,23 +29,15 @@
 
   <!-- wp:group -->
   <div class="wp-block-group content container mx-auto desktop:px-0 px-[20px]">
-    <!-- wp:heading {"level":4} -->
-    <h4><span class="block text-[30px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">Pliki cookie</span></h4>
-    <!-- /wp:heading -->
     <!-- wp:paragraph -->
-    <p class="mb-[40px]"><span class="block text-[18px] text-bgDarkGray leading-[30px] aos-init aos-animate" data-aos="fade-right" data-aos-offset="30">Aby zapewnić sprawne funkcjonowanie tego portalu, czasami umieszczamy na komputerze użytkownika (bądź innym urządzeniu) małe pliki – tzw. cookies („ciasteczka”). Podobnie postępuje większość dużych witryn internetowych.</span></p>
+    <p class="mb-[40px]"><span class="block text-[18px] text-bgDarkGray leading-[30px] aos-init aos-animate" data-aos="fade-right" data-aos-offset="30">Jako administrator Państwa danych osobowych, działając zgodnie z art. 13 ust. 1 i ust. 2 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych) (Dz.U.UE.L.2016.119.1 z dnia 04.05.2016), dalej zwanym RODO, poniżej przedstawiamy Państwu informacje z zakresu ochrony danych osobowych.</span></p>
     <!-- /wp:paragraph -->
-    <!-- wp:heading {"level":4} -->
-    <h4><span class="block text-[30px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">Czym są pliki cookie?</span></h4>
-    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p class="block text-[18px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">I Administrator danych osobowych</p>
+    <!-- /wp:paragraph -->
     <!-- wp:paragraph -->
     <p class="mb-[40px]"><span class="block text-[18px] text-bgDarkGray leading-[30px] aos-init aos-animate" data-aos="fade-right" data-aos-offset="30">„Cookie” to mały plik tekstowy, który strona internetowa zapisuje na komputerze lub urządzeniu przenośnym internauty w chwili, gdy ten ją przegląda. Strona ta może w ten sposób zapamiętać na dłużej czynności i preferencje internauty (takie jak nazwa użytkownika, język, rozmiar czcionki i inne opcje). Dzięki temu użytkownik nie musi wpisywać tych samych informacji za każdym razem, gdy powróci na tę stronę lub przejdzie z jednej strony na inną.</span></p>
-    <!-- /wp:paragraph -->
-    <!-- wp:heading {"level":4} -->
-    <h4><span class="block text-[30px] text-textGray font-bold" data-aos="fade-right" data-aos-offset="30">Jak kontrolować pliki cookie?</span></h4>
-    <!-- /wp:heading -->
-    <!-- wp:paragraph -->
-    <p class="mb-[40px]"><span class="block text-[18px] text-bgDarkGray leading-[30px] aos-init aos-animate" data-aos="fade-right" data-aos-offset="30">Pliki cookie można samodzielnie kontrolować i usuwać. Można usunąć wszystkie pliki cookie zamieszczone na swoim komputerze, a w większości przeglądarek wybrać ustawienie, które uniemożliwia instalowanie tych plików. W takim przypadku może się okazać konieczne dostosowanie niektórych preferencji przy każdej wizycie na danej stronie, a część opcji i usług może być niedostępna.</span></p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
