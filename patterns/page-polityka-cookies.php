569c4d122c2aa9ce7d5537de31c5c3dfb3b59d7d
Tomek <TK-54-4B>
2023-11-16 15:00:17 +0100

    test

--- patterns/page-polityka-cookies.php
+++ patterns/page-polityka-cookies.php
@@ -9,21 +9,32 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="Banner" class="wp-block-group banner-polityka-cookies relative">
+<div id="polityka-cookies" class="wp-block-group polityka-cookies relative">
 
   <!-- wp:group -->
-  <div class="wp-block-group w-full bg-[#f5f7fa] relative py-[80px]">
+  <div class="wp-block-group title w-full bg-[#f5f7fa] relative py-[80px]">
     <!-- wp:group -->
     <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pl-[50px]">
       <!-- wp:group -->
       <div class="wp-block-group title text-[36px] desktop:text-[60px] font-bold mb-[50px] leading-[50px] desktop:leading-[70px]">
-        <!-- wp:paragraph -->
-        <span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">Polityka cookies</span>
-        <!-- /wp:paragraph -->
+        <!-- wp:heading {"level":1} -->
+        <h1><span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">Polityka cookies</span></h1>
+        <!-- /wp:heading {"level":1} -->
       </div>
       <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+
+  <!-- wp:group -->
+  <div class="wp-block-group content container mx-auto desktop:px-0 px-[20px]">
+    <!-- wp:heading -->
+    <h4><span class="block" data-aos="fade-right" data-aos-offset="30">Pliki cookie</span></h4>
+    <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p><span class="block" data-aos="fade-right" data-aos-offset="30">Aby zapewnić sprawne funkcjonowanie tego portalu, czasami umieszczamy na komputerze użytkownika (bądź innym urządzeniu) małe pliki – tzw. cookies („ciasteczka”). Podobnie postępuje większość dużych witryn internetowych.</span></p>
+    <!-- /wp:paragraph -->
   </div>
   <!-- /wp:group -->
 </div>
