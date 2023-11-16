93e8b73d0493971d83a2eece7f1da28620fc7110
Tomek <TK-54-4B>
2023-11-16 17:12:25 +0100

    test

--- patterns/page-rodo.php
+++ patterns/page-rodo.php
@@ -264,6 +264,45 @@
     <!-- wp:paragraph -->
     <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">W celu skorzystania z powyższych praw, należy skontaktować się z Inspektorem ochrony danych listownie na adres Przedsiębiorstwa Budowlanego Górski z dopiskiem „Inspektor Ochrony Danych”, bądź na adres e-mail: <a href="mailto:iod@pbgorski.pl" class="underline">iod@pbgorski.pl.</a></p>
     <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Przedsiębiorstwo Budowlane Górski bez zbędnej zwłoki – nie później niż w terminie miesiąca od dnia wpłynięcia wniosku – udzieli osobie, której dane dotyczą, informacji o działaniach podjętych w związku z realizacją powyższych praw.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Termin miesięczny może ulec przedłużeniu o kolejne dwa miesiące z uwagi na skomplikowany charakter wniosku lub liczbę wniosków. W takim przypadku Przedsiębiorstwo Budowlane Górski w terminie miesiąca od otrzymania wniosku, poinformuje osobę, której dane dotyczą, o takim przedłużeniu terminu z podaniem przyczyn opóźnienia.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Wniosek powinien w miarę możliwości precyzyjnie wskazywać, czego dotyczy żądanie, tj. w szczególności:</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:list -->
+    <ul class="wp-block-list text-[18px] text-bgDarkGray leading-[30px]">
+      <!-- wp:list-item -->
+      <li class="mb-[10px]">1. Z jakiego uprawnienia chce skorzystać osoba składająca wniosek (np. prawo do otrzymania kopii danych, prawo do usunięcia danych, itd.)</li>
+      <!-- /wp:list-item -->
+      <!-- wp:list-item -->
+      <li class="mb-[10px]">2. Jakiego procesu przetwarzania dotyczy żądanie (np. korzystanie z określonej usługi, aktywność w określonym serwisie internetowym, otrzymywanie newslettera zawierającego informacje handlowe na określony adres email, itp.)</li>
+      <!-- /wp:list-item -->
+      <!-- wp:list-item -->
+      <li class="mb-[10px]">3. Jakich celów przetwarzania dotyczy żądanie (np. cele marketingowe, cele analityczne, itp.).</li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Informujemy, że jeżeli Administrator nie będzie w stanie ustalić treści żądania lub zidentyfikować osoby składającej wniosek w oparciu o dokonane zgłoszenie, zwróci się do wnioskodawcy o dodatkowe informacje.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Odpowiedź będzie udzielana na adres e-mail, z którego przesłano wniosek, a w przypadku wniosków skierowanych listownie, listem poleconym na adres wskazany przez wnioskodawcę, o ile z treści listu nie będzie wynikała chęć otrzymania informacji zwrotnej na adres e-mail (w takim przypadku należy podać adres e-mail).</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">W przypadku wątpliwości co do prawidłowości przetwarzania Państwa danych osobowych przez Przedsiębiorstwo Budowlane Górski mają Państwo prawo wniesienia skargi do organu nadzorczego zajmującego się ochroną danych osobowych, to jest Prezesa Urzędu Ochrony Danych Osobowych.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Naszym celem jest zawsze zachować prywatność i bezpieczeństwo danych, a także kontrolować przetwarzanie wszystkich powierzonych nam przez Państwa danych.</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p class="mb-[30px] text-[18px] text-bgDarkGray leading-[30px]" data-aos="fade-right" data-aos-offset="30">Administrator na bieżąco prowadzi analizę ryzyka w celu zapewnienia, że dane osobowe przetwarzane są przez niego w sposób bezpieczny – zapewniający przede wszystkim, że dostęp do danych mają jedynie osoby upoważnione i jedynie w zakresie, w jakim jest to niezbędne ze względu na wykonywane przez nie zadania służbowe.</p>
+    <!-- /wp:paragraph -->
 
 
   </div>
