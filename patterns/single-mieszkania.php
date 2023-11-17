4310bf78db1de6e7bf3223f32530f53c937a66c9
Tomek <TK-54-4B>
2023-11-17 13:44:01 +0100

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -135,6 +135,9 @@
       <!-- wp:group -->
       <div class="wp-block-group left-col desktop:w-[60%]">
         <!-- wp:post-content /-->
+        <!-- wp:shortcode -->
+        <?php echo do_shortcode('[pdf_attachment file="1" name=" Pobierz kartę lokalu"]'); ?>
+        <!-- /wp:shortcode -->
       </div>
       <!-- /wp:group -->
 
