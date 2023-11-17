03e37de0c81e67df0ada7050903808fbc769bad6
Tomek <TK-54-4B>
2023-11-17 11:39:16 +0100

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="cta" class="wp-block-group cta h-[560px] desktop:h-fit relative">
+<div id="cta" class="wp-block-group cta h-[560px] relative">
   <!-- wp:image -->
   <figure class="wp-block-image cta-img w-full h-full absolute left-0 top-0 z-[-1] scale-x-[-1] desktop:scale-[1]">
     <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/frontpage/cta-bg.jpeg" alt="cta-bg">
