cb1f45276ee656f9b4305df774db2cd997cbfbfa
Tomek <TK-54-4B>
2023-11-17 11:41:09 +0100

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -27,40 +27,44 @@
     <!-- /wp:group -->
 
     <!-- wp:group -->
-    <div class="wp-block-group col2 w-[100%] desktop:w-[55%] py-[60px] fflex flex-col gap-[30px] items-center jusify-center">
-      <!-- wp:heading -->
-      <h2><span class="text-[28px] desktop:text-[38px] font-bold mb-[60px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
-      <!-- /wp:heading -->
-
+    <div class="wp-block-group col2 w-[100%] desktop:w-[55%] py-[60px]">
       <!-- wp:group -->
-      <div class="wp-block-group inner-cols-wrapper mb-[10px] desktop:h-[100px] desktop:flex">
+      <div class="wp-block-group flex items-center jusify-center">
+        <!-- wp:heading -->
+        <h2><span class="text-[28px] desktop:text-[38px] font-bold mb-[60px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
+        <!-- /wp:heading -->
+
         <!-- wp:group -->
-        <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
-          <!-- wp:paragraph -->
-          <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Zadzwoń do nas</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="relative text-[22px] font-medium inline-block border-b-primaryRed border-b-[2px] cursor-pointer" data-aos="fade-up" data-aos-offset="30">(58) 340 79 00</p>
-          <!-- /wp:paragraph -->
+        <div class="wp-block-group inner-cols-wrapper mb-[10px] desktop:h-[100px] desktop:flex">
+          <!-- wp:group -->
+          <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Zadzwoń do nas</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="relative text-[22px] font-medium inline-block border-b-primaryRed border-b-[2px] cursor-pointer" data-aos="fade-up" data-aos-offset="30">(58) 340 79 00</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
+          <!-- wp:group -->
+          <div class="wp-block-group inner-col2 mb-[40px] desktop:mb-0 desktop:pl-[50px] desktop:w-[60%]">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Napisz</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="relative text-[22px] font-medium inline-block border-b-primaryRed border-b-[2px] cursor-pointer" data-aos="fade-up" data-aos-offset="30">sprzedaz@pbgorski.pl</p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
 
-        <!-- wp:group -->
-        <div class="wp-block-group inner-col2 mb-[40px] desktop:mb-0 desktop:pl-[50px] desktop:w-[60%]">
-          <!-- wp:paragraph -->
-          <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Napisz</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:paragraph -->
-          <p class="relative text-[22px] font-medium inline-block border-b-primaryRed border-b-[2px] cursor-pointer" data-aos="fade-up" data-aos-offset="30">sprzedaz@pbgorski.pl</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
+        <!-- wp:paragraph -->
+        <span class="group btn-find-more block relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer" data-aos="fade-right" data-aos-offset="30"><span class="font-bold text-white z-[1]"><a href="<?php echo home_url() . '/contact' ?>"> Skontaktuj się</a></span></span>
+        <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
-
-      <!-- wp:paragraph -->
-      <span class="group btn-find-more block relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer" data-aos="fade-right" data-aos-offset="30"><span class="font-bold text-white z-[1]"><a href="<?php echo home_url() . '/contact' ?>"> Skontaktuj się</a></span></span>
-      <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
 
