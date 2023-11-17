9fee75b58efd597791c6a1840568db710d063a37
Tomek <TK-54-4B>
2023-11-17 12:53:04 +0100

    test

--- patterns/single-mieszkania.php
+++ patterns/single-mieszkania.php
@@ -125,57 +125,6 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group container mx-auto modal relative desktop:px-0 px-[20px]">
-    <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-    <div class="wp-block-group relative">
-      <!-- wp:group -->
-      <div class="wp-block-group call-back-modal-bg hidden desktop:block opacity-0 z-[-1] bg-[rgba(61,75,102,0.5)] fixed top-0 left-0 w-screen h-screen transition duration-300 ease"></div>
-      <!-- /wp:group -->
-      <!-- wp:group -->
-      <div class="wp-block-group call-back-modal hidden desktop:block opacity-0 w-[850px] h-[370px] p-[45px_60px] bg-white fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-[-1] transition duration-300 ease">
-        <!-- wp:group -->
-        <div class="wp-block-group btn-close absolute top-[-5px] right-[15px] cursor-pointer">
-          <!-- wp:paragraph -->
-          <p class="text-[40px] text-bgDarkGray opacity-[50%]">×</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:paragraph -->
-        <p class="text-[40px] text-center font-bold">Zamów darmowe połączenie</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:paragraph -->
-        <p class="text-center text-bgDarkGray mb-[35px]">Zostaw numer, nasz konsultant oddzwoni do Ciebie</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:group -->
-        <div class="wp-block-group input mb-[25px] flex">
-          <!-- wp:paragraph -->
-          <p class="w-[370px] h-[60px] bg-white text-bgDarkGray text-[14px] leading-[150%]">Numer telefonu</p>
-          <!-- /wp:paragraph -->
-          <!-- wp:group -->
-          <div class="wp-block-group group btn relative bg-[#e80d38] text-white w-[370px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-btnHoverRed before:transition-height before:duration-300 before:ease cursor-pointer">
-            <!-- wp:paragraph -->
-            <p class="font-bold z-[1]">Zamawiam rozmowę</p>
-            <!-- /wp:paragraph -->
-          </div>
-          <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-        <!-- wp:group -->
-        <div class="wp-block-group input checkbox">
-          <!-- wp:paragraph -->
-          <p class="text-bgDarkGray text-[12px] leading-[22px]">Wyrażam zgodę na używanie przez PB Górski sp. z o.o. S.K.A. telekomunikacyjnych urządzeń końcowych dla celów marketingu bezpośredniego, tj. aby przedstawić oferty PB Górski sp. z o.o. S.K.A. telefonicznie. Jednocześnie informujemy, że rozmowa będzie nagrywana. Administratorem Danych Osobowych jest PB Górski sp. z o.o. S.K.A. z siedzibą w Gdańsku (80-312) ul. Wita Stwosza 31A/31. tutaj</p>
-          <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-      </div>
-      <!-- /wp:group -->
-
-    </div>
-    <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-
-  <!-- wp:group -->
   <div class="wp-block-group container mx-auto relative desktop:px-0 px-[20px] pt-[140px] pb-[100px]">
     <!-- wp:paragraph -->
     <p class="desktop:mb-[60px] mb-0 text-[24px] font-medium">Przykładowa aranżacja <span class="text-[#828894] font-normal desktop:text-[24px] text-[18px]">(kliknij zdjęcie by powiększyć)</span></p>
