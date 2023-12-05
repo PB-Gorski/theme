1e4316e4913f36cfa1091ec02507e1ce374be6ce
Tomek <TK-54-4B>
2023-12-05 15:46:58 +0100

    test

--- patterns/header.php
+++ patterns/header.php
@@ -107,20 +107,20 @@
                   $taxNoSpaces = str_replace(' ', '-', strtolower($tax->name));
                   $taxNoSpaces2 = str_replace('.', '', strtolower($taxNoSpaces));
 
-                  $alias = $taxNoSpaces2;
-                  $alias = strtolower($alias);
-                  $alias = str_replace(' ', '-', $alias);
-                  $alias = preg_replace('/[^0-9a-ąćęłńóśźżś\-]+/', '', $alias);
+                  $alias3 = $taxNoSpaces2;
+                  $alias3 = strtolower($alias3);
+                  $alias3 = str_replace(' ', '-', $alias3);
+                  $alias3 = preg_replace('/[^0-9a-ąćęłńóśźżś\-]+/', '', $alias3);
 
-                  $alias = preg_replace('/[\-]+/', '-', $alias);
-                  $alias = trim($alias, '-');
-                  $alias = str_replace(array('ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'Ś'), array('a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z', 's'), $alias);
-                  $alias = str_replace(array(',', ':', ';', ' '), array('', '', '', '-'), $alias);
-                  $alias = strtr($alias, ' ', '-');
+                  $alias3 = preg_replace('/[\-]+/', '-', $alias3);
+                  $alias3 = trim($alias3, '-');
+                  $alias3 = str_replace(array('ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'Ś'), array('a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z', 's'), $alias3);
+                  $alias3 = str_replace(array(',', ':', ';', ' '), array('', '', '', '-'), $alias3);
+                  $alias3 = strtr($alias3, ' ', '-');
                 ?>
                   <!-- wp:list-item -->
                   <li class="hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
-                    <a href="<?php echo home_url() . '/o-inwestycji-' . $alias; ?>" class="inline-block">
+                    <a href="<?php echo home_url() . '/o-inwestycji-' . $alias3; ?>" class="inline-block">
                       <!-- wp:paragraph -->
                       <p class=""><?php echo $tax->name; ?></p>
                       <!-- /wp:paragraph -->
@@ -221,11 +221,22 @@
                     $currentTermCount = (get_term($taxonomies[$i], 'inwestycja'))->count;
                     $taxNoSpaces = str_replace(' ', '-', strtolower($taxonomies[$i]->name));
                     $taxNoSpaces2 = str_replace('.', '', strtolower($taxNoSpaces));
+
+                    $alias = $taxNoSpaces2;
+                    $alias = strtolower($alias);
+                    $alias = str_replace(' ', '-', $alias);
+                    $alias = preg_replace('/[^0-9a-ąćęłńóśźżś\-]+/', '', $alias);
+
+                    $alias = preg_replace('/[\-]+/', '-', $alias);
+                    $alias = trim($alias, '-');
+                    $alias = str_replace(array('ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż', 'Ś'), array('a', 'c', 'e', 'l', 'n', 'o', 's', 'z', 'z', 's'), $alias);
+                    $alias = str_replace(array(',', ':', ';', ' '), array('', '', '', '-'), $alias);
+                    $alias = strtr($alias, ' ', '-');
                 ?>
 
                     <!-- wp:list-item -->
                     <li class="hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
-                      <a href="#"><?php echo $taxInwestycjeName; ?></a>
+                      <a href="<?php echo home_url() . '/o-inwestycji-' . $alias3; ?>"><?php echo $taxInwestycjeName; ?></a>
                     </li>
                     <!-- /wp:list-item -->
                 <?php
