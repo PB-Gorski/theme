a083b51d5db6dc816e059dc818217399ceace5c7
Tomek <TK-54-4B>
2023-12-05 12:34:00 +0100

    test

--- atcf7/newsletter.php
+++ atcf7/newsletter.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
 ?>
 
 <div class="newsletter wrapper">
-  <div class="mx-auto text-center">
+  <div class="mx-auto desktop:flex text-center">
     <!-- <div> [email* your-email id:your-email ] </div> -->
     <input id="your-email" name="your-email" type="text" placeholder="Adres e-mail" class="input-newsletter mb-[20px] w-[370px] h-[55px] bg-white text-[20px] leading-[150%]">
     <div>
@@ -16,9 +16,8 @@ if (!defined('ABSPATH')) {
       <button type="submit" class="btn-newsletter group relative bg-primaryRed text-white w-[170px] h-[60px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-primaryRed before:transition-height before:duration-300 before:ease cursor-pointer"><span class="z-[1] relative">Zapisz się</span></button>
     </div>
   </div>
-  <div class="flex">
-    <input id="check1" cols="50" row="10" name="check1" type="checkbox" />
-    <p class="ml-[10px] mb-[20px] w-full text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych. <a href="<?php echo home_url() . '/rodo' ?>">Więcej.</a></p>
+  <div class="">
+    <p class="ml-[10px] mb-[20px] max-w-[540px] text-white text-[14px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). Więcej informacji o administracji danych. <a href="<?php echo home_url() . '/rodo' ?>">Więcej.</a></p>
   </div>
   <!-- <input type="submit" value="Submit"> -->
 
