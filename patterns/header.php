e0b69c50f03e8e69c7347aef68aede71c5a45e57
Tomek <TK-54-4B>
2023-12-05 13:38:06 +0100

    test

--- patterns/header.php
+++ patterns/header.php
@@ -119,7 +119,7 @@
                   $alias = strtr($alias, ' ', '-');
                 ?>
                   <!-- wp:list-item -->
-                  <li class="hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-[#e80d38] cursor-pointer">
+                  <li class="hover:underline hover:underline-offset-4 hover:decoration-2 hover:decoration-primaryRed cursor-pointer">
                     <a href="<?php echo home_url() . '/o-inwestycji-' . $alias; ?>" class="inline-block">
                       <!-- wp:paragraph -->
                       <p class=""><?php echo $tax->name; ?></p>
