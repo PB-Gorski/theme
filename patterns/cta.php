f199b93828e81705819f58f0233dfcb44dd5124f
Tomek <TK-54-4B>
2023-12-05 12:03:17 +0100

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -17,15 +17,17 @@
       <!-- wp:group -->
       <div class="wp-block-group">
         <!-- wp:heading -->
-        <h2><span class="block text-[28px] desktop:text-[38px] font-bold mb-[20px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
+        <h2><span class="block text-[28px] desktop:text-[38px] text-center font-bold mb-[20px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
         <!-- /wp:heading -->
 
         <!-- wp:group -->
-        <div class="wp-block-group inner-cols-wrapper mb-[50px] desktop:h-[100px] desktop:flex">
+        <div class="wp-block-group inner-cols-wrapper mb-[50px] desktop:flex">
           <!-- wp:group -->
           <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
             <!-- wp:paragraph -->
-            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30"><svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: #b22944;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1"><path d="M418.144 639.168h166.976a94.016 94.016 0 0 1 93.92 93.952v156.544c0 17.248 14.08 31.296 31.328 31.296h166.976a31.36 31.36 0 0 0 31.296-31.296V480.128a31.264 31.264 0 0 0-10.912-23.776L522.016 134.336a31.104 31.104 0 0 0-40.768 0L105.568 456.32a31.2 31.2 0 0 0-10.944 23.776v409.536a31.36 31.36 0 0 0 31.296 31.296h167.008a31.36 31.36 0 0 0 31.296-31.296V733.12a94.048 94.048 0 0 1 93.92-93.952z m459.2 344.416h-166.976a94.016 94.016 0 0 1-93.92-93.92V733.12a31.36 31.36 0 0 0-31.328-31.36h-166.976a31.36 31.36 0 0 0-31.296 31.36v156.544a94.048 94.048 0 0 1-93.92 93.92H125.92A94.048 94.048 0 0 1 32 889.664V480.128c0-27.456 11.968-53.472 32.8-71.36L440.512 86.816a93.44 93.44 0 0 1 122.272 0l375.712 322.016a93.888 93.888 0 0 1 32.768 71.328v409.536a94.016 94.016 0 0 1-93.92 93.92z"/></svg></p>
+            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30"><svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" style="width: 80px; height: 80px;vertical-align: middle;fill: #b22944;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1">
+                <path d="M418.144 639.168h166.976a94.016 94.016 0 0 1 93.92 93.952v156.544c0 17.248 14.08 31.296 31.328 31.296h166.976a31.36 31.36 0 0 0 31.296-31.296V480.128a31.264 31.264 0 0 0-10.912-23.776L522.016 134.336a31.104 31.104 0 0 0-40.768 0L105.568 456.32a31.2 31.2 0 0 0-10.944 23.776v409.536a31.36 31.36 0 0 0 31.296 31.296h167.008a31.36 31.36 0 0 0 31.296-31.296V733.12a94.048 94.048 0 0 1 93.92-93.952z m459.2 344.416h-166.976a94.016 94.016 0 0 1-93.92-93.92V733.12a31.36 31.36 0 0 0-31.328-31.36h-166.976a31.36 31.36 0 0 0-31.296 31.36v156.544a94.048 94.048 0 0 1-93.92 93.92H125.92A94.048 94.048 0 0 1 32 889.664V480.128c0-27.456 11.968-53.472 32.8-71.36L440.512 86.816a93.44 93.44 0 0 1 122.272 0l375.712 322.016a93.888 93.888 0 0 1 32.768 71.328v409.536a94.016 94.016 0 0 1-93.92 93.92z" />
+              </svg></p>
             <!-- /wp:paragraph -->
             <!-- wp:paragraph -->
             <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Odwiedź nas</p>
@@ -37,7 +39,7 @@
           <!-- /wp:group -->
 
           <!-- wp:group -->
-          <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
+          <div class="wp-block-group inner-col2 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
             <!-- wp:paragraph -->
             <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Zadzwoń do nas</p>
             <!-- /wp:paragraph -->
@@ -48,7 +50,7 @@
           <!-- /wp:group -->
 
           <!-- wp:group -->
-          <div class="wp-block-group inner-col2 mb-[40px] desktop:mb-0 desktop:pl-[50px] desktop:w-[60%]">
+          <div class="wp-block-group inner-col3 mb-[40px] desktop:mb-0 desktop:pl-[50px] desktop:w-[60%]">
             <!-- wp:paragraph -->
             <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Napisz</p>
             <!-- /wp:paragraph -->
@@ -61,7 +63,7 @@
         <!-- /wp:group -->
 
         <!-- wp:paragraph -->
-        <span class="group btn-find-more block relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer" data-aos="fade-right" data-aos-offset="30"><span class="font-bold text-white z-[1]"><a href="<?php echo home_url() . '/contact' ?>"> Skontaktuj się</a></span></span>
+        <span class="group btn-find-more mx-auto block relative bg-[#3d4b66] text-white w-[210px] h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease cursor-pointer" data-aos="fade-right" data-aos-offset="30"><span class="font-bold text-white z-[1]"><a href="<?php echo home_url() . '/contact' ?>"> Skontaktuj się</a></span></span>
         <!-- /wp:paragraph -->
       </div>
       <!-- /wp:group -->
