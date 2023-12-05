561dab8bbe0261d55262c3ce9d58faf3cddf493f
Tomek <TK-54-4B>
2023-12-05 11:58:41 +0100

    test

--- patterns/cta.php
+++ patterns/cta.php
@@ -9,19 +9,33 @@
 ?>
 
 <!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
-<div id="cta" class="wp-block-group cta h-[560px] relative">
+<div id="cta" class="wp-block-group cta h-[560px] bg-[#edeef3] relative">
   <!-- wp:group -->
   <div class="wp-block-group container mx-auto desktop:px-0 px-[20px] h-full">
     <!-- wp:group -->
-    <div class="wp-block-group py-[60px] flex items-center jusify-center">
+    <div class="wp-block-group py-[60px] flex items-center justify-center">
       <!-- wp:group -->
       <div class="wp-block-group">
         <!-- wp:heading -->
-        <h2><span class="block text-[28px] desktop:text-[38px] font-bold mb-[20px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
+        <h2><span class="block text-[28px] desktop:text-[38px] font-bold mb-[20px] desktop:mb-[40px]" data-aos="fade-right" data-aos-offset="30">Potrzebujesz więcej informacji?</span></h2>
         <!-- /wp:heading -->
 
         <!-- wp:group -->
         <div class="wp-block-group inner-cols-wrapper mb-[50px] desktop:h-[100px] desktop:flex">
+          <!-- wp:group -->
+          <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30"><svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" style="width: 1em; height: 1em;vertical-align: middle;fill: #b22944;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1"><path d="M418.144 639.168h166.976a94.016 94.016 0 0 1 93.92 93.952v156.544c0 17.248 14.08 31.296 31.328 31.296h166.976a31.36 31.36 0 0 0 31.296-31.296V480.128a31.264 31.264 0 0 0-10.912-23.776L522.016 134.336a31.104 31.104 0 0 0-40.768 0L105.568 456.32a31.2 31.2 0 0 0-10.944 23.776v409.536a31.36 31.36 0 0 0 31.296 31.296h167.008a31.36 31.36 0 0 0 31.296-31.296V733.12a94.048 94.048 0 0 1 93.92-93.952z m459.2 344.416h-166.976a94.016 94.016 0 0 1-93.92-93.92V733.12a31.36 31.36 0 0 0-31.328-31.36h-166.976a31.36 31.36 0 0 0-31.296 31.36v156.544a94.048 94.048 0 0 1-93.92 93.92H125.92A94.048 94.048 0 0 1 32 889.664V480.128c0-27.456 11.968-53.472 32.8-71.36L440.512 86.816a93.44 93.44 0 0 1 122.272 0l375.712 322.016a93.888 93.888 0 0 1 32.768 71.328v409.536a94.016 94.016 0 0 1-93.92 93.92z"/></svg></p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="mb-[10px] text-bgDarkGray" data-aos="fade-up" data-aos-offset="30">Odwiedź nas</p>
+            <!-- /wp:paragraph -->
+            <!-- wp:paragraph -->
+            <p class="relative text-[22px] font-medium inline-block border-b-primaryRed border-b-[2px] cursor-pointer" data-aos="fade-up" data-aos-offset="30"><a href="https://maps.app.goo.gl/CkQVYxRxJZ6ZihVm7">ul. Joachima Lelewela 37 lok. E,<br>80-442 Gdańsk</a></p>
+            <!-- /wp:paragraph -->
+          </div>
+          <!-- /wp:group -->
+
           <!-- wp:group -->
           <div class="wp-block-group inner-col1 mb-[30px] desktop:mb-0 desktop:w-[40%] ">
             <!-- wp:paragraph -->
