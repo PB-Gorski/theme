7efb2b057524178da83b09964f5901ff954820b1
Tomek <TK-54-4B>
2023-12-05 17:34:26 +0100

    test

--- patterns/page-contact-banner.php
+++ patterns/page-contact-banner.php
@@ -14,7 +14,7 @@
   <!-- wp:group -->
   <div class="wp-block-group w-full bg-[#f5f7fa] relative py-[80px]">
     <!-- wp:group -->
-    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pl-[50px]">
+    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px]">
       <!-- wp:heading {"level":1} -->
       <h1 class="wp-block-heading title text-[36px] desktop:text-[60px] font-bold mb-[50px] leading-[50px] desktop:leading-[70px]"><span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">Z chęcią odpowiemy na</span><span class="desktop:block inline" data-aos="fade-right" data-aos-offset="30">wszystkie Twoje pytania.</span></h1>
       <!-- /wp:heading -->
