065af958452837e4ecb8a29e24601ca46b14081f
Tomek <TK-54-4B>
2023-12-05 12:48:47 +0100

    test

--- patterns/newsletter.php
+++ patterns/newsletter.php
@@ -22,7 +22,7 @@
     <!-- /wp:heading -->
 
     <!-- wp:group -->
-    <div class="wp-block-group mb-[15px] desktop:flex justifu">
+    <div class="wp-block-group mb-[15px] desktop:flex justify-center items-center">
       <!-- wp:shortcode -->
       <?php echo do_shortcode('[contact-form-7 id="e479141" title="newsletter"]'); ?>
       <!-- /wp:shortcode -->
