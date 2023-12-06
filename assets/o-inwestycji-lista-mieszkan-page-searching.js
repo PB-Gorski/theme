9a4a8b13927c655c9ce9e5d0f89bb6d7ce9d5814
Tomek <TK-54-4B>
2023-12-06 12:07:02 +0100

    test

--- assets/o-inwestycji-lista-mieszkan-page-searching.js
+++ assets/o-inwestycji-lista-mieszkan-page-searching.js
@@ -826,7 +826,7 @@ window.addEventListener("load", function() {
 
         console.log('price max fdafsd: ',filteredPriceMaxFromCookies);
 
-        document.querySelector('.dropdown__value-max').childNodes[0].innerHTML = filteredPriceMaxFromCookies == 'NaN' ? document.getElementById('inputPriceMax').value : (filteredPriceMaxFromCookies == 10000000 ? 'Max' :  filteredPriceMaxFromCookies == '' ? document.getElementById('inputPriceMax').value : Number(filteredPriceMaxFromCookies).toLocaleString());
+        document.querySelector('.dropdown__value-max').childNodes[0].innerHTML = filteredPriceMaxFromCookies == 'NaN' ? document.getElementById('inputPriceMax').value : (filteredPriceMaxFromCookies == 10000000 ? document.getElementById('inputPriceMax').value :  filteredPriceMaxFromCookies == '' ? document.getElementById('inputPriceMax').value : Number(filteredPriceMaxFromCookies).toLocaleString());
         
         
         document.querySelector('.dropdown__value-min-metraz').childNodes[0].innerHTML = filteredMetrazMinFromCookies == '' ? 0 : Number(filteredMetrazMinFromCookies);
