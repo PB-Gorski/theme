2649940f8daba1c8646d4b579c56e902c05f04a4
Tomek <TK-54-4B>
2023-12-06 11:36:38 +0100

    test

--- filter-layout2.php
+++ filter-layout2.php
@@ -117,7 +117,7 @@ $tax_sort = $taxonomy;
           <div class="dropdown-filter customPrice price-max flex-col bg-white">
             <p class="dropdown-filter-title">Cena do:</p>
             <div class="dropdown__value dropdown__value-price dropdown__value-max font-bold after:content-['']"><span class="hidden"></span><input type="text" id="inputPriceMax" placeholder="Max" class="w-full"></div>
-            <ul class="dropdown__list">
+            <ul class="dropdown__list hidden">
               <li class="dropdown__item">
                 <span class="dropdown__link menu-link cursor-pointer">50 000</span>
               </li>
