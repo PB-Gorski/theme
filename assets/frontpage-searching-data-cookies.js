315b41bc5f1e4af69e3929ec4ccd8102b7818527
Tomek <TK-54-4B>
2023-12-06 10:09:55 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
   if(document.body.classList.contains('home') ||
   document.body.classList.contains('_page-parent') ||
   document.body.classList.contains('_page-child')){
-    document.querySelector('.btn-ssearch').addEventListener('click',()=>{
+    document.querySelector('.btn-search').addEventListener('click',()=>{
       document.querySelector('.dropdown__value-min').childNodes[0].innerHTML = document.getElementById('inputPriceMin').value;
     })
 
