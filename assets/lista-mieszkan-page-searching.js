696134f7b552e5185568f19ccb71aa1ca47eb912
Tomek <TK-54-4B>
2023-12-13 11:55:27 +0100

    test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -64,7 +64,8 @@ window.addEventListener("load", function() {
         function showActiveFilterName(){
           let miastoArr = [28,40];
           let inwestycjaArr = [72,71,82,81];
-          let pokojeArr = [43,34,53,25];
+          // let pokojeArr = [43,34,53,25];
+          let pokojeArr = [640,641,642,643,644,645,646];
           let pietroArr = [97,78,60,66];
           let terminArr = [49,91,38,122,93];
           let inneArr = [520,521,522];
@@ -614,7 +615,8 @@ window.addEventListener("load", function() {
       function showActiveFilterNameFromCookies(){
         let miastoArr = [28,40];
         let inwestycjaArr = [72,71,82,81];
-        let pokojeArr = [43,34,53,25];
+        // let pokojeArr = [43,34,53,25];
+        let pokojeArr = [640,641,642,643,644,645,646];
         let pietroArr = [97,78,60,66];
         let terminArr = [49,91,38,122,93];
         let inneArr = [520,521,522];
