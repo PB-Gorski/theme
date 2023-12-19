7042f75abfeb636a3f1f40e423a44dbd206601c7
Tomek <TK-54-4B>
2023-12-19 18:06:48 +0100

    test

--- patterns/banner-main.php
+++ patterns/banner-main.php
@@ -13,7 +13,7 @@
   <!-- wp:group -->
   <div class="wp-block-group h-full mx-auto">
     <!-- wp:group -->
-    <div class="wp-block-group aos-zoom-in h-full w-full">
+    <div class="wp-block-group aos-zoom-out h-full w-full">
       <!-- wp:core/video -->
       <figure class="wp-block-video banner-video h-full"><video autoplay="true" loop muted preload src="https://www.pbgorski.pl/assets/front/videos/header/hp.mp4?1699011570"></video>
       </figure>
