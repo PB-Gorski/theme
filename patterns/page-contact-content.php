ea7e5dc4b75bb5ad08dc421ff68117363907f96e
Tomek <TK-54-4B>
2023-12-20 14:03:45 +0100

    test

--- patterns/page-contact-content.php
+++ patterns/page-contact-content.php
@@ -208,7 +208,7 @@
               <!-- wp:group -->
               <div class="wp-block-group text-black">
                 <!-- wp:group -->
-                <div class="wp-block-group mb-[25px] phone flex gap-[40px]">
+                <div class="wp-block-group mb-[25px] text-[#8a8f99] phone flex gap-[40px]">
                   <!-- wp:group -->
                   <div class="wp-block-group labels">
                     <!-- wp:paragraph -->
@@ -247,7 +247,7 @@
                 <li>
                   <a href="https://www.google.com/maps/place/Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A/@54.3836995,18.6080757,17.6z/data=!4m5!3m4!1s0x0:0xc1029ad51bc7ce0c!8m2!3d54.4041492!4d18.5669008" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
-                    <figure class="wp-block-image w-[20px]">
+                    <figure class="wp-block-image w-[20px] m-0">
                       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/maps-pin.png" alt="map-pin">
                     </figure>
                     <!-- /wp:image -->
@@ -261,7 +261,7 @@
                 <li>
                   <a href="https://www.google.com/maps/dir//Deweloper+PB+G%C3%B3rski+Sp.+z+o.o.+S.K.A,+Lelewela+6%2Flok.+1,+80-442+Gda%C5%84sk/@54.3831786,18.6099402,16.19z/data=!4m9!4m8!1m0!1m5!1m1!1s0x46fd73f68f036d41:0xc1029ad51bc7ce0c!2m2!1d18.5669008!2d54.4041492!3e0" class="font-bold flex gap-[10px]">
                     <!-- wp:image -->
-                    <figure class="wp-block-image w-[20px]">
+                    <figure class="wp-block-image w-[20px] m-0">
                       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-contact/nav-pin.png" alt="nav-pin">
                     </figure>
                     <!-- /wp:image -->
