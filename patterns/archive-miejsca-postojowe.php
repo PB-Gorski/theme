dad1780b3bf1e860696058571b585c8d00c99165
Tomek <TK-54-4B>
2023-12-28 15:03:28 +0100

    test

---
+++ patterns/archive-miejsca-postojowe.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Title: archive-miejsca-postojowe
+ * Slug: archive-miejsca-postojowe
+ * Categories: text, banner
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","anchor":true} -->
+<div id="content" class="wp-block-group miejsca-postojowe content bg-[#ebeef2] relative">
+  <!-- wp:group -->
+  <div class="wp-block-group">
+    <!-- wp:list -->
+    <ul class="wp-block-list js-lista-mieszkan-list w-full bg-white">
+      <!-- wp:list-item -->
+      <li class="short-code-ymc-filter applyContainer1 applyContainer2">
+        <?php echo do_shortcode('[ymc_filter id="2323"]'); ?>
+      </li>
+      <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
