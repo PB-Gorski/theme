b642ddd9282bd544f02d84f72af6a2576a3b0e87
Tomek <TK-54-4B>
2024-01-10 14:18:26 +0100

    test

--- patterns/archive-inwestycje.php
+++ patterns/archive-inwestycje.php
@@ -123,7 +123,7 @@
                 } else { ?>
 
                   <!-- wp:image -->
-                  <figure class="wp-block-image desktop:w-[50%] h-[330px] desktop:h-full mb-[30px]">
+                  <figure class="wp-block-image desktop:w-full h-[330px] desktop:h-full mb-[30px]">
                     <img src="<?php echo home_url() . '/wp-content/themes/pbgorski/assets/img/page-blog/default-post-image.jpg'; ?>" alt="inwestycje-img">
                   </figure>
                   <!-- /wp:image -->
