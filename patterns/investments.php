cb82f6ea1856f8559956e2615a84066a2bd0ae66
Tomek <TK-54-4B>
2024-01-22 17:21:22 +0100

    test

--- patterns/investments.php
+++ patterns/investments.php
@@ -77,12 +77,13 @@
                     <!-- /wp:heading {"level":3} -->
 
                     <!-- wp:paragraph -->
-                    <p class="mr-[20px] test text-bgDarkGray text-[16px] desktop:text-[18px]">
+                    <p class="mr-[20px] text-bgDarkGray text-[16px] desktop:text-[18px]">
                       <?php
+                      print_r($tax)
                       if ($tax->name == 'Osiedle Srebrniki') echo 'Gdańsk';
                       elseif ($tax->name == 'Św. Piotra') echo 'Gdynia';
                       elseif ($tax->name == 'Toruńska 16') echo 'Gdańsk';
-                      elseif ($tax->name == 'Wieżycka Folwark') echo 'Gdańsk2';
+                      elseif ($tax->name == 'Wieżycka Folwark') echo 'Gdańsk';
                       ?>
                     </p>
                     <!-- /wp:paragraph -->
