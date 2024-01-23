74568713a7b598c544ccef152214335f5bb6a6b8
Tomek <TK-54-4B>
2024-01-23 21:00:20 +0100

    test

--- patterns/page-about-team.php
+++ patterns/page-about-team.php
@@ -25,20 +25,20 @@
   <!-- /wp:group -->
 
   <!-- wp:group -->
-  <div class="wp-block-group aos-fade-right block cursor-default desktop:max-w-[900px] mx-auto">
+  <div class="wp-block-group aos-fade-right flex gap-[20px] block cursor-default desktop:max-w-[900px] mx-auto">
     <!-- wp:image -->
     <figure class="wp-block-image mb-[30px] desktop:max-w-[900px] mx-auto">
-      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-2.webp" alt="team-image">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-2.jpg" alt="team-image">
     </figure>
     <!-- /wp:image -->
     <!-- wp:image -->
     <figure class="wp-block-image mb-[30px] desktop:max-w-[900px] mx-auto">
-      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-1.webp" alt="team-image">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-1.jpg" alt="team-image">
     </figure>
     <!-- /wp:image -->
     <!-- wp:image -->
     <figure class="wp-block-image mb-[30px] desktop:max-w-[900px] mx-auto">
-      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-3.webp" alt="team-image">
+      <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-about/pbg-3.jpg" alt="team-image">
     </figure>
     <!-- /wp:image -->
   </div>
