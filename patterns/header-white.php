73cb25cc20892dedb10e120123bbdb8639b188df
Tomek <TK-54-4B>
2024-01-24 15:50:49 +0100

    test

--- patterns/header-white.php
+++ patterns/header-white.php
@@ -255,11 +255,11 @@
             <!-- /wp:group -->
 
             <!-- wp:group -->
-            <div class="wp-block-group col2 w-[40%]">
+            <div class="wp-block-group col2 w-[40%] pl-[20px]">
               <!-- wp:group -->
               <div class="wp-block-group title2 mb-[20px]">
                 <!-- wp:paragraph -->
-                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie2</p>
+                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie</p>
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
@@ -368,11 +368,11 @@
             <!-- /wp:group -->
 
             <!-- wp:group -->
-            <div class="wp-block-group col2 w-[40%]">
+            <div class="wp-block-group col2 w-[40%] pl-[20px]">
               <!-- wp:group -->
               <div class="wp-block-group title2 mb-[20px]">
                 <!-- wp:paragraph -->
-                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie2</p>
+                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie</p>
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
--- patterns/header.php
+++ patterns/header.php
@@ -258,11 +258,11 @@
             <!-- /wp:group -->
 
             <!-- wp:group -->
-            <div class="wp-block-group col2 w-[40%]">
+            <div class="wp-block-group col2 w-[40%] pl-[20px]">
               <!-- wp:group -->
               <div class="wp-block-group title2 mb-[20px]">
                 <!-- wp:paragraph -->
-                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie2</p>
+                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie</p>
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
@@ -293,7 +293,7 @@
               <!-- wp:group -->
               <div class="wp-block-group title1 mb-[20px]">
                 <!-- wp:paragraph -->
-                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Lista inwestycji3</p>
+                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Lista inwestycji</p>
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
@@ -371,11 +371,11 @@
             <!-- /wp:group -->
 
             <!-- wp:group -->
-            <div class="wp-block-group col2 w-[40%]">
+            <div class="wp-block-group col2 w-[40%] pl-[20px]">
               <!-- wp:group -->
               <div class="wp-block-group title2 mb-[20px]">
                 <!-- wp:paragraph -->
-                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie2</p>
+                <p class="uppercase text-[#8A8F99] text-[11px] font-medium tracking-[1.5px]">Wyszukiwanie</p>
                 <!-- /wp:paragraph -->
               </div>
               <!-- /wp:group -->
