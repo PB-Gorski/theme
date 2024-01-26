d1b74609331619539d208925190df508c41db18e
Kamil <k.medrala99@gmail.com>
2024-01-26 11:40:12 +0100

    [PBGOR-24]
    last section padding bottom

--- patterns/page-o-inwestycji-sukiennicza-19a-content.php
+++ patterns/page-o-inwestycji-sukiennicza-19a-content.php
@@ -212,7 +212,7 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
       <!-- /wp:group -->
 
       <!-- wp:group -->
-      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[50px_100px_20px] desktop:w-1/2">
+      <div class="wp-block-group mb-[40px] desktop:mb-0 desktop:p-[50px_100px_60px] desktop:w-1/2">
         <!-- wp:heading -->
         <h2><span class="block mb-[40px] text-[30px] desktop:text-[34px] leading-[36px] desktop:leading-[44px]" data-aos="fade-right" data-aos-offset="30">Tajemnice gdańskiego zamczyska</span></h2>
         <!-- /wp:heading -->
