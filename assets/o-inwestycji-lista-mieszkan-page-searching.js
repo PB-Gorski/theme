65311118f91622c5e97305b39d12a6c895c36efd
Tomek <TK-54-4B>
2024-01-28 15:57:24 +0100

    test

--- assets/o-inwestycji-lista-mieszkan-page-searching.js
+++ assets/o-inwestycji-lista-mieszkan-page-searching.js
@@ -741,7 +741,7 @@ window.addEventListener("load", function() {
             
             passiveOptions.forEach(option => {
               if(option.childNodes[1].dataset.termid == item){
-                console.log('option from cookies for finding parent: ',option);
+                console.log('option from cookies for finding parent: ', option);
                 returnChoosenFromCookiesInwestycje.push(option)
               };              
             });
@@ -753,43 +753,56 @@ window.addEventListener("load", function() {
             //   console.log('Sw Piotra');
             // }
 
-            if(filteredTermsFromCookies == '72'){
+
+
+            if(filteredTermsFromCookies == investID){
               returnChoosenFromCookiesInwestycje.forEach(el => {
                 // inwestycja filtr
-                el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Osiedle Srebrniki';
+                el.parentNode.previousElementSibling.childNodes[1].innerHTML = investName;
                 el.parentNode.previousElementSibling.classList.add('pointer-events-none');
                 // miasto filtr
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
-              });
-            }else if(filteredTermsFromCookies == '71'){
-              returnChoosenFromCookiesInwestycje.forEach(el => {
-                // inwestycja filtr
-                el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Św. Piotra';
-                el.parentNode.previousElementSibling.classList.add('pointer-events-none');
-                // miasto filtr
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdynia';
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
-              });
-            }else if(filteredTermsFromCookies == '82'){
-              returnChoosenFromCookiesInwestycje.forEach(el => {
-                // inwestycja filtr
-                el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Toruńska 16';
-                el.parentNode.previousElementSibling.classList.add('pointer-events-none');
-                // miasto filtr
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
-              });
-            }else if(filteredTermsFromCookies == '81'){
-              returnChoosenFromCookiesInwestycje.forEach(el => {
-                // inwestycja filtr
-                el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Wieżycka Folwark';
-                el.parentNode.previousElementSibling.classList.add('pointer-events-none');
-                // miasto filtr
-                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
+                el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'city' + cityName;
                 el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
               });
             }
+
+            // if(filteredTermsFromCookies == '72'){
+            //   returnChoosenFromCookiesInwestycje.forEach(el => {
+            //     // inwestycja filtr
+            //     el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Osiedle Srebrniki';
+            //     el.parentNode.previousElementSibling.classList.add('pointer-events-none');
+            //     // miasto filtr
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
+            //   });
+            // }else if(filteredTermsFromCookies == '71'){
+            //   returnChoosenFromCookiesInwestycje.forEach(el => {
+            //     // inwestycja filtr
+            //     el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Św. Piotra';
+            //     el.parentNode.previousElementSibling.classList.add('pointer-events-none');
+            //     // miasto filtr
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdynia';
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
+            //   });
+            // }else if(filteredTermsFromCookies == '82'){
+            //   returnChoosenFromCookiesInwestycje.forEach(el => {
+            //     // inwestycja filtr
+            //     el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Toruńska 16';
+            //     el.parentNode.previousElementSibling.classList.add('pointer-events-none');
+            //     // miasto filtr
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
+            //   });
+            // }else if(filteredTermsFromCookies == '81'){
+            //   returnChoosenFromCookiesInwestycje.forEach(el => {
+            //     // inwestycja filtr
+            //     el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Wieżycka Folwark';
+            //     el.parentNode.previousElementSibling.classList.add('pointer-events-none');
+            //     // miasto filtr
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].childNodes[1].innerHTML = 'Gdańsk';
+            //     el.parentNode.previousElementSibling.parentNode.previousElementSibling.childNodes[3].classList.add('pointer-events-none');
+            //   });
+            // }
 
             // returnChoosenFromCookiesInwestycje.forEach(el => {
             //   el.parentNode.previousElementSibling.childNodes[1].innerHTML = 'Wybrano: ' + returnChoosenFromCookiesInwestycje.length;
