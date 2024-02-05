e0e830321c45ead06ab3da918f5c2df83b1ec096
Tomek <TK-54-4B>
2024-02-05 21:22:58 +0100

    test

--- patterns/page-lokalizacja-osiedle-srebrniki-banner.php
+++ patterns/page-lokalizacja-osiedle-srebrniki-banner.php
@@ -12,6 +12,9 @@
 <div id="Banner" class="wp-block-group page-lokalizacja relative">
   <!-- wp:group -->
   <div class="wp-block-group w-full h-[50vh] desktop:h-[80vh] bg-banner relative flex desktop:block items-center">
+    <!-- wp:group -->
+    <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] absolute top-0 left-0"></div>
+    <!-- /wp:group -->
     <!-- wp:image -->
     <figure class="wp-block-image w-full h-full absolute top-0 left-0">
       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/osiedle-srebrniki-bg.jpeg" alt="banner-background">
@@ -19,7 +22,7 @@
     <!-- /wp:image -->
 
     <!-- wp:group -->
-    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] z-[0] relative text-white">
+    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] z-[2] relative text-white">
       <!-- wp:group -->
       <div class="wp-block-group title text-[36px] desktop:text-[60px] desktop:pt-[290px] mb-[50px] leading-[44px] desktop:leading-[70px]">
         <!-- wp:heading {"level":1} -->
