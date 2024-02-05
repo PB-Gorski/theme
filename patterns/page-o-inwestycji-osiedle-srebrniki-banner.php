6ec9793ecde41e6b5be3935d51bdbe6df146c9c7
Tomek <TK-54-4B>
2024-02-05 20:41:49 +0100

    test

--- patterns/page-o-inwestycji-osiedle-srebrniki-banner.php
+++ patterns/page-o-inwestycji-osiedle-srebrniki-banner.php
@@ -13,7 +13,7 @@
   <div class="wp-block-group w-full h-full bg-banner relative flex desktop:block items-center">
 
     <!-- wp:group -->
-    <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] relative"></div>
+    <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] absolute top-0 left-0"></div>
     <!-- /wp:group -->
 
     <!-- wp:image -->
