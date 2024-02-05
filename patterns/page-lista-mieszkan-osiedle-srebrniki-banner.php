a1965ad923f5967304a3377d5bd66b2bfdcb8c71
Tomek <TK-54-4B>
2024-02-05 21:42:11 +0100

    test

--- patterns/page-lista-mieszkan-osiedle-srebrniki-banner.php
+++ patterns/page-lista-mieszkan-osiedle-srebrniki-banner.php
@@ -12,6 +12,9 @@
 <div id="Banner" class="wp-block-group banner-lista-mieszkan-osiedle-srebrniki relative">
   < <!-- wp:group -->
     <div class="wp-block-group w-full h-[50vh] desktop:h-[80vh] relative py-[60px]">
+      <!-- wp:group -->
+      <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] absolute top-0 left-0"></div>
+      <!-- /wp:group -->
       <!-- wp:image -->
       <figure class="wp-block-image w-full h-full absolute top-0 left-0 z-[-1]">
         <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-osiedle-srebrniki/osiedle-srebrniki-bg2.jpeg" alt="banner-bg">
