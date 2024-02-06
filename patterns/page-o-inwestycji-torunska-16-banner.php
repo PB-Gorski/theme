7bce517753c557d6cac76b11fa86a45d6a4f86d6
Tomek <TK-54-4B>
2024-02-06 20:49:44 +0100

    test

--- patterns/page-o-inwestycji-torunska-16-banner.php
+++ patterns/page-o-inwestycji-torunska-16-banner.php
@@ -14,6 +14,7 @@
     <!-- wp:group -->
     <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] absolute top-0 left-0"></div>
     <!-- /wp:group -->
+
     <!-- wp:image -->
     <figure class="wp-block-image w-full h-full desktop:h-[90vh] absolute top-0 left-0">
       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-torunska-16/torunska-16-bg.jpg" alt="banner-background">
