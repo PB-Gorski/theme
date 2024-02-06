0cd9ebb2d1774c858211f8146ecd5bb8f78bf08f
Tomek <TK-54-4B>
2024-02-06 20:24:24 +0100

    test

--- patterns/page-lista-mieszkan-sw-piotra-banner.php
+++ patterns/page-lista-mieszkan-sw-piotra-banner.php
@@ -12,13 +12,17 @@
 <div id="Banner" class="wp-block-group banner-lista-mieszkan-sw-piotra relative">
   <!-- wp:group -->
   <div class="wp-block-group w-full h-[480px] desktop:h-[600px] relative py-[60px]">
+    <!-- wp:group -->
+    <div class="wp-block-group banner w-full h-full bg-black opacity-30 z-[1] absolute top-0 left-0"></div>
+    <!-- /wp:group -->
+
     <!-- wp:image -->
     <figure class="wp-block-image w-full h-full absolute top-0 left-0 z-[-1]">
       <img src="<?php echo home_url(); ?>/wp-content/themes/pbgorski/assets/img/page-o-inwestycji-sw-piotra/sw-piotra-bg2.jpeg" alt="banner-bg">
     </figure>
     <!-- /wp:image -->
     <!-- wp:group -->
-    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pt-[100px]">
+    <div class="wp-block-group text-content container mx-auto desktop:px-0 px-[20px] desktop:pt-[100px] z-[2]">
       <!-- wp:group -->
       <div class="wp-block-group title-wrapper">
         <!-- wp:group -->
