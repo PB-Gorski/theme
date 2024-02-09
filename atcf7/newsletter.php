e3df1a3e28c10a2ce5e9c195801bf51a6ace3d2a
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 20:32:33 +0100

    Newsletter Form

--- atcf7/newsletter.php
+++ atcf7/newsletter.php
@@ -14,8 +14,8 @@ if (!defined('ABSPATH')) {
       [submit "Submit"]
     </div>
   </div>
-  <label class="flex" style="max-width:560px">
+  <label class="flex mb-[20px]" style="max-width:560px">
     [checkbox* check1 id:check1]<input id="check1" cols="50" row="10" name="check1" type="checkbox" />
-    <p class="ml-[10px] mb-[20px] text-white text-[13px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). <a href="/rodo" class="hover:text-primaryRed">Więcej informacji o administracji danych.</a></p>
+    <span class="ml-[10px] text-white text-[13px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). <a href="/rodo" class="hover:text-primaryRed">Więcej informacji o administracji danych.</a></span>
   </label>
 </div>
