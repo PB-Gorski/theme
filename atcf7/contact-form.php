f46096a36726b025e1d0cc45daefb3b1e5038121
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 12:31:19 +0100

    Contact form

--- atcf7/contact-form.php
+++ atcf7/contact-form.php
@@ -40,16 +40,16 @@ if (!defined('ABSPATH')) {
     <tr>
       <th><label for="your-content" onclick=""></label></th>
       <td class="flex gap-[6px]">
-        <!-- [checkbox* your-content x5 id:your-content ] -->
-        <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="inline" />
+        [checkbox* check1 class:inline x5 id:check1 ]
+        <!-- <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="inline" /> -->
         <p class="ml-[10px] mb-[20px] inline text-[#8a8f99] text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
     <tr>
       <th><label for="your-content" onclick=""></label></th>
       <td class="flex gap-[6px]">
-        <!-- [checkbox* your-content x5 id:your-content ] -->
-        <input id="check2" name="check2" type="checkbox" class="inline" />
+        [checkbox* check2 class:inline x5 id:check2 ]
+        <!-- <input id="check2" name="check2" type="checkbox" class="inline" /> -->
         <p class="ml-[10px] inline text-[#8a8f99] text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
