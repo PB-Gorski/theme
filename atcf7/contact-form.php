7ff29756b952938b423591684709a21dab807af6
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 12:30:01 +0100

    Contact form

--- atcf7/contact-form.php
+++ atcf7/contact-form.php
@@ -2,39 +2,39 @@
 if (!defined('ABSPATH')) {
   exit;
 }
-/** 
+/**
  * Template Name: Formularz kontaktowy
  * */
 ?>
 
-<table class="w-full">
+<table class="w-full contact-form">
   <tbody>
     <tr class="">
       <th><label for="your-name" onclick=""></label></th>
       <td>
-        <!-- [text* your-name id:your-name ] -->
-        <input id="your-name" name="your-name" type="text" placeholder="Imię i nazwisko" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575] focus:border-white active:border-black drop-shadow-lg">
+        [text* your-name id:your-name placeholder "Imię i nazwisko"]
+        <!-- <input id="your-name" name="your-name" type="text" placeholder="Imię i nazwisko" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575] focus:border-white active:border-black drop-shadow-lg"> -->
       </td>
     </tr>
     <tr>
       <th><label for="your-email" onclick=""></label></th>
       <td>
-        <!-- <div> [email* your-email id:your-email ] </div> -->
-        <input id="your-name" name="your-name" type="text" placeholder="Adres email" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]">
+        [email* your-email id:your-email placeholder "Adres email"]
+        <!-- <input id="your-name" name="your-name" type="text" placeholder="Adres email" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]"> -->
       </td>
     </tr>
     <tr>
       <th><label for="your-phone" onclick=""></label></th>
       <td>
-        <!-- <div> [email* your-email id:your-email ] </div> -->
-        <input id="your-phone" name="your-phone" type="text" placeholder="Numer telefonu" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]">
+        [text* your-phone id:your-phone placeholder "Numer telefonu"]
+        <!-- <input id="your-phone" name="your-phone" type="text" placeholder="Numer telefonu" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]"> -->
       </td>
     </tr>
     <tr>
       <th><label for="your-content" onclick=""></label></th>
       <td>
-        <!-- [textarea* your-content x5 id:your-content ] -->
-        <textarea id="your-name" cols="50" row="10" name="your-name" type="textarea" placeholder="Treść wiadomości" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[200px] text-[#2e384d] placeholder:text-[#757575]"></textarea>
+        [textarea* your-phone id:your-phone placeholder "Numer telefonu"]
+        <!-- <textarea id="your-name" cols="50" row="10" name="your-name" type="textarea" placeholder="Treść wiadomości" class="px-[15px] py-[10px] mb-[10px] w-[500px] w-full h-[200px] text-[#2e384d] placeholder:text-[#757575]"></textarea> -->
       </td>
     </tr>
     <tr>
@@ -62,6 +62,6 @@ if (!defined('ABSPATH')) {
   </tbody>
 </table>
 <div>
-  <!-- [submit "Submit"] -->
-  <button type="submit" class="relative mt-[40px] bg-[#3d4b66] text-white w-full h-[55px] flex items-center justify-center z-[1] before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Wyślij wiadomość</button>
+  <!-- [submit "Wyślij wiadomość"] -->
+  <!-- <button type="submit" class="relative mt-[40px] bg-[#3d4b66] text-white w-full h-[55px] flex items-center justify-center z-[1] before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#2e384d] before:transition-height before:duration-300 before:ease before:z-[-1] cursor-pointer">Wyślij wiadomość</button> -->
 </div>
