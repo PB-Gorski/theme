a80ff227c20ee9da271f4471fa52dcdbcc217975
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 20:38:09 +0100

    Newsletter Form

--- atcf7/newsletter.php
+++ atcf7/newsletter.php
@@ -10,12 +10,9 @@ if (!defined('ABSPATH')) {
 <div class="newsletter wrapper">
   <div class="mx-auto desktop:flex justify-center items-start">
     [email* your-email id:your-email]
-    <div>
-      [submit "Submit"]
+    <div class="[&>]:z-[1] [&>]:relative">
+      [submit "Zapisz się"]
     </div>
   </div>
-  <label class="flex mb-[20px]" style="max-width:560px">
-    [checkbox* check1 id:check1]<input id="check1" cols="50" row="10" name="check1" type="checkbox" />
-    <span class="ml-[10px] text-white text-[13px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). <a href="/rodo" class="hover:text-primaryRed">Więcej informacji o administracji danych.</a></span>
-  </label>
+  <label class="flex mb-[20px]" style="max-width:560px">[checkbox* check1 id:check1]<input id="check1" cols="50" row="10" name="check1" type="checkbox" /><span class="ml-[10px] text-white text-[13px] leading-[28px]">Wyrażam zgodę na otrzymywanie informacji handlowych w tym marketingowych od PB Górski Sp. z o.o. SKA drogą elektroniczną (za pośrednictwem adresu e-mail). <a href="/rodo" class="hover:text-primaryRed">Więcej informacji o administracji danych.</a></span></label>
 </div>
