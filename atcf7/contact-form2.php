e8678d75992214cf62e407c992b09b0962526a92
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 22:04:50 +0100

    Contact Form CTA

--- atcf7/contact-form2.php
+++ atcf7/contact-form2.php
@@ -2,7 +2,7 @@
 if (!defined('ABSPATH')) {
   exit;
 }
-/** 
+/**
  * Template Name: Formularz kontaktowy2
  * */
 ?>
@@ -11,28 +11,33 @@ if (!defined('ABSPATH')) {
   <tbody>
     <tr class="flex flex-col desktop:flex-row gap-[20px]">
       <td class="w-[100%] desktop:w-[50%] leading-[0px]">
-        <!-- [text* your-name id:your-name ] -->
-        <input id="your-name" name="your-name" type="text" placeholder="Imię i nazwisko" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575] focus:border-white active:border-black drop-shadow-lg">
-        <!-- <div> [email* your-email id:your-email ] </div> -->
-        <input id="your-name" name="your-name" type="text" placeholder="Adres email" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]">
-        <!-- <div> [email* your-email id:your-email ] </div> -->
-        <input id="your-phone" name="your-phone" type="text" placeholder="Numer telefonu" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]">
+        [text* your-name id:your-name placeholder "Imię i nazwisko"]
+        <!-- <input id="your-name" name="your-name" type="text" placeholder="Imię i nazwisko" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575] focus:border-white active:border-black drop-shadow-lg"> -->
+        [email* your-email id:your-email placeholder "Adres email"]
+        <!-- <input id="your-name" name="your-name" type="text" placeholder="Adres email" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]"> -->
+        [text* your-phone id:your-phone placeholder "Numer telefonu"]
+        <!-- <input id="your-phone" name="your-phone" type="text" placeholder="Numer telefonu" class="px-[15px] py-[10px] w-[330px] desktop:w-full h-[60px] text-[#2e384d] placeholder:text-[#757575]"> -->
       </td>
       <td class="w-[100%] desktop:w-[50%]">
-        <textarea id="your-name" cols="50" row="10" name="your-name" type="textarea" placeholder="Treść wiadomości" class="px-[15px] py-[10px] mb-[10px] w-[330px] desktop:w-full h-[240px] text-[#2e384d] placeholder:text-[#757575]"></textarea>
+        [textarea* your-content id:your-content placeholder "Treść wiadomości"]
+        <!-- <textarea id="your-name" cols="50" row="10" name="your-name" type="textarea" placeholder="Treść wiadomości" class="px-[15px] py-[10px] mb-[10px] w-[330px] desktop:w-full h-[240px] text-[#2e384d] placeholder:text-[#757575]"></textarea>  -->
       </td>
     </tr>
 
     <tr class="w-full">
       <td class="w-full flex">
-        <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="w-full" />
-        <p class="ml-[10px] mb-[20px] w-full text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        <label>
+          [checkbox* check1 class:inline id:check1]<input id="check1" cols="50" row="10" name="check1" type="checkbox" class="w-full" />
+          <p class="ml-[10px] mb-[20px] w-full text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        </label>
       </td>
     </tr>
     <tr>
       <td class="flex">
-        <input id="check2" name="check2" type="checkbox" class="inline" />
-        <p class="ml-[10px] text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        <label>
+          [checkbox* check2 class:inline id:check2]<input id="check2" name="check2" type="checkbox" class="inline" />
+          <p class="ml-[10px] text-white text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
+        </label>
       </td>
     </tr>
     <tr>
@@ -43,6 +48,6 @@ if (!defined('ABSPATH')) {
   </tbody>
 </table>
 <div>
-  <!-- [submit "Submit"] -->
-  <button type="submit" class="relative mx-auto bg-primaryRed px-[20px] text-white w-fit h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#95102b] before:transition-height before:duration-300 before:ease cursor-pointer" style="border-bottom:3px solid #95102b"><span class=" z-[1] relative">Wyślij</span></button>
+  [submit "Wyślij wiadomość"]
+  <!-- <button type="submit" class="relative mx-auto bg-primaryRed px-[20px] text-white w-fit h-[55px] flex items-center justify-center before:content-[''] before:inline-block before:absolute before:bottom-0 before:left-0 before:w-full before:min-h-[0%] hover:before:min-h-[100%] before:bg-[#95102b] before:transition-height before:duration-300 before:ease cursor-pointer" style="border-bottom:3px solid #95102b"><span class=" z-[1] relative">Wyślij</span></button>  -->
 </div>
