db50141bedb99d688d9a10cf327f8b614460c427
Tomek <TK-54-4B>
2024-02-09 20:19:05 +0100

    prrice field test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -1045,15 +1045,15 @@ window.addEventListener("load", function() {
       }
 
       // old
-      // YMCTools({
-      //   target: filterID,
-      //   terms: choosenOptions.join() + ',' + pricesID.join() + ',' + metrazeID.join() + ',' + searchedReadyArr.join(),      
-      // }).apiTermUpdate(); 
-
       YMCTools({
         target: filterID,
-        terms: pricesID.join(),      
+        terms: choosenOptions.join() + ',' + pricesID.join() + ',' + metrazeID.join() + ',' + searchedReadyArr.join(),      
       }).apiTermUpdate(); 
+
+      // YMCTools({
+      //   target: filterID,
+      //   terms: pricesID.join(),      
+      // }).apiTermUpdate(); 
 
       console.log('in btn prices: ', pricesID.join());
       console.log('in btn metraze: ', metrazeID.join());
