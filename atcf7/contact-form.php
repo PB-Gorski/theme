9f03535ef984c7b8fb36bf3ebbc4dd1553b65376
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-09 13:09:16 +0100

    Contact form settings

--- atcf7/contact-form.php
+++ atcf7/contact-form.php
@@ -41,7 +41,7 @@ if (!defined('ABSPATH')) {
       <th><label for="check1" onclick=""></label></th>
       <td class="flex gap-[6px]">
         [checkbox* check1 class:inline x5 id:check1 ]
-        <!-- <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="inline" /> -->
+        <input id="check1" cols="50" row="10" name="check1" type="checkbox" class="inline" />
         <p class="ml-[10px] mb-[20px] inline text-[#8a8f99] text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu udzielenia odpowiedzi na zapytanie. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
@@ -49,7 +49,7 @@ if (!defined('ABSPATH')) {
       <th><label for="check2" onclick=""></label></th>
       <td class="flex gap-[6px]">
         [checkbox* check2 class:inline x5 id:check2 ]
-        <!-- <input id="check2" name="check2" type="checkbox" class="inline" /> -->
+        <input id="check2" name="check2" type="checkbox" class="inline" />
         <p class="ml-[10px] inline text-[#8a8f99] text-[12px] leading-[18px]"> Wyrażam zgodę na przetwarzanie moich danych osobowych, podanych w formularzu kontaktowym, przez Przedsiębiorstwo Budowlane Górski spółka z ograniczoną odpowiedzialnością spółka komandytowo-akcyjna z siedzibą w Gdańsku w celu przesyłania informacji marketingowych na mój adres email podany w formularzu. Podanie danych jest dobrowolne. Oświadczam, że zapoznałem się z klauzulą informacyjną w zakresie ochrony danych osobowych dostępną tutaj</p>
       </td>
     </tr>
