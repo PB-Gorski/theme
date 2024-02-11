289179154eea588f285c2f01ba518ed91fb36014
Tomek <TK-54-4B>
2024-02-11 18:04:01 +0100

    filter o inwestycji test

--- assets/o-inwestycji-lista-mieszkan-page-searching.js
+++ assets/o-inwestycji-lista-mieszkan-page-searching.js
@@ -633,7 +633,7 @@ window.addEventListener("load", function () {
           document.referrer.search("galeria-" + investName) > 20 ||
           document.referrer.search("kronika-budowy-" + investName) > 20
         ) {
-          document.cookie = "filteredTermsFromCookies=" + investID + ";";
+          document.cookie = "filteredTermsFromCookies=" + investID + ',642' + ";";
           globalCurrentInvestID = investID;
           globalCurrentInvestName = investName;
         }
