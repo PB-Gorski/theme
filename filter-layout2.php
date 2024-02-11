4ce8f46e856fca0a245ac24fb43444c51e9145ef
Tomek <TK-54-4B>
2024-02-11 16:09:13 +0100

    current invest o inwestycji page test

--- filter-layout2.php
+++ filter-layout2.php
@@ -185,7 +185,7 @@ $tax_sort = $taxonomy;
             $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 
             $escaped_url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
-            echo $escaped_url
+            echo $escaped_url;
 
             ?>
 
