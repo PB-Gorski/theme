36fd4e9bceb399b8711c7605a5b87870e1bb0d82
Tomek <TK-54-4B>
2024-02-11 18:59:52 +0100

    filter search button link test

--- filter-layout2.php
+++ filter-layout2.php
@@ -181,8 +181,7 @@ $tax_sort = $taxonomy;
             $url =  "https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
             $escaped_url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
             $currentInvest = '-' . str_replace('/', '', (str_replace(home_url() . '/o-inwestycji-', '', $escaped_url)));
-            print_r($currentInvest);
-            echo '<br>';
+
             if (strpos($url, 'o-inwestycji') == false) {
               // homepage
               $searchBtnLink = '/lista-mieszkan' . $currentInvest;
