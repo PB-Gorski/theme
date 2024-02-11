a58d29d1cf4a6040d865c875ad8a171b3371232e
Tomek <TK-54-4B>
2024-02-11 16:31:07 +0100

    current invest o inwestycji page test

--- filter-layout2.php
+++ filter-layout2.php
@@ -182,7 +182,7 @@ $tax_sort = $taxonomy;
             </div>
 
             <?php
-            $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+            $url =  "https{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 
             $escaped_url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
             echo $escaped_url;
@@ -194,7 +194,7 @@ $tax_sort = $taxonomy;
             echo '2**********';
             echo $url;
             echo '</br>';
-            echo str_replace(home_url(), '', $escaped_url);
+            echo str_replace(home_url(), 'test', $escaped_url);
             echo '</br>';
             echo '2**********';
             echo '</br>';
