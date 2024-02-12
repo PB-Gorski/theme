253faa1f51277c2d71f4c708380c749916afbed1
Tomek <TK-54-4B>
2024-02-12 18:05:15 +0100

    cookies test

--- assets/lista-mieszkan-page-searching.js
+++ assets/lista-mieszkan-page-searching.js
@@ -480,7 +480,8 @@ window.addEventListener("load", function() {
             const eqPos = cookie.indexOf("=");
             const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-        }
+        };
+        console.log('deleteAllCookies() done');
     };
     deleteAllCookies();
 
