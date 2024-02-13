c7f2a546213b057b375c2df35e28f45beb5600ec
Tomek <TK-54-4B>
2024-02-13 19:36:11 +0100

    cookies test

--- functions.php
+++ functions.php
@@ -354,17 +354,17 @@ function pb_gorski_blocks_register_styles()
 	wp_enqueue_script('aos', '//unpkg.com/aos@2.3.1/dist/aos.js', '2.3');
 
 
-	wp_enqueue_style('custom-styles', get_theme_file_uri('/assets/styles.min.css'), '1.0', array('swiper-css'));
+	wp_enqueue_style('custom-styles', get_theme_file_uri('/assets/styles.min.css'), '2.0', array('swiper-css'));
 	wp_enqueue_style('google-fonts-api', '//fonts.googleapis.com', '1.0');
 	wp_enqueue_style('google-fonts-gstatic', '//fonts.gstatic.com', '1.0');
 	wp_enqueue_style('google-fonts-roboto', '//fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap', '1.0');
 	wp_enqueue_style('google-fonts-Montserrat', '//fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap', '1.0');
 	wp_enqueue_style('aos-styles', '//unpkg.com/aos@2.3.1/dist/aos.css', '1.0');
-	wp_enqueue_script('customJS', get_theme_file_uri('/assets/custom-js.js'), '1.0', array('custom-js', 'tailwind-config'));
-	wp_enqueue_script('customJS2', get_theme_file_uri('/assets/frontpage-searching-data-cookies.js'), '1.0', array('frontpage-searching-data-cookies.js', 'tailwind-config'));
-	wp_enqueue_script('customJS3', get_theme_file_uri('/assets/lista-mieszkan-page-searching.js'), '1.0', array('lista-mieszkan-page-searching.js', 'tailwind-config'));
-	wp_enqueue_script('customJS4', get_theme_file_uri('/assets/o-inwestycji-lista-mieszkan-page-searching.js'), '1.0', array('o-inwestycji-lista-mieszkan-page-searching.js', 'tailwind-config'));
-	wp_enqueue_script('customJS5', get_theme_file_uri('/assets/single-mieszkania-cookie-data.js'), '1.0', array('single-mieszkania-cookie-data.js', 'tailwind-config'));
+	wp_enqueue_script('customJS', get_theme_file_uri('/assets/custom-js.js'), array(), '2.0');
+	wp_enqueue_script('customJS2', get_theme_file_uri('/assets/frontpage-searching-data-cookies.js'),  array(), '2.0');
+	wp_enqueue_script('customJS3', get_theme_file_uri('/assets/lista-mieszkan-page-searching.js'),  array(), '2.0');
+	wp_enqueue_script('customJS4', get_theme_file_uri('/assets/o-inwestycji-lista-mieszkan-page-searching.js'), array(), '2.0');
+	wp_enqueue_script('customJS5', get_theme_file_uri('/assets/single-mieszkania-cookie-data.js'), array(), '2.0');
 }
 add_action('wp_enqueue_scripts', 'pb_gorski_blocks_register_styles');
 
