e52e8458c013bf338ea723434d14762eb61fe4ea
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-15 15:27:24 +0100

    Terms in page

--- patterns/page-lista-mieszkan-default-filtr.php
+++ patterns/page-lista-mieszkan-default-filtr.php
@@ -18,10 +18,13 @@
       <li class="short-code-ymc-filter applyContainer1 applyContainer2">
         <?php echo do_shortcode('[ymc_filter id="148"]'); ?>
         <?php
-        $terms = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+        $scheme  = (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] === "off") ? "http" : "https";
+        $url     = "$scheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        echo url_to_postid($url);
 
+        $terms = wp_get_object_terms(url_to_postid($url), 'inwestycja');
         foreach ($terms as $term) {
-          echo "<span data-investment-id='" . $term->term_id . "'></span>";
+          echo "<span data-investment-id='" . $term->ID . "'></span>";
         }
         ?>
       </li>
