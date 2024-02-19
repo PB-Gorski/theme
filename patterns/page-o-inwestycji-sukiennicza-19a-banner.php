2f2614f19dc2c88104c8d30af02c9d55833a6b1d
Tomek <TK-54-4B>
2024-02-19 16:43:03 +0100

    term-data test

--- patterns/page-o-inwestycji-sukiennicza-19a-banner.php
+++ patterns/page-o-inwestycji-sukiennicza-19a-banner.php
@@ -37,5 +37,34 @@
     <!-- /wp:group -->
   </div>
   <!-- /wp:group -->
+
+  <!-- wp:list -->
+  <ul class="test">
+    <!-- wp:list-item -->
+    <li class="test">
+      <?php
+      $scheme  = (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] === "off") ? "http" : "https";
+      $url     = "$scheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      $page_id = url_to_postid($url);
+      $terms = wp_get_object_terms($page_id, 'inwestycja');
+
+      echo "<span id='term-data' ";
+      foreach ($terms as $term) {
+        $term_city = get_field('miasto_inwestycji', $term);
+
+        echo " data-investment-city-id='" . $term_city->term_id . "' data-investment-city-name='" . $term_city->name . "' ";
+      }
+
+      foreach ($terms as $term) {
+        echo " data-investment-id='" . $term->term_id . "' data-investment-name='" . $term->name . "' ";
+      }
+
+      echo "></span>";
+
+      ?>
+    </li>
+    <!-- /wp:list-item -->
+  </ul>
+  <!-- /wp:list -->
 </div>
 <!-- /wp:group -->
