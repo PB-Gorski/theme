42a8b44716a8fed552cf4db6da6840ff8e7f60ec
Tomek <TK-54-4B>
2024-02-19 17:17:34 +0100

    term-data added [pages o inwestycji galeria]

--- patterns/page-galeria-torunska-16.php
+++ patterns/page-galeria-torunska-16.php
@@ -42,6 +42,32 @@ $currentPage = str_replace('/', '', substr("http://$_SERVER[HTTP_HOST]$_SERVER[R
   </div>
   <!-- /wp:group -->
 
+  <!-- wp:list -->
+  <ul class="term_data_container">
+    <!-- wp:list-item -->
+    <li>
+      <?php
+      $scheme  = (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] === "off") ? "http" : "https";
+      $url     = "$scheme://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+      $page_id = url_to_postid($url);
+      $terms = wp_get_object_terms($page_id, 'inwestycja');
+
+      echo "<span id='term-data' ";
+      foreach ($terms as $term) {
+        $term_city = get_field('miasto_inwestycji', $term);
+        echo " data-investment-city-id='" . $term_city->term_id . "' data-investment-city-name='" . $term_city->name . "' ";
+      }
+
+      foreach ($terms as $term) {
+        echo " data-investment-id='" . $term->term_id . "' data-investment-name='" . $term->name . "' ";
+      }
+      echo "></span>";
+      ?>
+    </li>
+    <!-- /wp:list-item -->
+  </ul>
+  <!-- /wp:list -->
+
   <!-- wp:pattern {"slug":"searchbar-mieszkania"} /-->
 
 
