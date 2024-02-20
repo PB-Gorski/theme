9291014d1b0d26af709f0c6d3e890f3e7b11e0f7
Tomek <TK-54-4B>
2024-02-20 17:46:07 +0100

    test

--- patterns/investments.php
+++ patterns/investments.php
@@ -61,7 +61,7 @@
           <li class="invest-tile max-w-full w-full inline-block">
             <!-- wp:group -->
             <div class="wp-block-group">
-              <!-- <a href="<?php echo home_url() . '/o-inwestycji-' . $alias; ?>" class="inline-block w-full"> -->
+
               <!-- wp:group -->
               <div class="wp-block-group relative group desktop:max-w-full  h-full">
                 <!-- wp:group -->
@@ -146,7 +146,7 @@
                 <!-- /wp:group -->
               </div>
               <!-- /wp:group -->
-              <!-- </a> -->
+
             </div>
             <!-- /wp:group -->
           </li>
