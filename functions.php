d2a909fc8566beb3a0899c602584e51ba78ad426
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-02-21 00:08:36 +0100

    Fix using interval

--- functions.php
+++ functions.php
@@ -360,23 +360,23 @@ function pb_gorski_blocks_register_styles()
 	wp_enqueue_style('google-fonts-roboto', '//fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap', '1.0');
 	wp_enqueue_style('google-fonts-Montserrat', '//fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap', '1.0');
 	wp_enqueue_style('aos-styles', '//unpkg.com/aos@2.3.1/dist/aos.css', '1.0');
-	wp_enqueue_script('customJS', get_theme_file_uri('/assets/custom-js.js'), array(), '2.5', array(
+	wp_enqueue_script('customJS', get_theme_file_uri('/assets/custom-js.js'), array(), '2.5.1', array(
 		'strategy'  => 'defer',
 		'in_footer'  => true,
 	));
-	wp_enqueue_script('customJS2', get_theme_file_uri('/assets/frontpage-searching-data-cookies.js'),  array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5', array(
+	wp_enqueue_script('customJS2', get_theme_file_uri('/assets/frontpage-searching-data-cookies.js'),  array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5.1', array(
 		'strategy'  => 'defer',
 		'in_footer'  => true,
 	));
-	wp_enqueue_script('customJS3', get_theme_file_uri('/assets/lista-mieszkan-page-searching.js'),  array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5', array(
+	wp_enqueue_script('customJS3', get_theme_file_uri('/assets/lista-mieszkan-page-searching.js'),  array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5.1', array(
 		'strategy'  => 'defer',
 		'in_footer'  => true,
 	));
-	wp_enqueue_script('customJS4', get_theme_file_uri('/assets/o-inwestycji-lista-mieszkan-page-searching.js'), array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5', array(
+	wp_enqueue_script('customJS4', get_theme_file_uri('/assets/o-inwestycji-lista-mieszkan-page-searching.js'), array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5.1', array(
 		'strategy'  => 'defer',
 		'in_footer'  => true,
 	));
-	wp_enqueue_script('customJS5', get_theme_file_uri('/assets/single-mieszkania-cookie-data.js'), array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5', array(
+	wp_enqueue_script('customJS5', get_theme_file_uri('/assets/single-mieszkania-cookie-data.js'), array('smart-filter-' . wp_create_nonce('smart-filter')), '2.5.1', array(
 		'strategy'  => 'defer',
 		'in_footer'  => true,
 	));
