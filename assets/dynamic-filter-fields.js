2ff3c13eae829fdd4cf914529fbe12987f0f7694
Tomek <TK-54-4B>
2024-02-22 20:00:46 +0100

    added raw dependencies to investment

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -1,5 +1,33 @@
-window.addEventListener("load", function () {
+// window.addEventListener("load", function () {
   if (document.querySelector('.ymc-smart-filter-container')){
     console.log('dynamic-filter-fields.js');
+    let inwestycjaOptionPassiveArr = document.querySelectorAll('.menu-link');
+    let swPiotrOption;
+
+    inwestycjaOptionPassiveArr.forEach(item =>{
+      if (item.dataset.name = 'Św. Piotra'){
+        item.dataset.dependencies = '{"dependencies":[{"termId":"40"},{"termId":"640"},{"termId":"641"}]}';
+        swPiotrOption = item;
+      };
+    });
+
+    let SwPiotraDependenciesArr = JSON.parse(swPiotrOption.dataset.dependencies).dependencies;
+
+    console.log('SwPiotraDependenciesArr: ', SwPiotraDependenciesArr);
+
+    let passiveOptionsArr = document.querySelectorAll(".menu-link");
+    let choosenFilterFieldsArr= [];
+
+    passiveOptionsArr.forEach(item => {
+      item.addEventListener('click', () => {
+        console.log('item: ', item);
+        if (item.classList.contains('active')){
+          console.log('active added');
+          choosenFilterFieldsArr.push(item);
+        };
+      });
+    });
+    
+    console.log('choosenFilterFieldsArr: ', choosenFilterFieldsArr);
   };
-});
+// });
