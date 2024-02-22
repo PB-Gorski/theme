b1913bf2dff296ebc0b5cec19b80893be2c1b26c
Tomek <TK-54-4B>
2024-02-22 20:33:46 +0100

    added raw dependencies to investment

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -1,6 +1,7 @@
 // window.addEventListener("load", function () {
   if (document.querySelector('.ymc-smart-filter-container')){
     console.log('dynamic-filter-fields.js');
+
     // removing items from arr
     function removeItemFromArr(arr, value) {
       let i = 0;
@@ -9,14 +10,17 @@
           arr.splice(i, 1);
         } else {
           ++i;
-        }
-      }
+        };
+      };
       return arr;
-    }
+    };
     // ---------
 
     let inwestycjaOptionPassiveArr = document.querySelectorAll('.menu-link');
     let swPiotrOption;
+    let SwPiotraDependenciesArr = JSON.parse(swPiotrOption.dataset.dependencies).dependencies;
+    let passiveOptionsArr = document.querySelectorAll(".menu-link");
+    let choosenFilterFieldsArr= [];
 
     inwestycjaOptionPassiveArr.forEach(item =>{
       if (item.dataset.name == 'Św. Piotra'){
@@ -24,33 +28,29 @@
         swPiotrOption = item;
       };
     });
-
-    let SwPiotraDependenciesArr = JSON.parse(swPiotrOption.dataset.dependencies).dependencies;
-
     console.log('SwPiotraDependenciesArr: ', SwPiotraDependenciesArr);
 
-    let passiveOptionsArr = document.querySelectorAll(".menu-link");
-    let choosenFilterFieldsArr= [];
+    function getChoosenIds(){
+      let activeIds = window.setInterval(function(){
+        if (item.classList.contains('active')){
+
+          if (choosenFilterFieldsArr.includes(item.dataset.termid)) {
+            removeItemFromArr(choosenFilterFieldsArr, item.dataset.termid);
+          } else {
+            choosenFilterFieldsArr.push((item.dataset.termid));
+          };
+          
+        };
+        clearInterval(activeIds);
+      },200)
+    };
 
     passiveOptionsArr.forEach(item => {
       item.addEventListener('click', () => {
-        let activeIds = window.setInterval(function(){
-          if (item.classList.contains('active')){
-
-            if (choosenFilterFieldsArr.includes(item.dataset.termid)) {
-              removeItemFromArr(choosenFilterFieldsArr, item.dataset.termid);
-            } else {
-              choosenFilterFieldsArr.push((item.dataset.termid));
-            };
-            
-          };
-          clearInterval(activeIds);
-        },200)
+        getChoosenIds();
         
         console.log('choosenFilterFieldsArr: ', choosenFilterFieldsArr);
       });
     });
-
-    
   };
 // });
