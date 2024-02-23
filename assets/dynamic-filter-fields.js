9e56b4d0e629f472badd16f111a868622e04c33b
Tomek <TK-54-4B>
2024-02-23 19:20:32 +0100

    dynamic filter tests

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -28,7 +28,12 @@
         item.dataset.dependencies = '{"dependencies":[{"termId":"40"},{"termId":"640"},{"termId":"641"}]}';
         swPiotrOption = item;
       }else{
-        item.dataset.dependencies = '{"dependencies":[{"termId":"0"},{"termId":"0"},{"termId":"0"}]}';
+        item.dataset.dependencies = '{"dependencies":[{"termId":"1"},{"termId":"2"},{"termId":"3"}]}';
+      };
+
+      if (item.dataset.name == 'Zatorze'){
+        item.dataset.dependencies = '{"dependencies":[{"termId":"40"},{"termId":"640"},{"termId":"641"}]}';
+        swPiotrOption = item;
       };
     });
 
@@ -37,55 +42,45 @@
 
     function getChoosenCityId(item){
       // let activeIds = window.setInterval(function(){
-        console.log('start: getChoosenCityId');
-        choosenFilterFieldsArr = [];
+        // console.log('start: getChoosenCityId');
+        
         if (item.classList.contains('active')){
-
-          // if (choosenFilterFieldsArr.includes(item.dataset.termid)) {
-            // removeItemFromArr(choosenFilterFieldsArr, item.dataset.termid);
-          // } else {
-            choosenFilterFieldsArr = ((item.dataset.termid));
-          // };
-          
+          choosenFilterFieldsArr = [];
+          choosenFilterFieldsArr = ((item.dataset.termid));
         };
         // console.log('choosenFilterFieldsArr: ', +choosenFilterFieldsArr);
         // clearInterval(activeIds);
-      // },200)
+      // },100);
+      // console.log('end: getChoosenCityId');
+      return choosenFilterFieldsArr;
     };
 
     function checkInwestycjeForDependenciesID(){
       console.log('start: checkInwestycjeForDependenciesID');
       let inwestycjeDependenciesArr = [];
-      inwestycjePassiveArr.forEach(item => {
-        console.log(item.childNodes[1]);
-        if (item.childNodes[1] != undefined){
 
-          // console.log('json parse: ', item.childNodes[1].dataset.dependencies);
-          // console.log('json parse2: ', JSON.parse(item.childNodes[1].dataset.dependencies).dependencies);
+      inwestycjePassiveArr.forEach(item => {
+        if (item.childNodes[1] != undefined){
           inwestycjeDependenciesArr.push(JSON.parse(item.childNodes[1].dataset.dependencies).dependencies);
 
-          console.log('inwestycjeDependenciesArr: ', inwestycjeDependenciesArr[0]);
-
           for (let i = 0 ; i < inwestycjeDependenciesArr.length ; i++){
-          //  console.log(inwestycjeDependenciesArr[i]);
-          for (let j = 0 ;  j < inwestycjeDependenciesArr[i].length ; j++){
-            console.log(inwestycjeDependenciesArr[i][j].termId);
-            console.log('2before check choosenFilterFieldsArr:', choosenFilterFieldsArr);
-            if (+inwestycjeDependenciesArr[i][j].termId == choosenFilterFieldsArr){
-              console.log('match: ',i,j);
+            for (let j = 0 ;  j < inwestycjeDependenciesArr[i].length ; j++){
+              if (+inwestycjeDependenciesArr[i][j].termId == +choosenFilterFieldsArr){
+                console.log('match: ', item);
+              };
             };
-          };
           };
         };
       });
     };
 
-
-
     passiveOptionsArr.forEach(item => {
       item.addEventListener('click', () => {
-        getChoosenCityId(item);
-        // checkInwestycjeForDependenciesID();
+        setTimeout(() => {
+          choosenFilterFieldsArr = +getChoosenCityId(item);
+
+          checkInwestycjeForDependenciesID();
+        }, 200);
 
       });
     });
