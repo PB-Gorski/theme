62b6ed456315f2055aa46b21ed2a1efa14c3b3df
Tomek <TK-54-4B>
2024-02-25 13:59:30 +0100

    dynamic filter tests

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -76,8 +76,9 @@
             console.log('matched item:: ', inwestycjePassiveArr[i+3]);
             // inwestycjePassiveArr[i+3].classList.remove('bg-red-500');
             inwestycjePassiveArr[i+3].classList.add('bg-green-500', 'dynamic-active','hover:bg-green-600');
-            inwestycjePassiveArr[i+3].style.background = 'green';
+            // inwestycjePassiveArr[i+3].style.background = 'green';
           }else if(+inwestycjeDependenciesArr[i][j].termId != +choosenFilterFieldsArr){
+            inwestycjePassiveArr[i+3].classList.remove('bg-green-500', 'dynamic-active','hover:bg-green-600');
             // inwestycjePassiveArr[i+3].style.background = 'red';
             // inwestycjePassiveArr[i+3].classList.add('bg-red-500');
           };
