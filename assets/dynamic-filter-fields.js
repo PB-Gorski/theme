e28667f31d17ec7e17561d25a880baec13c4a138
Tomek <TK-54-4B>
2024-02-25 14:15:25 +0100

    dynamic filter tests

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -78,6 +78,14 @@
           };
         };
       };
+
+      passiveOptionsArr.forEach(item => {
+        if(!item.classList.contains('dynamic-active')){
+          console.log('item without dynamic-active: ', item);
+          item.classList.remove('bg-green-500');
+          item.classList.add('bg-yellow-500');
+        };
+      });
     };
 
     passiveOptionsArr.forEach(item => {
