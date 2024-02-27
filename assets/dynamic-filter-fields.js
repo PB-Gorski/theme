84901fa2a9c7e397b1bebd84a3bebf27281e0d95
Tomek <TK-54-4B>
2024-02-27 19:22:56 +0100

    dynamic filter tests

--- assets/dynamic-filter-fields.js
+++ assets/dynamic-filter-fields.js
@@ -92,7 +92,7 @@
       // cleaning
       let inwestycjeDependenciesArr = [];
       inwestycjePassiveArr.forEach(item => {
-        if (item.childNodes[1] != undefined  && !item.childNodes[1].classList.contains('dynamic-active')){
+        if (item.childNodes[1] != undefined  && !item.classList.contains('dynamic-active')){
           console.log('removed dynamic-active');
           item?.classList.remove('dynamic-active');
         };
