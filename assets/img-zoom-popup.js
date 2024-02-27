6af1210106e7bf5e079fb05a0b00aa5c40dd84a6
Tomek <TK-54-4B>
2024-02-27 17:29:12 +0100

    featured image zoom tests

--- assets/img-zoom-popup.js
+++ assets/img-zoom-popup.js
@@ -2,5 +2,18 @@
   if (document.body.classList.contains('single-mieszkania')){
     console.log('img-zoom-popup.js');
 
+    let featuredImage = document.querySelector('.wp-block-post-featured-image');
+    let featuredImageBig = document.querySelector('.js-featured-img-popup');
+    let featuredImageBigLink = document.querySelector('.js-featured-img-popup').childNodes[0].src;
+    let featuredImageLink = document.querySelector('.wp-block-post-featured-image').childNodes[0].src;
+
+    featuredImageBigLink = featuredImageLink;
+
+    
+
+    featuredImage.addEventListener('click', () => {
+
+    });
+    
   };
 // });
