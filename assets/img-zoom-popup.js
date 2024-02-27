37d48b0eeb47b96787544f5a2a778ae1150e8098
Tomek <TK-54-4B>
2024-02-27 19:08:51 +0100

    featured image zoom tests

--- assets/img-zoom-popup.js
+++ assets/img-zoom-popup.js
@@ -1,7 +1,5 @@
 // window.addEventListener("load", function () {
   if (document.body.classList.contains('single-mieszkania')){
-    console.log('img-zoom-popup.js');
-
     let featuredImage = document.querySelector('.wp-post-image');
     let featuredImageLink = document.querySelector('.wp-post-image').src;
     let featuredImageBig = document.querySelector('.js-featured-img-popup').childNodes[1];
