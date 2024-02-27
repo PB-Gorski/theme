ae1e9f5aeb51a0871204ee0224768926ea1f99c9
Tomek <TK-54-4B>
2024-02-27 17:54:55 +0100

    featured image zoom tests

--- assets/img-zoom-popup.js
+++ assets/img-zoom-popup.js
@@ -4,15 +4,23 @@
 
     let featuredImage = document.querySelector('.wp-block-post-featured-image');
     let featuredImageBig = document.querySelector('.js-featured-img-popup');
-    let featuredImageBigLink = document.querySelector('.js-featured-img-popup').childNodes[1]
     let featuredImageLink = document.querySelector('.wp-block-post-featured-image').childNodes[0].src;
+    let popupImageCloseBtn = document.querySelector('.js-close-popup-featured-image');
+    let popupImageContainer = document.querySelector('.js-featured_img_popup_container');
 
-    featuredImageBigLink.src = featuredImageLink;
-    featuredImageBigLink.classList.add('scale-[0.6]')
+    featuredImageBig.childNodes[1].src = featuredImageLink;
+    featuredImageBig.classList.add('scale-[0.6]')
 
 
 
     featuredImage.addEventListener('click', () => {
+      popupImageContainer.classList.toggle('hidden');
+      console.log('popupImageContainer toggle1');
+    });
+
+    popupImageCloseBtn.addEventListener('click', () => {
+      popupImageContainer.classList.toggle('hidden');
+      console.log('popupImageContainer toggle2');
 
     });
     
