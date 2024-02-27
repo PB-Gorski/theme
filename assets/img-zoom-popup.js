68e629c42d59fe8c605cc25a1292f63b0fe031a8
Tomek <TK-54-4B>
2024-02-27 17:06:09 +0100

    added js file - img-zoom-popup

--- assets/img-zoom-popup.js
+++ assets/img-zoom-popup.js
@@ -1,6 +1,6 @@
-// window.addEventListener("load", function () {
+window.addEventListener("load", function () {
   if (document.body.classList.contains('single-mieszkania')){
     console.log('img-zoom-popup.js');
 
   };
-// });
+});
