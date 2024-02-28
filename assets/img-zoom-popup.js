43b110e958c5d50f35a88f16eac069f643fe4e55
Tomek <TK-54-4B>
2024-02-28 16:55:40 +0100

    closing popup featured image fix

--- assets/img-zoom-popup.js
+++ assets/img-zoom-popup.js
@@ -2,6 +2,8 @@
   if (document.body.classList.contains('single-mieszkania')){
     let featuredImage = document.querySelector('.wp-post-image');
     let featuredImageLink = document.querySelector('.wp-post-image').src;
+
+
     if(featuredImageLink.includes('data:')){
       featuredImageLink = document.querySelector('.wp-post-image').dataset.src;
     }
@@ -13,15 +15,25 @@
     featuredImageBig.src = featuredImageLink;
     featuredImageBig.style.height = '90%';
 
+    // open popup
     featuredImage.addEventListener('click', () => {
       popupImageContainer.classList.toggle('opacity-100');
       popupImageContainer.classList.toggle('z-[2]');
     }, {passive: true});
 
+    // close popup
     popupImageCloseBtn.addEventListener('click', () => {
       popupImageContainer.classList.toggle('opacity-100');
       popupImageContainer.classList.toggle('z-[2]');
     }, {passive: true});
 
+    // close popup by clicking outiside img
+    popupImageContainer.addEventListener('click', (e) => {
+      popupImageContainer.classList.toggle('opacity-100');
+      popupImageContainer.classList.toggle('z-[2]');
+    }, {passive: true});
+    featuredImageBig.addEventListener('click', (e) => {
+      e.stopPropagation();
+    }, {passive: true});
   };
 // });
