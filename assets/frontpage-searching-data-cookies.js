c2066a8007a08a0cbdba81dd516a547c210df24f
Tomek <TK-54-4B>
2024-03-07 17:15:05 +0100

    test

--- assets/frontpage-searching-data-cookies.js
+++ assets/frontpage-searching-data-cookies.js
@@ -566,11 +566,13 @@ window.addEventListener("load", function() {
           console.log('else', choosenOptions);
           choosenOptionsTemp.push(e.target.dataset.termid)
         };
-        
+
         if(e.target.dataset.termid == '28'){
+          removeItemAll(choosenOptionsTemp,'28');
           choosenOptionsTemp.includes('40') ? removeItemAll(choosenOptionsTemp,'40') : null;
           choosenOptionsTemp.push('28');
         }else if((e.target.dataset.termid == '40')){
+          removeItemAll(choosenOptionsTemp,'40');
           choosenOptionsTemp.includes('28') ? removeItemAll(choosenOptionsTemp,'28') : null;
           choosenOptionsTemp.push('40');
         };
